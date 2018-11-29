ac6ab2f12b148d8b85f49d0ef3f79bff2c12a2fe
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2018-11-29 20:55:17 +0200

    feat: Added support for more block types.

--- preact-demo/packages/blocks/Token.js
+++ preact-demo/packages/blocks/Token.js
@@ -1,12 +1,17 @@
-import { h } from 'preact';
+import classnames       from 'classnames';
+import { h }            from 'preact';
 
 import S from './Token.sass';
 
-export default ({ block: { text }}) =>
+export default ({ block: { text, type }}) =>
     ( !text )
         ? null
         : (
-            <div className={ `${ S.className } qa-blocks-token` }>
+            <div className={ classnames(
+                S.className,
+                S[type],
+                'qa-blocks-token',
+            ) }>
                 { text }
             </div>
         );
