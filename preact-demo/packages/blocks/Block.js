af083c35c4a2f28502ddbd276dfffb4a3b1d12d1
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2018-12-06 22:36:00 +0200

    refactor: Greatly simplified Block factory function.

--- preact-demo/packages/blocks/Block.js
+++ preact-demo/packages/blocks/Block.js
@@ -1,49 +1,14 @@
-const EMPTY_FUNCTION =          () => {};
+export default ({ init, json, type, ...mixin }) =>
+    Object.assign(
+        Blockly => {
+            Blockly.Blocks[type] = {
 
-
-export default ({ init, json, type, ...spec }) =>
-    Object.assign( Blockly => {
-
-        const hasMutator = !!(
-            spec.mutationToDom || spec.domToMutation
-            || spec.compose || spec.decompose
-            || spec.blockList
-        );
-
-        const mutator = (
-            json.mutator
-            || ( hasMutator && `${ type }_mutator` )
-            || undefined
-        );
-
-        const hasExtension =    !!init;
-
-        const extensions = (
-            json.extensions
-            || ( hasExtension && !hasMutator && [ `${ type }_extension` ])
-            || undefined
-        );
-
-        const blockJson = {
-            type,
-            ...json,
-            extensions,
-            mutator,
-        };
-
-        Blockly.defineBlocksWithJsonArray([ blockJson ]);
-
-        if( hasMutator ) {
-            Blockly.Extensions.registerMutator(
-                mutator,
-                spec,
-                init || EMPTY_FUNCTION,
-                spec.blockList,
-            );
-        } else if( hasExtension ) {
-            Blockly.Extensions.register(
-                extensions[0],
-                init || EMPTY_FUNCTION,
-            );
-        }
-    }, { json, type, ...spec });
+                init() {
+                    this.jsonInit( json );
+                    this.mixin( mixin );
+                    init && init.call( this );
+                },
+            };
+        },
+        { json, type, ...mixin },
+    );
