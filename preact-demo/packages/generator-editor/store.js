09533c485a0519cc6d9c02922bc3fd63ef2c9111
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2018-12-06 23:19:16 +0200

    refactor: Moved tokenizer json to blockly xml converter to a more appropriate location.

--- preact-demo/packages/generator-editor/store.js
+++ preact-demo/packages/generator-editor/store.js
@@ -1,8 +1,8 @@
 import pTap                 from 'p-tap';
 
 import tokenizer            from '../tokenizer/tokenizer';
+import tokensToBlockly      from '../tokenizer/json-to-blockly';
 
-import tokensToBlockly      from './tokens-to-blockly';
 
 
 const EXAMPLE_XML = '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="segment" id="MQEB3TTN0~=+,-VDEEbU" x="49" y="122"><field name="GOAL">description</field><statement name="CHILDREN"><block type="unordered-list" id="_b^5;))u@H+5SQ]`-(H%"><mutation input_count="4"></mutation><value name="CHILD0"><block type="attribute" id="C./$Z:(d*%mw#$~00]I^"><field name="ATTRIBUTE">color</field></block></value><value name="CHILD1"><block type="attribute" id="MLQLyUhX!{c5$UgOA_Wl"><field name="ATTRIBUTE">material</field></block></value><value name="CHILD2"><block type="attribute" id="a,b4[pxr_5YA)nwLSh(f"><field name="ATTRIBUTE">make</field></block></value></block></statement></block></xml>';
