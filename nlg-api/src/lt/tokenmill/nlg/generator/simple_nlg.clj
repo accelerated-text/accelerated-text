72646d88d60a1a0b3cfa60e68bdfd991a72314ef
Šarūnas Navickas <sarunas.navickas@tokenmill.lt>
2019-01-04 11:17:13 +0200

    Basic sentence

--- nlg-api/src/lt/tokenmill/nlg/generator/simple_nlg.clj
+++ nlg-api/src/lt/tokenmill/nlg/generator/simple_nlg.clj
@@ -1,7 +1,7 @@
 (ns lt.tokenmill.nlg.generator.simple-nlg
   (:require [clojure.tools.logging :as log])
   (:import (simplenlg.lexicon Lexicon)
-           (simplenlg.framework NLGFactory)
+           (simplenlg.framework NLGFactory CoordinatedPhraseElement)
            (simplenlg.realiser.english Realiser)
            (simplenlg.phrasespec SPhraseSpec NPPhraseSpec VPPhraseSpec)))
 
@@ -12,8 +12,21 @@
 (defn add-verb [^SPhraseSpec clause verb] (.setVerb clause verb))
 (defn add-complement [^SPhraseSpec clause complement] (.addComplement clause complement))
 
-(defn create-noun [^NLGFactory factory str] (.createNounPhrase factory str))
-(defn create-verb [^NLGFactory factory str] (.createVerbPhrase factory str))
+(defn create-noun
+  ([^NLGFactory factory noun]
+   (.createNounPhrase factory noun))
+   ([^NLGFactory factory specifier noun]
+    (.createNounPhrase factory specifier noun)))
+  
+(defn create-verb [^NLGFactory factory verb] (.createVerbPhrase factory verb))
+(defn create-adverb [^NLGFactory factory adverb] (.createAdverbPhrase factory adverb))
+
+(defn concat-multi [^NLGFactory factory elements]
+  (let [coordinated (.createCoordinatedPhrase factory)]
+    (do
+      (doseq [e elements] (.addCoordinate coordinated e))
+      coordinated)))
+    
 
 (defn generator
   []
@@ -21,5 +34,5 @@
         realiser (Realiser. lexicon)]
     (fn [fun]
       (let [clause (.createClause factory)
-            _ (fun clause)]
+            _ (fun clause factory)]
         (.realiseSentence realiser clause)))))
