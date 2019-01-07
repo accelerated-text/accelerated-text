682374e4f2d46cbafe9e193c7f8b29d777a37738
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2019-01-07 15:48:41 +0200

    refactor: Split dom-to-gremlin module into smaller ones.

---
+++ preact-demo/packages/blockly-gremlin/constants.js
@@ -0,0 +1,7 @@
+module.exports = {
+    BLOCKLY_ID:         'blockly-id',
+    CHAIN_SEP:          '',
+    GLOBAL:             'g.traversal()',
+    STATEMENT_SEP:      ';\n',
+};
+
--- preact-demo/packages/blockly-gremlin/dom-to-gremlin.js
+++ preact-demo/packages/blockly-gremlin/dom-to-gremlin.js
@@ -3,55 +3,26 @@
 const {
     flatten,
     identity,
-    toPairs,
 } = require( 'ramda' );
 
-/// Constants ------------------------------------------------------------------
-
-const BLOCKLY_ID =      'blockly-id';
-const CHAIN_SEP =       '';
-const GLOBAL =          'g.traversal()';
-const STATEMENT_SEP =   ';\n';
-
-/// Element type tests ---------------------------------------------------------
-
-const isBlock =         el => el.tagName.toLowerCase() === 'block';
-const isField =         el => el.tagName.toLowerCase() === 'field';
-const isMutation =      el => el.tagName.toLowerCase() === 'mutation';
-const isNext =          el => el.tagName.toLowerCase() === 'next';
-const isStatement =     el => el.tagName.toLowerCase() === 'statement';
-const isValue =         el => el.tagName.toLowerCase() === 'value';
-
-/// Base Gremlin generators ----------------------------------------------------
-
-const addEdge =         ( type, from, to ) =>
-    `${ GLOBAL }.addE('${ type }').from(${ from }).to(${ to })`;
-
-const addVertex =       type =>
-    `${ GLOBAL }.addV('${ type }')`;
-
-const setProperty =     ( name, value ) =>
-    `.property('${ name }', '${ value }')`;
-
-const vById =           id =>
-    `${ GLOBAL }.V().has('${ BLOCKLY_ID }', '${ id }').next()`;
-
-/// Complex Gremlin generators -------------------------------------------------
-
-const objToProperties = ( obj = {}) =>
-    toPairs( obj )
-        .map(([ k, v ]) => setProperty( k, v ))
-        .join( CHAIN_SEP );
-
-const connectElements = ( type, fromEl, toEl, properties ) => [
-    addEdge(
-        type,
-        vById( fromEl.getAttribute( 'id' )),
-        vById( toEl.getAttribute( 'id' )),
-    ),
-    objToProperties( properties ),
+const {
+    BLOCKLY_ID,
+    CHAIN_SEP,
     STATEMENT_SEP,
-];
+} = require( './constants' );
+const {
+    addVertex,
+    connectElements,
+    isBlock,
+    isField,
+    isMutation,
+    isNext,
+    isStatement,
+    isValue,
+    setProperty,
+} = require( './functions' );
+const mutations =       require( './mutations' );
+
 
 /// Mutation converters --------------------------------------------------------
 
@@ -61,22 +32,10 @@ const setMutationProperties = el =>
         .map(({ name, value }) => setProperty( name, value ))
         .join( CHAIN_SEP );
 
-const convertMutations = parentEl => mutationEl =>
+const convertMutation = parentEl => mutationEl =>
     [ ...mutationEl.attributes ]
-        .map(({ name, value }) => {
-            switch( name ) {
-            case 'next_values':
-                return [ ...parentEl.children ]
-                    .filter( isValue )
-                    .filter( valueEl => valueEl.getAttribute( 'name' ).startsWith( value ))
-                    .map(( valueEl, idx, array ) =>
-                        idx && connectElements( 'has-next', array[idx - 1].firstElementChild, valueEl.firstElementChild ).join( CHAIN_SEP )
-                    ).filter( identity )
-                    .join( CHAIN_SEP );
-            default:
-                return null;
-            }
-        })
+        .filter(({ name, value }) => mutations[name])
+        .map(({ name, value }) => mutations[name]( parentEl, value, mutationEl ))
         .filter( identity )
         .join( STATEMENT_SEP );
 
@@ -125,7 +84,7 @@ const convertBlock =    ( el, parentEl, parentEdgeType, parentEdgeName ) => {
             ])),
         ...children
             .filter( isMutation )
-            .map( convertMutations( el )),
+            .map( convertMutation( el )),
     ].join( CHAIN_SEP );
 };
 
