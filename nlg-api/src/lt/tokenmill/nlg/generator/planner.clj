8f5de00b76aa5c9df286e7191de053131ba60729
Šarūnas Navickas <sarunas.navickas@tokenmill.lt>
2019-01-11 13:53:15 +0200

    Document code a bit

--- nlg-api/src/lt/tokenmill/nlg/generator/planner.clj
+++ nlg-api/src/lt/tokenmill/nlg/generator/planner.clj
@@ -1,28 +1,31 @@
 (ns lt.tokenmill.nlg.generator.planner)
 
-;; Lang functions
 (defn set-subj [selector]  (fn [context data] (assoc context :subj (selector data))))
 (defn set-verb-w-selector [selector] (fn [context data] (assoc context :verb (selector data))))
 (defn set-verb-static [verb] (fn [context _] (assoc context :verb verb)))
 (defn set-obj [selector] (fn [context data] (update context :objs (fn [vals] (conj vals (selector data))))))
 
 (defn compile-attribute-selector
+  "Defines what attribute to select from CSV"
   [value]
   (let [attr-name (value :attribute)]
     (fn [data]
       (get data attr-name))))
 
 (defn compile-static-seq
+  "Builds a list from multiple attributes"
   [value]
   (let [selectors (map compile-attribute-selector (value :attributes))]
     (map #(set-obj %) selectors)))
 
 (defn compile-single
+  "Compiles single attribute"
   [value]
   (let [selector (compile-attribute-selector value)]
     (set-obj selector)))
 
 (defn compile-purpose
+  "it can be either single attribute, or a list (strict order, random order)"
   [purpose]
   (let [rel-name (purpose :relationship)
         value (purpose :value)
@@ -33,11 +36,13 @@
     (conj children (set-verb-static rel-name))))
 
 (defn compile-purposes
+  "purposes - list of relations and elaborations" 
   [purposes]
   (when (not (nil? purposes))
     (map compile-purpose purposes)))
 
 (defn compile-component
+  "Component - Product or Product's Component element"
   [component]
   (let [purposes  (compile-purposes (component :purposes))]
     (concat (list
@@ -46,12 +51,18 @@
             (flatten purposes))))
 
 (defn compile-dp
+  "document-plan - a hashmap representing document plan
+   returns: a list of lists of functions. Each list represents single sentence"
   [document-plan]
   (let [items (document-plan :items)]
     (doall (map compile-component items))))
 
 
-(defn build-dp-instance [dp data]
+(defn build-dp-instance
+  "dp - a hashmap compiled with `compile-dp`
+   data - a flat hashmap (represents CSV)
+   returns: hashmap (context) which will be used to generate text"
+  [dp data]
   (loop [context {:subj nil
                   :objs []
                   :verb nil}
