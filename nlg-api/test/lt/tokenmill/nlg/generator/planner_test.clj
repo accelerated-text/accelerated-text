e3dc246ecd2bbb184edede42d02ce4ea29120480
Šarūnas Navickas <sarunas.navickas@tokenmill.lt>
2019-01-11 13:38:51 +0200

    Handle more complicated examples

--- nlg-api/test/lt/tokenmill/nlg/generator/planner_test.clj
+++ nlg-api/test/lt/tokenmill/nlg/generator/planner_test.clj
@@ -4,13 +4,34 @@
 
 (deftest test-compile-single-node-plan
   (testing "Create a single subject plan"
-    (let [document-plan {:items [{:type "Product"}]}
+    (let [document-plan {:items [{:type "Product" :name {:attribute "product-name"}}]}
           compiled (compile-dp document-plan)]
       (is (not (empty? compiled)))
       (is (= 1 (count compiled)))
       (let [concrete-plan (first compiled)
-            data {:name "TestSubj"}
-            expected {:subj "TestSubj"}]
+            data {"product-name" "TestSubj"}
+            expected {:subj "TestSubj" :verb nil :objs []}]
+        (println "Concrete plan: " concrete-plan)
+        (let [result (build-dp-instance concrete-plan data)]
+          (is (= expected result))))))
+  (testing "Create subject with two features"
+    (let [document-plan {:items [{:type "Product"
+                                  :name {:attribute "product-name"}
+                                  :purposes [{:relationship "provide"
+                                              :value {:type "All"
+                                                      :attributes [{:attribute "main-feature"}
+                                                                   {:attribute "secondary-feature"}]}}
+                                             ]}]}
+          compiled (compile-dp document-plan)]
+      (is (not (empty? compiled)))
+      (is (= 1 (count compiled)))
+      (let [concrete-plan (first compiled)
+            data {"product-name" "Nike Air"
+                  "main-feature" "light"
+                  "secondary-feature" "comfortable"}
+            expected {:subj "Nike Air"
+                      :objs ["light", "comfortable"]
+                      :verb "provide"}]
         (println "Concrete plan: " concrete-plan)
         (let [result (build-dp-instance concrete-plan data)]
           (is (= expected result)))))))
