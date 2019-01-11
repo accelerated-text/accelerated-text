2f0aa3ca48494cbb8a12812692b33d0c2b8a1477
Šarūnas Navickas <sarunas.navickas@tokenmill.lt>
2019-01-11 11:18:49 +0200

    Moving forward

--- nlg-api/src/lt/tokenmill/nlg/generator/planner.clj
+++ nlg-api/src/lt/tokenmill/nlg/generator/planner.clj
@@ -1,11 +1,34 @@
 (ns lt.tokenmill.nlg.generator.planner)
 
 (def set-subj (fn [context data] (assoc context :subj (data :name))))
+(defn set-verb-w-selector [selector] (fn [context data] (assoc context :verb (selector data))))
+(defn set-verb-static [verb] (fn [context _] (assoc context :verb verb)))
+
+(defn compile-attribute [value])
+
+(defn compile-static-seq
+  [value]
+  (map compile-attribute (value :items)))
+
+(defn compile-purpose
+  [purpose]
+  (let [rel-name (purpose :relationship)
+        value (purpose :value)
+        children (case (value :type)
+                   "Attribute" (compile-attribute value)
+                   "All" (compile-static-seq value))]
+    (conj children (set-verb-static rel-name))))
+
+(defn compile-purposes
+  [purposes]
+  (when (not (nil? purposes))
+    (map compile-purpose purposes)))
 
 (defn compile-component
   [component]
-  (let [type (component :type)]
-    (list set-subj)))
+  (let [type (component :type)
+        purposes  (compile-purposes (component :purposes))]
+    (concat (list set-subj) (flatten purposes))))
 
 (defn compile-dp
   [document-plan]
