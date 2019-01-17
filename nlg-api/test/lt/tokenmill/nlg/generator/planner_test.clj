85f4cc1c12b22d4b4d59140ce37583c0df433f5e
Šarūnas Navickas <sarunas.navickas@tokenmill.lt>
2019-01-17 15:20:06 +0200

    Two test cases to try to implement

--- nlg-api/test/lt/tokenmill/nlg/generator/planner_test.clj
+++ nlg-api/test/lt/tokenmill/nlg/generator/planner_test.clj
@@ -63,3 +63,45 @@
                         "Nike Air provides comfort, support and style with sleek update on a classic design. Premium lacing results in a situation where laces never gets into a knot."]]
       (println "Result: " result)
       (is (some #(= % result) expected-any)))))
+
+
+(deftest compile-advanced-case
+  (testing "Testing some different sentence structure"
+    (let [document-plan {:items [{:type "Product"
+                                  :name {:attribute "product-name"}
+                                  :purposes [{:relationship "provide"
+                                              :value {:type "Attribute"
+                                                      :attribute "main-feature"
+                                                      :modifier {:synset-id :good-shoe}
+                                                      :selector {:min 2 :max 4}
+                                                      :elaborate {:type :quote
+                                                                  :quote "lasts as long as you can run"}}}]}
+                                 {:type "Component"
+                                  :name {:attribute "cushioning-name"}
+                                  :purposes [{:relationship "is"
+                                              :value {:type "All"
+                                                      :attributes [{:attribute "quote1"}
+                                                                   {:attribute "quote2"}]}}]}]}
+          data {"product-name" "Nike Odyssey React Men's Running Shoe"
+                "main-feature" "comfort"
+                "cushioning-name" "Nike React foam cushioning"
+                "quote1" "responsive yet lightweight"
+                "quote2" "durable yet soft"}
+          result (render-dp document-plan data)
+          expected "The Nike Odyssey React Men's Running Shoe provides crazy comfort that lasts as long as you can run. Its Nike React foam cushioning is responsive yet lightweight, durable yet soft."]
+      (println "Result: " result)))
+  (testing "Interesting case"
+    (let [document-plan {:items [{:type "Product"
+                                  :quote "Inspired by the look of the late '90s, specifically the shoes featured in the Nike Alpha Project"
+                                  :name {:attribute "product-name"}
+                                  :purposes [{:relationship :provide
+                                              :value {:type :all
+                                                      :attributes [{:attribute "main-feature"}
+                                                                   {:attribute "secondary-feature"}]}
+                                              }]
+                                  }]}
+          data {"product-name" "Nike Zoom 2K"
+                "main-feature" "heritage style"
+                "secondary-feature" "modern comfort of Zoom Air cushioning"}
+          ;; result (render-dp document-plan data)
+          expected "Inspired by the look of the late '90s, specifically the shoes featured in the Nike Alpha Project, the Nike Zoom 2K combines heritage style and the modern comfort of Zoom Air cushioning"])))
