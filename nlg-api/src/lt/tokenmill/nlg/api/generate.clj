846ab2ef7293498bae35b78db85ef878183981d5
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2019-01-18 12:22:02 +0200

    Basic NLG endpoints

--- nlg-api/src/lt/tokenmill/nlg/api/generate.clj
+++ nlg-api/src/lt/tokenmill/nlg/api/generate.clj
@@ -7,9 +7,28 @@
     :name lt.tokenmill.nlg.api.NLGHandler
     :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler]))
 
+
+(defn generate-request [request-body]
+  (let [document-plan-id (request-body :documentPlanId)
+        data-id (request-body :dataId)]
+    {:status 200
+     :body {:result-id "123A"}}))
+
+(defn read-result [path-params]
+  (let [request-id (path-params :id)]
+    {:status 200
+     :body {:ready false}}))
+
 (defn -handleRequest [_ is os _]
   (let [input (utils/decode-body is)
-        body {:result "Still working on it..."}]
+        input (utils/decode-body is)
+        method (input :httpMethod)
+        path-params (input :pathParameters)
+        query-params (input :queryStringParameters)
+        request-body (ch/decode (input :body))
+        {:keys [status body]} (case method
+                                "GET"    (read-result path-params)
+                                "POST"   (generate-request request-body))]
     (log/debugf "Received '%s' and produced output '%s'" input body)
     (with-open [^BufferedWriter w (io/writer os)]
       (.write w ^String (utils/resp 200 body)))))
