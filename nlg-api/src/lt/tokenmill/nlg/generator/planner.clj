a05be93f3be0b5b9eb37d471db30ce70e129765c
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2019-01-18 14:59:55 +0200

    Burning bridges

--- nlg-api/src/lt/tokenmill/nlg/generator/planner.clj
+++ nlg-api/src/lt/tokenmill/nlg/generator/planner.clj
@@ -11,68 +11,6 @@
 (defn set-obj [selector] (fn [context data] (update context :objs (fn [vals] (conj vals (selector data))))))
 (defn set-complement [selector] (fn [context data] (assoc context :complement (selector data))))
 
-(defn node-plus-children [node children] (cons node children))
-
-(defn normalize-context
-  "Build proper context for our segment builder"
-  [context]
-  context)
-
-(defn compile-attribute-selector
-  "Defines what attribute to select from CSV"
-  [value]
-  (let [attr-name (value :attribute)]
-    (fn [data]
-      (get data attr-name))))
-
-(defn compile-random-quote-selector
-  "Selects random attribute from list each time"
-  [value]
-  (let [quotes (map #(% :quote) (value :quotes))]
-    (fn [_] (rand-nth quotes))))
-
-(defn compile-elaborate
-  "Adds an elaboration quote at the end of sentence"
-  [value]
-  (when value
-    (let [type (value :type)
-          selector (case type
-                     "Attribute" (compile-attribute-selector value)
-                     "Any-of" (compile-random-quote-selector value))]
-      (set-complement selector))))
-
-(defn compile-purpose
-  "it can be either single attribute, or a list (strict order, random order)"
-  [purpose]
-  (let [rel-name (purpose :relationship)
-        value (purpose :value)
-        type (value :type)
-        children (case type
-                   "Attribute" (list (set-obj (compile-attribute-selector value)))
-                   "All" (map #(set-obj (compile-attribute-selector %)) (value :attributes))
-                   "Any-of" (list (set-complement (compile-random-quote-selector value))))]
-    (conj children (set-verb-static rel-name))))
-
-(defn compile-purposes
-  "purposes - list of relations and elaborations" 
-  [purposes]
-  (when (not (nil? purposes))
-    (map compile-purpose purposes)))
-
-(defn compile-component
-  "Component - Product or Product's Component element"
-  [component]
-  (let [purposes  (compile-purposes (component :purposes))
-        elaborate (compile-elaborate (component :elaborate))]
-    (concat (remove
-             nil?
-             (list
-              (set-subj
-               (compile-attribute-selector (component :name)))
-              elaborate))
-            (flatten purposes))))
-
-
 (defn load-example-dp
   []
   (with-open [in (io/input-stream (io/resource "dp-example.json"))]
@@ -178,13 +116,18 @@
                                (context :objs))))
          (nlg/add-complement clause (context :complement)))))))
 
+
+(defn render-segment
+  [segment data]
+  (let [instances (map #(build-dp-instance % data) segment)
+        sentences (map generate-sentence instances)]
+    (log/debug "Plans for building string: " segment)
+    (string/join " " sentences)))
+
 (defn render-dp
   "document-plan - a hash map with document plan
    data - a flat hashmap (represents CSV)
    returns: generated text"
   [document-plan data]
-  (let [plans (compile-dp document-plan)
-        instances (map #(build-dp-instance % data) plans)
-        sentences (map generate-sentence instances)]
-    (log/debug "Plans for building string: " plans)
-    (string/join " " sentences)))
+  (let [segments (map #(render-segment % data) parse-document-plan document-plan)]
+    (string/join "" segments)))
