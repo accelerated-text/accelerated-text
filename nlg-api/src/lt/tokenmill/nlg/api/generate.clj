c88c23b24bbf86f043893b85035cd8ae6927affc
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2019-01-18 15:49:28 +0200

    API tweaks

--- nlg-api/src/lt/tokenmill/nlg/api/generate.clj
+++ nlg-api/src/lt/tokenmill/nlg/api/generate.clj
@@ -2,6 +2,7 @@
   (:require [clojure.tools.logging :as log]
             [clojure.java.io :as io]
             [lt.tokenmill.nlg.api.utils :as utils]
+            [lt.tokenmill.nlg.db.dynamo-ops :as ops]
             [cheshire.core :as ch])
   (:import (java.io BufferedWriter))
   (:gen-class
@@ -11,7 +12,8 @@
 
 (defn generate-request [request-body]
   (let [document-plan-id (request-body :documentPlanId)
-        data-id (request-body :dataId)]
+        data-id (request-body :dataId)
+        document-plan (ops/get-workspace document-plan-id)]
     {:status 200
      :body {:resultId "123A"}}))
 
@@ -20,16 +22,21 @@
     {:status 200
      :body {:ready false}}))
 
+(defn delete-result [path-params]
+  (let [request-id (path-params :id)]
+    {:status 200
+     :body {:success true}}))
+
 (defn -handleRequest [_ is os _]
   (let [input (utils/decode-body is)
-        input (utils/decode-body is)
         method (input :httpMethod)
         path-params (input :pathParameters)
         query-params (input :queryStringParameters)
         request-body (ch/decode (input :body))
-        {:keys [status body]} (case method
-                                "GET"    (read-result path-params)
-                                "POST"   (generate-request request-body))]
+        {:keys [status body]} (case (keyword method)
+                                :GET    (read-result path-params)
+                                :DELETE (delete-result path-params)
+                                :POST   (generate-request request-body))]
     (log/debugf "Received '%s' and produced output '%s'" input body)
     (with-open [^BufferedWriter w (io/writer os)]
       (.write w ^String (utils/resp 200 body)))))
