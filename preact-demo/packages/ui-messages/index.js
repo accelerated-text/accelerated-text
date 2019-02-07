c42d311e1044834f0dff566240d3b6cabda26950
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2019-02-07 18:32:15 +0200

    chore: Added index export for ui-messages.

---
+++ preact-demo/packages/ui-messages/index.js
@@ -0,0 +1,4 @@
+export { default as Error }             from './Error';
+export { default as Info }              from './Info';
+export { default as Loading }           from './Loading';
+export { default as UnexpectedWarning } from './UnexpectedWarning';
