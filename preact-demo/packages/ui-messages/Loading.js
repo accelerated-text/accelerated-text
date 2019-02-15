551f0802486779400d7ff35f0de95e61f7d3066d
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2019-02-15 11:07:13 +0200

    fix: Fix ui-messages to display non-text message text.

--- preact-demo/packages/ui-messages/Loading.js
+++ preact-demo/packages/ui-messages/Loading.js
@@ -3,11 +3,16 @@ import { h }            from 'preact';
 import ClockSpinner     from '../clock-spinner/ClockSpinner';
 
 
-export default ({ className, justIcon, message = 'Loading' }) =>
-    <a
-        className={ className }
-        title={ message && message.toString() }
-    >
-        <ClockSpinner />
-        { justIcon ? null : [ ' ', message ] }
-    </a>;
+export default ({ className, justIcon, message = 'Loading' }) => {
+    const text =        message && message.toString();
+
+    return (
+        <a
+            className={ className }
+            title={ text }
+        >
+            <ClockSpinner />
+            { justIcon ? null : [ ' ', text ] }
+        </a>
+    );
+};
