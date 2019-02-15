02335efa3bea1c42259001a0fa2ad810cc45de73
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2019-02-15 10:06:13 +0200

    feat: Switched to polling correct NLG API for results.

--- preact-demo/packages/variants-api/api.js
+++ preact-demo/packages/variants-api/api.js
@@ -1,8 +1,30 @@
 import * as nlgApi      from '../nlg-api/';
 
 
-const PREFIX =          '/document-plans';
+const POLL_INTERVAL =   500;
+const PREFIX =          '/nlg';
 
 
-export const getVariants = planId =>
-    nlgApi.GET( `${ PREFIX }/${ planId }/variants` );
+const checkResult = async ( resultId, resolve, reject ) => {
+
+    const { ready, results } =
+        await nlgApi.GET( `${ PREFIX }/${ resultId }` );
+    if( ready ) {
+        resolve( results );
+    } else {
+        setTimeout( checkResult, POLL_INTERVAL, resultId, resolve, reject );
+    }
+};
+
+
+export const getVariants = async documentId => {
+
+    const { resultId } = await nlgApi.POST( `${ PREFIX }/`, {
+        dataId:     '-1',
+        documentId,
+    });
+
+    return new Promise(( resolve, reject ) =>
+        checkResult( resultId, resolve, reject )
+    );
+};
