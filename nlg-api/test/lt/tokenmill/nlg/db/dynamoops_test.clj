1d7c363e9fae840bdafdf322d6ed0d03a83b8cae
Šarūnas Navickas <sarunas.navickas@tokenmill.lt>
2019-02-19 13:41:35 +0200

    Testing to check our dynamo ops

---
+++ nlg-api/test/lt/tokenmill/nlg/db/dynamoops_test.clj
@@ -0,0 +1,25 @@
+(ns lt.tokenmill.nlg.db.dynamoops_test
+  (:require [clojure.test :refer :all]
+            [lt.tokenmill.nlg.db.dynamo-ops :as ops]
+            [lt.tokenmill.nlg.api.utils :as utils]))
+
+(def mock-db
+  (reify ops/DBAccess
+    (read-item [this key] ())
+    (write-item [this key data]
+      (let [body (-> data
+                     (assoc :key key)
+                     (assoc :createdAt (utils/ts-now))
+                     (assoc :updatedAt (utils/ts-now)))]
+        body))
+    (update-item [this key data] ())
+    (delete-item [this key] ())
+    (list-items [this limit] ())))
+
+(deftest test-write-wo-key
+  (let [db mock-db
+        result (ops/write! db {:something "somedata"})]
+    (is (not (empty? result)))
+    (is (:something result))))
+
+
