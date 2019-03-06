5e17d0632b0191168d8e71cac55b65f78be7b634
Rokas <rokas.ramanauskas@tokenmill.lt>
2019-03-06 17:06:38 +0200

    update search function

--- nlg-api/src/lt/tokenmill/nlg/api/lexicon.clj
+++ nlg-api/src/lt/tokenmill/nlg/api/lexicon.clj
@@ -1,7 +1,8 @@
 (ns lt.tokenmill.nlg.api.lexicon
   (:require [lt.tokenmill.nlg.api.utils :as utils]
             [lt.tokenmill.nlg.db.dynamo-ops :as ops]
-            [lt.tokenmill.nlg.api.resource :as resource])
+            [lt.tokenmill.nlg.api.resource :as resource]
+            [clojure.string :as str])
   (:gen-class
     :name lt.tokenmill.nlg.api.LexiconHandler
     :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler]))
@@ -19,13 +20,17 @@
      :body   {:path-params  path-params
               :request-body request-body}}))
 
-(defn search [query-params path-params]
-  (let [db (get-db)
-        result (ops/read! db (get path-params :query))]
-    {:status 200
-     :body   {:query-params query-params
-              :path-params  path-params
-              :result       result}}))
+
+(defn process-search-response [resp offset limit]
+  resp)
+
+(defn search [{:keys [query offset limit] :as query-params} path-params]
+  (let [db (get-db)]
+    (utils/do-return
+      (comp (fn [resp]
+              (when resp (process-search-response resp offset limit)))
+            (partial ops/scan! db))
+      {:word {:begins-with query}})))
 
 (def -handleRequest
   (resource/build-resource {:put-handler  update
