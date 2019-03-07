ae198115d790095506c019478f02c1b6b220d5e4
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2019-03-07 15:55:15 +0200

    Include compiled JAR

--- nlg-api/local_server.clj
+++ nlg-api/local_server.clj
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-"exec" "clj" "-Sdeps" "{:deps,{http-kit,{:mvn/version\"2.3.0\"}}}" "$0" "$@"
+"exec" "clj" "-Sdeps" "{:deps,{http-kit,{:mvn/version\"2.3.0\"},lt.tokenmill.nlg,{:local/root\"target/uberjar/nlg-api-0.1.0-SNAPSHOT-standalone.jar\"}}}" "$0" "$@"
 
 (ns local-server
-  (:gen-class)
-  (:require [org.httpkit.server :as server]))
+  (:require [org.httpkit.server :as server])
+  (:import (lt.tokenmill.nlg.api DataHandler)))
+
+(defn normalize-req [req] req)
+
+(def dataHandler (new DataHandler))
 
 (defn app [req]
-  {:status  200
-   :headers {"Content-Type" "text/html"}
-   :body    "hello HTTP!"})
+  (let [path (req :uri)]
+    (case path
+      "/data" (.handleRequest dataHandler (normalize-req req)))))
 
 (println "Running server on: localhost:8080. Press Ctrl+C to stop")
 (server/run-server app {:port 8080})
