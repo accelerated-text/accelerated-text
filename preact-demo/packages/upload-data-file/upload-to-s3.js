2e9935efea77a3984c4d8cac8d693b99a4e131d6
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2019-03-13 19:47:14 +0200

    refactor: Moved S3 URL to .env file. Added response status checking in upload-data-file.

--- preact-demo/packages/upload-data-file/upload-to-s3.js
+++ preact-demo/packages/upload-data-file/upload-to-s3.js
@@ -1,16 +1,29 @@
-const BUCKET_URL =  'https://s3.eu-central-1.amazonaws.com/augmented-writer-data-files';
+import unfetch          from 'isomorphic-unfetch';
 
 
-export default fileFromInput => {
+export default async fileFromInput => {
 
-    const fData =   new FormData;
+    const fData =       new FormData;
 
     fData.append( 'key',    fileFromInput.name );
     fData.append( 'file',   fileFromInput );
 
-    return fetch( `${ BUCKET_URL }/`, {
-        method:     'POST',
-        body:       fData,
-        mode:       'cors',
+    const URL =         `${ process.env.DATA_FILES_BUCKET }/`;
+    const response =    await unfetch( URL, {
+        method: 'POST',
+        body:   fData,
+        mode:   'cors',
     });
+
+    if( response.status >= 400 ) {
+        throw Error( response.statusText );
+    } else {
+        const contentType = response.headers.get( 'Content-Type' );
+
+        if( contentType === 'application/json' ) {
+            return await response.json();
+        } else {
+            return await response.blob();
+        }
+    }
 };
