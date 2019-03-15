da6bf179248f48974d0a33f7fb16d0dcc963dc03
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2019-03-15 14:49:06 +0200

    test: Extracted no-records response template for tests.

--- preact-demo/packages/tests/basic.test.js
+++ preact-demo/packages/tests/basic.test.js
@@ -1,9 +1,9 @@
+const noRecords =           require( './response-templates/no-records' );
+
+
 describe( 'basic', () => {
 
-    beforeEach( async () => {
-
-        await page.goto( TEST_URL, { waitUntil: 'load' });
-    }, 10e3 );
+    beforeEach(() => noRecords( page ), 10e3 );
 
     test( 'should render logo', async () => {
 
--- preact-demo/packages/tests/no-records.test.js
+++ preact-demo/packages/tests/no-records.test.js
@@ -1,33 +1,16 @@
-const debugConsole =        require( '../qa-utils/debug-console' );
-const nlgProvide =          require( '../nlg-api/provide-response' );
-const requestInterceptor =  require( '../qa-utils/request-interceptor' );
+const noRecords =           require( './response-templates/no-records' );
+
 
 const { SELECTORS } =       require( './constants' );
 
+
 describe( 'no records', () => {
+
+    beforeEach(() => noRecords( page ), 10e3 );
 
     test( 'should handle empty plan list', async () => {
 
-        debugConsole( page );
-
-        const {
-            continueAll,
-            provideOnce,
-            stopInterception,
-        } = await requestInterceptor( page );
-        const nlgProvideOnce =  nlgProvide( provideOnce );
-
-        continueAll( 'GET', new RegExp( `${ TEST_URL }/.*` ));
-
-        page.goto( TEST_URL );
-
-        await Promise.all([
-            nlgProvideOnce( 'GET', '/data/', []),
-            nlgProvideOnce( 'GET', '/document-plans/', []),
-        ]);
-
         await expect( page ).toMatchElement( SELECTORS.BTN_NEW_PLAN );
 
-        await stopInterception( page );
     }, 10e3 );
 });
