d2362532c8c72df5544b86c17e9aad092fdbd933
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2019-03-20 10:44:34 +0200

    A little step further

--- nlg-api/src/local_server.clj
+++ nlg-api/src/local_server.clj
@@ -1,27 +1,74 @@
 (ns local-server
   (:require [org.httpkit.server :as server]
             [lt.tokenmill.nlg.api.data :as data]
+            [lt.tokenmill.nlg.api.lexicon :as lexicon]
             [cheshire.core :refer :all]
             [clojure.java.io :as io])
   (:gen-class))
 
+
+(defn upper-case-keyword
+  [key]
+  (-> (name key)
+      (clojure.string/upper-case)
+      (keyword)))
+
 (defn normalize-req
-  [req]
-  (let [json-str (generate-string req)]
+  [req path]
+  (println req)
+  (let [headers (:headers req)
+        path-params (:path-params path)
+        query-string (:query-string req)
+        body (:body req)
+        method (:request-method req)
+        normalized {:httpMethod (upper-case-keyword method)
+                    :queryStringParameters query-string
+                    :headers headers
+                    :body body
+                    :pathParams path-params}
+        json-str (generate-string normalized)]
     json-str))
 
+(defonce server (atom nil))
+
+(defn read-os
+  [os]
+  (String. (.toByteArray os) (. java.nio.charset.Charset defaultCharset)))
+
+(defn http-result
+  [raw-resp]
+  (let [resp (decode raw-resp)
+        body (:body resp)]
+      {:status 200
+       :headers {"Content-Type" "application/json"}
+       :body (generate-string body)}))
+
+(defn parse-path
+  [uri]
+  (let [namespace (re-find #"/\w+/?" uri)]
+    {:namespace namespace
+     :path-params {}}))
+
+(defn stop-server []
+  (when-not (nil? @server)
+    ;; graceful shutdown: wait 100ms for existing requests to be finished
+    ;; :timeout is optional, when no timeout, stop immediately
+    (@server :timeout 100)
+    (reset! server nil)))
+
 (defn app [req]
-  (let [path (req :uri)
-        is (io/input-stream (.getBytes (normalize-req req)))
+  (let [path (parse-path (req :uri))
+        is (io/input-stream (.getBytes (normalize-req req path)))
         os (java.io.ByteArrayOutputStream.)]
-    (case path
+    (case (:namespace path)
       "/data" (data/-handleRequest nil is os nil))
-    (-> (slurp os)
-        decode)))
+    
+    (-> (read-os os)
+        (http-result))))
 
 
 (defn -main
   [& args]
   (println "Running server on: localhost:8080. Press Ctrl+C to stop")
-  (server/run-server app {:port 8080}))
+  (reset! server (server/run-server app {:port 8080})))
 
