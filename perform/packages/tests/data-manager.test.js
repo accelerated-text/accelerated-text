dca95357441b64451c74a64dcec91d6aaa01ba2e
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2019-04-19 10:17:32 +0300

    test: Added TODOs for data manager tests.

---
+++ perform/packages/tests/data-manager.test.js
@@ -0,0 +1,14 @@
+import test             from 'ava';
+
+
+test.todo( 'file list visible' );
+test.todo( 'can change file' );
+test.todo( 'no cell table when no file selected' );
+
+test.todo( 'can download file' );
+test.todo( 'can upload file' );
+
+test.todo( 'drag-in blocks for cells' );
+test.todo( 'cell names visible' );
+test.todo( 'cell values visible' );
+test.todo( 'can change cell value row' );
