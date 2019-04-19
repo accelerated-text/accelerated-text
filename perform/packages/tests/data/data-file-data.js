06a441a099d0a6a7c57c6d6b27f18fbe417af6fb
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2019-04-19 10:10:07 +0300

    test: Added data file data to tests.

---
+++ perform/packages/tests/data/data-file-data.js
@@ -0,0 +1,16 @@
+export default {
+    key:    'tests-data-data-file-data-key',
+    data: [{
+        First:  'tests data data file data 0 First',
+        Second:  'tests data data file data 0 Second',
+        Third:  'tests data data file data 0 Third',
+    }, {
+        First:  'tests data data file data 1 First',
+        Second:  'tests data data file data 1 Second',
+        Third:  'tests data data file data 1 Third',
+    }, {
+        First:  'tests data data file data 2 First',
+        Second:  'tests data data file data 2 Second',
+        Third:  'tests data data file data 2 Third',
+    }],
+};
---
+++ perform/packages/tests/data/data-file-list.js
@@ -0,0 +1,10 @@
+import { createDataFile }   from './data-file';
+
+
+export default [
+    createDataFile({ fileName: 'tests-data-data-file-list-fileName-0.csv' }),
+    createDataFile({ fileName: 'tests-data-data-file-list-fileName-1.csv' }),
+    createDataFile({ fileName: 'tests-data-data-file-list-fileName-2.csv' }),
+    createDataFile({ fileName: 'tests-data-data-file-list-fileName-3.csv' }),
+    createDataFile({ fileName: 'tests-data-data-file-list-fileName-4.csv' }),
+];
