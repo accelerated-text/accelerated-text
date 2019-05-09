081fe45e75b81b9a57b6445cd60879db42c8f93a
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2019-05-09 15:02:09 +0300

    test: Created function for generating data file data in tests.

--- perform/packages/tests/data/data-file-data.js
+++ perform/packages/tests/data/data-file-data.js
@@ -1,16 +1,17 @@
-export default {
-    key:    'tests-data-data-file-data-key',
-    data: [{
-        First:  'tests data data file data 0 First',
-        Second:  'tests data data file data 0 Second',
-        Third:  'tests data data file data 0 Third',
-    }, {
-        First:  'tests data data file data 1 First',
-        Second:  'tests data data file data 1 Second',
-        Third:  'tests data data file data 1 Third',
-    }, {
-        First:  'tests data data file data 2 First',
-        Second:  'tests data data file data 2 Second',
-        Third:  'tests data data file data 2 Third',
-    }],
-};
+import { range }        from 'ramda';
+
+
+export const createDataFileData = ({
+    prefix =    'tests-data-data-file-data',
+    rowCount =  3,
+}) => ({
+    key:    `${ prefix }-key`,
+    data:   range( 0, rowCount ).map( row => ({
+        First:  `${ prefix } row ${ row } First`,
+        Second: `${ prefix } row ${ row } Second`,
+        Third:  `${ prefix } row ${ row } Third`,
+    })),
+});
+
+
+export default createDataFileData({});
