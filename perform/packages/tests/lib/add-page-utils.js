5c0e49412710f8524575719e8a2628b3f0837d08
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2019-05-09 16:35:34 +0300

    test: Added more page utils.

--- perform/packages/tests/lib/add-page-utils.js
+++ perform/packages/tests/lib/add-page-utils.js
@@ -6,10 +6,48 @@ const SELECTOR_WAIT_OPTIONS = {
 export default ( t, run, ...args ) =>
     run(
         Object.assign( t, {
+
             findElement: ( selector, page = t.page ) =>
                 t.notThrowsAsync( page.waitForSelector( selector, SELECTOR_WAIT_OPTIONS )),
+
+            getElementAttribute: ( selector, attributeName, page = t.page ) =>
+                t.page.evaluate(
+                    ( selector, attributeName ) =>
+                        document.querySelector( selector ).getAttribute( attributeName ),
+                    selector,
+                    attributeName,
+                ),
+
+            getElementProperty: ( selector, propertyName, page = t.page ) =>
+                t.page.evaluate(
+                    ( selector, propertyName ) =>
+                        document.querySelector( selector )[propertyName],
+                    selector,
+                    propertyName,
+                ),
+
+            getElementValue: ( selector, page = t.page ) =>
+                t.page.evaluate(
+                    selector => document.querySelector( selector ).value,
+                    selector,
+                ),
+
             notFindElement: ( selector, page = t.page ) =>
                 t.throwsAsync( page.waitForSelector( selector, SELECTOR_WAIT_OPTIONS )),
+
+            waitUntilElementGone: async ( selector, timeout = 10e3, page = t.page ) => {
+
+                const timeoutTimestamp =    +new Date + timeout;
+
+                while( +new Date < timeoutTimestamp ) {
+                    try {
+                        await page.waitForSelector( selector, { timeout: 500 });
+                    } catch( _ ) {
+                        return;
+                    }
+                }
+                throw Error( `Timeout (${ timeout }ms) exceeded while waiting for selector ${ selector }` );
+            },
         }),
         ...args,
     );
