026e7f86a2691ad24b5ceed78b99420eea951b98
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2019-05-20 13:11:18 +0000

    Resolve "Reduce lambda wakeup time to <0.5s"

--- nlg-api/test/lt/tokenmill/nlg/api/util_test.clj
+++ nlg-api/test/lt/tokenmill/nlg/api/util_test.clj
@@ -2,7 +2,6 @@
   (:require [clojure.test :refer :all]
             [lt.tokenmill.nlg.api.utils :refer :all]))
 
-
 (deftest test-read-stub-csv
   (let [data (read-stub-csv)
         first-row (first data)]
