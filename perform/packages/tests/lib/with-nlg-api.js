a6ae2eb37a58e5a52c2b6ed6be2e1dcc8ea61881
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2019-05-20 18:48:42 +0300

    test: Added more functions to nlgApi test macro.

--- perform/packages/tests/lib/with-nlg-api.js
+++ perform/packages/tests/lib/with-nlg-api.js
@@ -7,20 +7,34 @@ export const NLG_API_CORS_HEADERS = {
 export const nlgApiUrl = url =>
     `${ process.env.NLG_API_URL }${ url }`;
 
+export const nlgApiRespond = ( request, body ) =>
+    request.respond({
+        status:         200,
+        headers:        NLG_API_CORS_HEADERS,
+        contentType:    'application/json',
+        body: (
+            typeof( body ) === 'string'
+                ? body
+                : JSON.stringify( body )
+        ),
+    });
+
 export const nlgApiEchoBody = interceptFn => ( method, url, bodyFn = null ) =>
     interceptFn(
         method,
         nlgApiUrl( url ),
-        request => request.respond({
-            status:         200,
-            headers:        NLG_API_CORS_HEADERS,
-            contentType:    'application/json',
-            body: (
-                bodyFn
-                    ? JSON.stringify( bodyFn( JSON.parse( request.postData())))
-                    : request.postData()
-            ),
-        }),
+        request => nlgApiRespond( request,
+            bodyFn
+                ? JSON.stringify( bodyFn( JSON.parse( request.postData())))
+                : request.postData()
+        ),
+    );
+
+export const nlgApiIntercept = interceptFn => ( method, url, onRequestFn ) =>
+    interceptFn(
+        method,
+        nlgApiUrl( url ),
+        onRequestFn,
     );
 
 export const nlgApiProvide = provideFn => ( method, url, body, status, headers ) =>
@@ -39,9 +53,13 @@ export default async ( t, run, ...args ) =>
                 echoBody:       nlgApiEchoBody( t.interceptor.intercept ),
                 echoBodyAll:    nlgApiEchoBody( t.interceptor.interceptAll ),
                 echoBodyOnce:   nlgApiEchoBody( t.interceptor.interceptOnce ),
+                intercept:      nlgApiIntercept( t.interceptor.intercept ),
+                interceptAll:   nlgApiIntercept( t.interceptor.interceptAll ),
+                interceptOnce:  nlgApiIntercept( t.interceptor.interceptOnce ),
                 provide:        nlgApiProvide( t.interceptor.provide ),
                 provideAll:     nlgApiProvide( t.interceptor.provideAll ),
                 provideOnce:    nlgApiProvide( t.interceptor.provideOnce ),
+                respond:        nlgApiRespond,
             },
         }),
         ...args,
