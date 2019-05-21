4360bd973bb7fd383a0ae4a818e30330567623c7
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2019-05-21 12:56:07 +0300

    test: Implemented tests for editing Lexicon item.

--- perform/packages/tests/lexicon.test.js
+++ perform/packages/tests/lexicon.test.js
@@ -2,27 +2,19 @@ import test                 from 'ava';
 
 import {
     areLexiconItemsVisible,
+    createItemSelectors,
     getLexiconSearchUrl,
 }   from './lib/lexicon-utils';
 import {
     createLexiconItem,
+    default as LEXICON_LIST,
 }   from './data/lexicon-list';
 import defaultResponsesPage from './lib/default-responses-page';
 import noRecordsPage        from './lib/no-records-page';
 import { SELECTORS }        from './constants';
 
 
-const S_NEW_ITEM = {
-    EDIT:           `${ SELECTORS.LEXICON_NEW_ITEM } ${ SELECTORS.LEXICON_EDIT }`,
-    EDIT_CANCEL:    `${ SELECTORS.LEXICON_NEW_ITEM } ${ SELECTORS.LEXICON_EDIT_CANCEL }`,
-    EDIT_SAVE:      `${ SELECTORS.LEXICON_NEW_ITEM } ${ SELECTORS.LEXICON_EDIT_SAVE }`,
-    EDIT_TEXT:      `${ SELECTORS.LEXICON_NEW_ITEM } ${ SELECTORS.LEXICON_EDIT_TEXT }`,
-    EDIT_ERROR:     `${ SELECTORS.LEXICON_NEW_ITEM } ${ SELECTORS.UI_ERROR }`,
-    EDIT_LOADING:   `${ SELECTORS.LEXICON_NEW_ITEM } ${ SELECTORS.UI_LOADING }`,
-    ITEM_BLOCK:     `${ SELECTORS.LEXICON_NEW_ITEM } ${ SELECTORS.LEXICON_ITEM_BLOCK }`,
-    ITEM_ID:        `${ SELECTORS.LEXICON_NEW_ITEM } ${ SELECTORS.LEXICON_ITEM_ID }`,
-    ITEM_PHRASES:   `${ SELECTORS.LEXICON_NEW_ITEM } ${ SELECTORS.LEXICON_ITEM_PHRASES }`,
-};
+const SEL_NEW_ITEM =        createItemSelectors( SELECTORS.LEXICON_NEW_ITEM );
 
 
 test( 'no records', noRecordsPage, async t => {
@@ -52,6 +44,8 @@ test( 'default list', defaultResponsesPage, async t => {
     await t.findElement( SELECTORS.LEXICON_ITEM );
     await t.findElement( SELECTORS.LEXICON_MORE );
     await t.notFindElement( SELECTORS.LEXICON_NEW_ITEM );
+
+    await areLexiconItemsVisible( t, LEXICON_LIST.items );
 });
 
 
@@ -60,7 +54,7 @@ test( 'search works', noRecordsPage, async t => {
 
     const query =       'test';
 
-    t.page.type( SELECTORS.LEXICON_SEARCH, query, { delay: 10 });
+    t.page.type( SELECTORS.LEXICON_SEARCH, query );
 
     let results = {
         limit:          4,
@@ -103,7 +97,7 @@ test( 'loading more items works', noRecordsPage, async t => {
 
     const query =       'test';
 
-    t.page.type( SELECTORS.LEXICON_SEARCH, query, { delay: 10 });
+    t.page.type( SELECTORS.LEXICON_SEARCH, query );
 
     const results0 = {
         limit:          1,
@@ -164,23 +158,23 @@ test( 'add new item form', noRecordsPage, async t => {
     await t.page.click( SELECTORS.LEXICON_NEW_BTN );
 
     await t.findElement( SELECTORS.LEXICON_NEW_ITEM );
-    await t.findElement( S_NEW_ITEM.EDIT );
-    await t.findElement( S_NEW_ITEM.EDIT_TEXT );
-    await t.findElement( S_NEW_ITEM.EDIT_SAVE );
-    await t.findElement( S_NEW_ITEM.EDIT_CANCEL );
+    await t.findElement( SEL_NEW_ITEM.EDIT );
+    await t.findElement( SEL_NEW_ITEM.EDIT_TEXT );
+    await t.findElement( SEL_NEW_ITEM.EDIT_SAVE );
+    await t.findElement( SEL_NEW_ITEM.EDIT_CANCEL );
 });
 
 test( 'add item cancel works', noRecordsPage, async t => {
     t.timeout( 5e3 );
 
     await t.page.click( SELECTORS.LEXICON_NEW_BTN );
-    await t.page.click( S_NEW_ITEM.EDIT_CANCEL );
+    await t.page.click( SEL_NEW_ITEM.EDIT_CANCEL );
 
     await t.notFindElement( SELECTORS.LEXICON_NEW_ITEM );
-    await t.notFindElement( S_NEW_ITEM.EDIT );
-    await t.notFindElement( S_NEW_ITEM.EDIT_TEXT );
-    await t.notFindElement( S_NEW_ITEM.EDIT_SAVE );
-    await t.notFindElement( S_NEW_ITEM.EDIT_CANCEL );
+    await t.notFindElement( SEL_NEW_ITEM.EDIT );
+    await t.notFindElement( SEL_NEW_ITEM.EDIT_TEXT );
+    await t.notFindElement( SEL_NEW_ITEM.EDIT_SAVE );
+    await t.notFindElement( SEL_NEW_ITEM.EDIT_CANCEL );
 });
 
 
@@ -192,12 +186,12 @@ test( 'add item save works', noRecordsPage, async t => {
     await t.page.click( SELECTORS.LEXICON_NEW_BTN );
 
     t.is(
-        await t.getElementProperty( S_NEW_ITEM.ITEM_ID, 'innerText' ),
+        await t.getElementText( SEL_NEW_ITEM.ITEM_ID ),
         ''
     );
 
-    await t.page.type( S_NEW_ITEM.EDIT_TEXT, item.synonyms.join( '\n' ));
-    t.page.click( S_NEW_ITEM.EDIT_SAVE );
+    await t.page.type( SEL_NEW_ITEM.EDIT_TEXT, item.synonyms.join( '\n' ));
+    t.page.click( SEL_NEW_ITEM.EDIT_SAVE );
 
     await t.nlgApi.interceptOnce( 'POST', '/lexicon/', request => {
         t.is(
@@ -209,14 +203,85 @@ test( 'add item save works', noRecordsPage, async t => {
     await t.waitUntilElementGone( SELECTORS.UI_LOADING );
 
     t.is(
-        await t.getElementProperty( S_NEW_ITEM.ITEM_ID, 'innerText' ),
+        await t.getElementText( SEL_NEW_ITEM.ITEM_ID ),
         item.key,
     );
     t.is(
-        await t.getElementProperty( S_NEW_ITEM.ITEM_PHRASES, 'innerText' ),
+        await t.getElementText( SEL_NEW_ITEM.ITEM_PHRASES ),
         item.synonyms.join( '' ),
     );
 });
 
 
-test.todo( 'editing item works' );
+test.todo( 'add item errors handled' );
+
+
+test( 'edit item form', defaultResponsesPage, async t => {
+    t.timeout( 5e3 );
+
+    const item =        LEXICON_LIST.items[0];
+    const SEL_ITEM =    createItemSelectors( `${ SELECTORS.LEXICON_ITEM }:first-child` );
+
+    t.page.click( SEL_ITEM.ITEM_PHRASES );
+
+    await t.findElement( SEL_ITEM.EDIT );
+    await t.findElement( SEL_ITEM.EDIT_TEXT );
+    await t.findElement( SEL_ITEM.EDIT_SAVE );
+    await t.findElement( SEL_ITEM.EDIT_CANCEL );
+
+    t.is(
+        await t.getElementValue( SEL_ITEM.EDIT_TEXT ),
+        item.synonyms.join( '\n' ),
+    );
+});
+
+
+test( 'edit item cancel works', defaultResponsesPage, async t => {
+    t.timeout( 5e3 );
+
+    const SEL_ITEM =    createItemSelectors( `${ SELECTORS.LEXICON_ITEM }:nth-child(2)` );
+
+    await t.page.click( SEL_ITEM.ITEM_PHRASES );
+    await t.page.click( SEL_ITEM.EDIT_CANCEL );
+
+    await t.notFindElement( SEL_ITEM.EDIT );
+    await t.notFindElement( SEL_ITEM.EDIT_TEXT );
+    await t.notFindElement( SEL_ITEM.EDIT_SAVE );
+    await t.notFindElement( SEL_ITEM.EDIT_CANCEL );
+});
+
+
+test( 'edit item save works', defaultResponsesPage, async t => {
+    t.timeout( 5e3 );
+
+    const item =        LEXICON_LIST.items[2];
+    const newItem = {
+        ...item,
+        synonyms:       [ t.title, 1, 2, 3, 4, 5 ],
+    };
+    const itemUrl =     `/lexicon/${ encodeURIComponent( item.key )}`;
+    const SEL_ITEM =    createItemSelectors( `${ SELECTORS.LEXICON_ITEM }:nth-child(3)` );
+
+    await t.page.click( SEL_ITEM.ITEM_PHRASES );
+    await t.clearInput( SEL_ITEM.EDIT_TEXT );
+    await t.page.type( SEL_ITEM.EDIT_TEXT, newItem.synonyms.join( '\n' ));
+
+    t.page.click( SEL_ITEM.EDIT_SAVE );
+
+    await t.nlgApi.provideOnce( 'OPTIONS', itemUrl, '' );
+    await t.nlgApi.provideOnce( 'PUT', itemUrl, newItem );
+    await t.waitUntilElementGone( SEL_ITEM.EDIT_LOADING );
+
+    t.is(
+        await t.getElementText( SEL_ITEM.ITEM_ID ),
+        item.key,
+    );
+    t.is(
+        await t.getElementText( SEL_ITEM.ITEM_PHRASES ),
+        newItem.synonyms.join( '' ),
+    );
+});
+
+test.todo( 'edit item errors handled' );
+
+test.todo( 'disabled editing while loading' );
--- perform/packages/tests/lib/lexicon-utils.js
+++ perform/packages/tests/lib/lexicon-utils.js
@@ -1,6 +1,41 @@
 import { SELECTORS }        from '../constants';
 
 
+export const areLexiconItemsVisible = async ( t, items ) => {
+
+    await t.resetMouse();
+
+    for( let i = 0; i < items.length; i += 1 ) {
+
+        const item =            items[i];
+        const itemSelector =    `${ SELECTORS.LEXICON_ITEM }:nth-child(${ i + 1 })`;
+
+        t.is(
+            await t.getElementText( `${ itemSelector } ${ SELECTORS.LEXICON_ITEM_ID }` ),
+            item.key,
+        );
+
+        t.is(
+            await t.getElementText( `${ itemSelector } ${ SELECTORS.LEXICON_ITEM_PHRASES }` ),
+            item.synonyms.join( '' ),
+        );
+    }
+};
+
+
+export const createItemSelectors = itemSelector => ({
+    EDIT:           `${ itemSelector } ${ SELECTORS.LEXICON_EDIT }`,
+    EDIT_CANCEL:    `${ itemSelector } ${ SELECTORS.LEXICON_EDIT_CANCEL }`,
+    EDIT_SAVE:      `${ itemSelector } ${ SELECTORS.LEXICON_EDIT_SAVE }`,
+    EDIT_TEXT:      `${ itemSelector } ${ SELECTORS.LEXICON_EDIT_TEXT }`,
+    EDIT_ERROR:     `${ itemSelector } ${ SELECTORS.UI_ERROR }`,
+    EDIT_LOADING:   `${ itemSelector } ${ SELECTORS.UI_LOADING }`,
+    ITEM_BLOCK:     `${ itemSelector } ${ SELECTORS.LEXICON_ITEM_BLOCK }`,
+    ITEM_ID:        `${ itemSelector } ${ SELECTORS.LEXICON_ITEM_ID }`,
+    ITEM_PHRASES:   `${ itemSelector } ${ SELECTORS.LEXICON_ITEM_PHRASES }`,
+});
+
+
 export const getLexiconSearchUrl = ({ query = '', offset = 0 }) => {
 
     const params =  new URLSearchParams();
@@ -12,32 +47,4 @@ export const getLexiconSearchUrl = ({ query = '', offset = 0 }) => {
     }
 
     return '/lexicon?' + params.toString();
-};
-
-
-export const areLexiconItemsVisible = async ( t, items ) => {
-
-    await t.resetMouse();
-
-    for( let i = 0; i < items.length; i += 1 ) {
-
-        const item =            items[i];
-        const itemSelector =    `${ SELECTORS.LEXICON_ITEM }:nth-child(${ i + 1 })`;
-
-        t.is(
-            await t.getElementProperty(
-                `${ itemSelector } ${ SELECTORS.LEXICON_ITEM_ID }`,
-                'innerText',
-            ),
-            item.key,
-        );
-
-        t.is(
-            await t.getElementProperty(
-                `${ itemSelector } ${ SELECTORS.LEXICON_ITEM_PHRASES }`,
-                'innerText',
-            ),
-            item.synonyms.join( '' ),
-        );
-    }
 };
