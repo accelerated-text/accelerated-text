a084cacf1629c5a53a5ee90154a8accf44abbcba
Justas <justinas.seskevicius@tokenmill.lt>
2019-05-21 17:44:26 +0300

    refactor: add lexicon to schema with custom scalar

--- graphql-api/src/graphql/core.clj
+++ graphql-api/src/graphql/core.clj
@@ -2,44 +2,28 @@
   (:require [clojure.tools.logging :as log]
             [clojure.java.io :as io]
             [clojure.edn :as edn]
-            [com.walmartlabs.lacinia.util :refer [attach-resolvers]]
+            [nlg.lexicon :as lexicon]
+            [com.walmartlabs.lacinia.util :as util]
             [com.walmartlabs.lacinia.schema :as schema]
-            [com.walmartlabs.lacinia :refer [execute]]))
+            [com.walmartlabs.lacinia :refer [execute]])
+  (:import (clojure.lang BigInt)))
 
-(defn get-hero [context arguments value]
-  (let [{:keys [episode]} arguments]
-    (if (= episode :NEWHOPE)
-      {:id "1000"
-       :name "Luke"
-       :home_planet "Tatooine"
-       :appears_in ["NEWHOPE" "EMPIRE" "JEDI"]}
-      {:id "2000"
-       :name "Lando Calrissian"
-       :home_planet "Socorro"
-       :appears_in ["EMPIRE" "JEDI"]})))
+(def transforms
+  {:big-int-parser     #(when (= BigInt (type %)) %)
+   :big-int-serializer #(when (= BigInt (type %)) %)})
 
-(defn get-droid [context arguments value]
-  (if (= (:id arguments) "2001")
-    {:id 2001
-     :name "droid 2k1"
-     :appears_in ["EMPIRE" "NEWHOPE"]
-     :primary_functions ["zapping" "hacking" "beeping"]}
-    {:id 666
-     :name "devil droid"
-     :appears_in ["EMPIRE"]
-     :primary_functions ["zapping" "hacking" "killing"]}))
+(defn get-lexicon [context arguments value]
+  (:body (lexicon/search arguments nil)))
 
-(def star-wars-schema
+(def nlg-schema
   (-> "schema.edn"
       (io/resource)
       slurp
       edn/read-string
-      (attach-resolvers {:get-hero get-hero
-                         :get-droid get-droid
-                         ;:friends (constantly {})
-                         :get-human get-hero})
+      (util/attach-scalar-transformers transforms)
+      (util/attach-resolvers {:get-lexicon get-lexicon})
       schema/compile))
 
 (defn nlg [request]
   (log/info "The request is: %s" request)
-  (execute star-wars-schema "{\n  hero {\n    id\n    name\n  }\n}" nil nil))
+  (execute nlg-schema "{\n  hero {\n    id\n    name\n  }\n}" nil nil))
