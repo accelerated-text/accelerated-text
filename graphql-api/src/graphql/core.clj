3ddf872015eaeb8b6906530b965caf9c0b0b01bb
Justas <justinas.seskevicius@tokenmill.lt>
2019-05-22 16:50:34 +0300

    refactor: code style & Cors header support

--- graphql-api/src/graphql/core.clj
+++ graphql-api/src/graphql/core.clj
@@ -5,25 +5,22 @@
             [nlg.lexicon :as lexicon]
             [com.walmartlabs.lacinia.util :as util]
             [com.walmartlabs.lacinia.schema :as schema]
-            [com.walmartlabs.lacinia :refer [execute]])
-  (:import (clojure.lang BigInt)))
-
-(def transforms
-  {:big-int-parser     #(when (= BigInt (type %)) %)
-   :big-int-serializer #(when (= BigInt (type %)) %)})
+            [com.walmartlabs.lacinia :refer [execute]]))
 
 (defn get-lexicon [context arguments value]
-  (:body (lexicon/search arguments nil)))
+  (log/infof "'get-lexicon' arguments -> '%s'" arguments)
+  (let [result (lexicon/search arguments nil)]
+    (log/infof "Lexicon search results -> '%s'" result)
+    (:body result)))
 
 (def nlg-schema
   (-> "schema.edn"
       (io/resource)
       slurp
       edn/read-string
-      (util/attach-scalar-transformers transforms)
       (util/attach-resolvers {:get-lexicon get-lexicon})
       schema/compile))
 
-(defn nlg [request]
+(defn nlg [{:keys [query variables context] :as request}]
   (log/info "The request is: %s" request)
-  (execute nlg-schema "{\n  hero {\n    id\n    name\n  }\n}" nil nil))
+  (execute nlg-schema query variables context))
--- graphql-api/src/nlg/utils.clj
+++ graphql-api/src/nlg/utils.clj
@@ -4,7 +4,6 @@
             [clojure.tools.logging :as log]
             [clj-time.core :as time]
             [clj-time.coerce :as tc]
-            [clojure.string :as string]
             [clojure.data.csv :as csv])
   (:import (java.io InputStream)
            (java.util UUID)))
@@ -13,28 +12,28 @@
 
 (defn ts-now [] (tc/to-long (time/now)))
 
+(defn stack-trace [e]
+  (reduce #(str %1 "\n" %2) (.getStackTrace ^Exception e)))
+
+(defn add-body [resp body]
+  (if resp
+    (assoc resp :body (cheshire/encode body))
+    (assoc resp :body "")))
+
+(defn add-headers [resp body]
+  (let [cors-headers {"Access-Control-Allow-Origin"  "*"
+                      "Access-Control-Allow-Methods" "GET, POST, PUT, DELETE, OPTIONS"}
+        headers (if body
+                  (conj cors-headers {"Content-Type" "application/json"})
+                  cors-headers)]
+    (assoc resp :headers headers)))
+
 (defn resp [status-code body]
-  (let [resp {"statusCode" status-code
+  (let [resp {"statusCode"      status-code
               "isBase64Encoded" false}]
-    (defn add-body
-      [r]
-      (if body
-        (assoc r :body (cheshire/encode body))
-        (assoc r :body "")))
-
-    (defn add-headers
-      [r]
-      (let [cors-headers {"Access-Control-Allow-Origin" "*"
-                          "Access-Control-Allow-Methods" "GET, POST, PUT, DELETE, OPTIONS"}
-            headers (if body
-                      (conj cors-headers ["Content-Type" "application/json"])
-                      cors-headers)]
-        (assoc r :headers headers)))
-
-
     (-> resp
-        (add-body)
-        (add-headers)
+        (add-body body)
+        (add-headers body)
         (cheshire/encode))))
 
 (defn decode-body [^InputStream is]
@@ -43,99 +42,89 @@
         (io/reader)
         (cheshire/decode-stream true))
     (catch Exception e
-      (log/errorf "Failed to decode the body with exception '%s'" e))))
+      (log/errorf "Failed to decode the body with exception '%s' \n %s" (.getMessage e) (stack-trace e)))))
 
-(defn read-stub-json
-  []
+(defn read-stub-json []
   (cheshire/decode (slurp (io/resource "stub.json"))))
 
-(defn get-stack-trace
-  [e]
-  (string/join "\n" (map str (.getStackTrace e))))
-
-(defn result-or-error
-  [results]
+(defn result-or-error [results]
   (try
     (doall results)
     (catch Exception e
-      (do
-        (log/errorf "Failed to get result: %s" (get-stack-trace e))
-        {:error true
-         :ready true
-         :message (.getMessage e)}))))
+      (log/errorf "Failed to get result: %s \n %s" (.getMessage e) (stack-trace e))
+      {:error   true
+       :ready   true
+       :message (.getMessage e)})))
 
 (defn zip [coll1 coll2]
   (interleave coll1 coll2))
 
-(defn csv-to-map
-  [f]
+(defn csv-to-map [f]
   (let [raw-csv (csv/read-csv f)]
     (log/debug "Raw CSV: " raw-csv)
-    (let [header (vec  (->> (first raw-csv)
-                            (map keyword)))
+    (let [header (vec (->> (first raw-csv)
+                           (map keyword)))
           data (rest raw-csv)
           pairs (map #(zip header %) data)]
       (log/debugf "Header: %s" header)
       (doall (map #(apply array-map %) pairs)))))
 
-(defn read-stub-csv
-  []
+(defn read-stub-csv []
   (let [data (slurp (io/resource "data-example.csv"))]
     (doall
       (csv-to-map data))))
 
-(defn do-return
-  [func & args]
-  (try (let [resp (apply func args)]
-         (if resp
-           (if (contains? resp :error)
-             {:status 500
-              :body {:error true :message "ERROR_01"}}
-             {:status 200
-              :body resp})
+(defn do-return [func & args]
+  (try
+    (let [resp (apply func args)]
+      (if resp
+        (if (contains? resp :error)
+          {:status 500
+           :body   {:error true :message "ERROR_01"}}
+          {:status 200
+           :body   resp})
+        {:status 404}))
+    (catch Exception e
+      (log/errorf "Exception caught in utils/do-return '%s' \n %s" (.getMessage e) (stack-trace e))
+      {:status 500
+       :body   {:error   true
+                :message (.getMessage e)}})))
 
-           {:status 404}))
-       (catch Exception e (do
-                            (log/error (get-stack-trace e))
-                            {:status 500
-                             :body {:error true
-                                    :message (.getMessage e)}}))))
+(defn do-insert [func & args]
+  (try
+    (let [id (gen-uuid)
+          insert-fn (partial func id)
+          resp (apply insert-fn args)]
+      {:status 200
+       :body   resp})
+    (catch Exception e
+      (log/errorf "Exception caught in utils/do-insert '%s' \n %s" (.getMessage e) (stack-trace e))
+      {:status 500
+       :body   {:error   true
+                :message (.getMessage e)}})))
 
-(defn do-insert
-  [func & args]
-  (let [id (gen-uuid)
-        insert-fn (partial func id)]
-    (try (let [resp (apply insert-fn args)]
-           {:status 200
-            :body resp})
-         (catch Exception e (do
-                              (log/error (get-stack-trace e))
-                              {:status 500
-                               :body {:error true
-                                      :message (.getMessage e)}})))))
+(defn do-delete [search-fn delete-fn & args]
+  (try
+    (let [original (apply search-fn args)]
+      (apply delete-fn args)
+      {:status 200
+       :body   original})
+    (catch Exception e
+      (log/errorf "Exception caught in utils/do-delete '%s' \n %s" (.getMessage e) (stack-trace e))
+      {:status 500
+       :body   {:error   true
+                :message (.getMessage e)}})))
 
-(defn do-delete
-  [search-fn delete-fn & args]
-  (try (let [original (apply search-fn args)
-             _ (apply delete-fn args)]
-         {:status 200
-          :body original})
-       (catch Exception e (do
-                            (log/error e)
-                            {:status 500
-                             :body {:error true
-                                    :message (.getMessage e)}}))))
-
-(defn do-update
-  [func & args]
-  (try (let [resp (apply func args)]
-         {:status 200
-          :body resp})
-       (catch Exception e (do
-                            (log/error e)
-                            {:status 500
-                             :body {:error true
-                                    :message (.getMessage e)}}))))
+(defn do-update [func & args]
+  (try
+    (let [resp (apply func args)]
+      {:status 200
+       :body   resp})
+    (catch Exception e
+      (log/errorf "Exception caught in utils/do-update '%s' \n %s" (.getMessage e) (stack-trace e))
+      {:status 500
+       :body   {:error   true
+                :message (.getMessage e)}})))
 
 (defn add-status [resp-vec]
   {:status (if (every? #(= 200 (get % :status)) resp-vec) 200 500)
