9abc85c688f32befaaa7264087582486972eaedd
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2019-05-30 18:19:00 +0300

    chore: Created resolvers for the new queries.

--- perform/packages/graphql/resolvers.js
+++ perform/packages/graphql/resolvers.js
@@ -1,3 +1,40 @@
+import {
+    flatten,
+    keys,
+    values,
+}   from 'ramda';
+
+
+const READER_FLAGS = [ 'junior', 'senior' ];
+
+const SYNONYMS = {
+    examine:       [ 'audit', 'check', 'check out', 'consider', 'criticize', 'delve into', 'explore', 'inspect', 'investigate', 'ponder', 'pore over', 'probe', 'read', 'research', 'review', 'scan', 'screen', 'scrutinize', 'study', 'survey', 'try', 'vet', 'view', 'appraise', 'assay', 'canvass', 'case', 'eye', 'finger', 'frisk', 'gun', 'inquire', 'parse', 'peruse', 'prospect', 'prove', 'reconnoiter', 'sift', 'sweep', 'weigh', 'winnow', 'chew over', 'dig into', 'go into', 'go over', 'go through', 'look over', 'look see', 'pat down', 'pick at', 'scope', 'scrutinate', 'search into', 'size up', 'take stock of', 'turn over' ],
+    look:           [ 'consider', 'glance', 'notice', 'peer', 'read', 'see', 'stare', 'study', 'view', 'watch', 'admire', 'attend', 'behold', 'beware', 'contemplate', 'eye', 'flash', 'focus', 'gape', 'gawk', 'gaze', 'glower', 'goggle', 'heed', 'inspect', 'mark', 'mind', 'note', 'observe', 'ogle', 'peep', 'regard', 'rubberneck', 'scan', 'scout', 'scrutinize', 'spot', 'spy', 'survey', 'tend', 'feast one\'s eyes', 'get a load of', 'pore over', 'take a gander', 'take in the sights' ],
+    notice:         [ 'acknowledge', 'catch', 'detect', 'discern', 'look at', 'note', 'recognize', 'regard', 'see', 'spot', 'advert', 'allude', 'clock', 'descry', 'dig', 'distinguish', 'espy', 'heed', 'mark', 'mind', 'refer', 'remark', 'flash on', 'get a load of', 'make out', 'pick up on', 'take in' ],
+    observe:        [ 'detect', 'discover', 'examine', 'inspect', 'look at', 'mark', 'monitor', 'note', 'pay attention to', 'recognize', 'regard', 'scrutinize', 'study', 'view', 'watch', 'witness', 'beam', 'behold', 'catch', 'contemplate', 'dig', 'discern', 'distinguish', 'espy', 'eyeball', 'flash', 'mind', 'perceive', 'read', 'spot', 'spy', 'survey', 'eagle-eye', 'get a load of', 'get an eyeful of', 'keep one\'s eye on', 'lamp', 'make out', 'pick up on', 'take in' ],
+    see:            [ 'detect examine', 'identify', 'look', 'look at', 'notice', 'observe', 'recognize', 'regard', 'spot', 'view', 'watch', 'witness', 'beam', 'behold', 'clock', 'contemplate', 'descry', 'discern', 'distinguish', 'espy', 'eye', 'flash', 'gape', 'gawk', 'gaze', 'glare', 'glimpse', 'heed', 'inspect', 'mark', 'mind', 'note', 'peek', 'peep', 'peer', 'peg', 'penetrate', 'pierce', 'remark', 'scan', 'scope', 'scrutinize', 'sight', 'spy', 'stare', 'survey', 'be apprised of', 'catch a glimpse of', 'catch sight of', 'get a load of', 'lay eyes on', 'make out', 'pay attention to', 'take notice' ],
+    view:           [ 'consider', 'examine', 'explore', 'notice', 'observe', 'perceive', 'read', 'regard', 'scrutinize', 'see', 'watch', 'witness', 'beam', 'behold', 'canvass', 'contemplate', 'descry', 'dig', 'discern', 'distinguish', 'espy', 'eye', 'flash', 'gaze', 'inspect', 'mark', 'pipe', 'rubberneck', 'scan', 'scope', 'spot', 'spy', 'stare', 'survey', 'check out', 'check over', 'eagle eye', 'feast eyes on', 'get a load of', 'lay eyes on', 'set eyes on', 'take in' ],
+    watch:          [ 'attend', 'check out', 'examine', 'follow', 'keep an eye on', 'listen', 'look', 'observe', 'regard', 'scan', 'scrutinize', 'see', 'stare', 'view', 'wait', 'case', 'concentrate', 'contemplate', 'eye', 'eyeball', 'focus', 'gaze', 'inspect', 'mark', 'mind', 'note', 'peer', 'pipe', 'rubberneck', 'scope', 'spy', 'eagle-eye', 'get a load of', 'give the once over', 'have a look-see', 'keep tabs on', 'pay attention', 'take in', 'take notice' ],
+};
+
+const USAGE = {
+    YES:            'YES',
+    NO:             'NO',
+    DONT_CARE:      'DONT_CARE',
+};
+
+const DICTIONARY = {
+    see:    [
+        [ 'see',            USAGE.YES,          USAGE.DONT_CARE,    USAGE.DONT_CARE ],
+        [ 'examine',        USAGE.YES,          USAGE.NO,           USAGE.DONT_CARE ],
+        [ 'look',           USAGE.YES,          USAGE.DONT_CARE,    USAGE.DONT_CARE ],
+        [ 'watch',          USAGE.YES,          USAGE.DONT_CARE,    USAGE.DONT_CARE ],
+        [ 'check out',      USAGE.NO,           USAGE.YES,          USAGE.DONT_CARE ],
+        [ 'contemplate',    USAGE.DONT_CARE,    USAGE.DONT_CARE,    USAGE.YES ],
+    ],
+};
+
+
 export const Organization = () => ({
     __typename:     'Organization',
     id:             'example-org',
@@ -12,7 +49,6 @@ export const ReaderFlag = name => ({
     __typename:     'ReaderFlag',
     id:             name,
     name,
-    somethingElse:  'test',
 });
 export const User = () => ({
     __typename:     'User',
@@ -22,12 +58,59 @@ export const User = () => ({
     organization:   Organization,
 });
 
+export const OrgDictionaryItem = ( name, usageModels = []) => ({
+    __typename:     'OrgDictionaryItem',
+    name,
+    usageModels,
+});
+
+export const PhraseUsageModel = ( phrase, defaultUsage, readerUsage = []) => ({
+    __typename:     'PhraseUsageModel',
+    phrase,
+    defaultUsage,
+    readerUsage,
+});
+
+export const ReaderFlagUsage = ( flag, usage ) => ({
+    __typename:     'ReaderFlagUsage',
+    flag,
+    usage,
+});
+
+export const orgDictionary = () =>
+    Object.keys( DICTIONARY )
+        .map( key => OrgDictionaryItem(
+            key,
+            DICTIONARY[key].map( row =>
+                PhraseUsageModel(
+                    Phrase( row[0]),
+                    ReaderFlagUsage( ReaderFlag( 'default' ), row[1]),
+                    row.slice( 2 ).map(( usage, i ) =>
+                        ReaderFlagUsage( ReaderFlag( READER_FLAGS[i]), usage )
+                    ),
+                )
+            ),
+        ));
+
+
 export default {
     Organization,
     User,
     Query: {
         me:             User,
-        phrases:        ( _, { query = 'phrase' }) => [ Phrase( query ) ],
-        readerFlags:    () => [ ReaderFlag( 'junior' ), ReaderFlag( 'senior' ) ],
+        orgDictionary,
+        phrases:        ( _, { query }) => {
+
+            const regexp =  new RegExp( `${ query }.*`, 'i' );
+
+            return flatten([ keys( SYNONYMS ), values( SYNONYMS ) ])
+                .find( regexp.test.bind( regexp ))
+                .map( Phrase );
+        },
+        readerFlags:    () => READER_FLAGS.map( ReaderFlag ),
+        synonyms:       ( _, { phraseId }) => ({
+            rootPhrase: Phrase( phraseId ),
+            phrases:    ( SYNONYMS[phraseId] || []).map( Phrase ),
+        }),
     },
 };
