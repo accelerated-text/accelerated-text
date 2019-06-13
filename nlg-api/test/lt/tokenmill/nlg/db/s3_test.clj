70b7eafa2415f1e187312c3d2e1664fb3648a469
Šarūnas Navickas <sarunas.navickas@tokenmill.lt>
2019-06-13 06:47:38 +0000

    Resolve "Apply reader model on lexicon"

--- nlg-api/test/lt/tokenmill/nlg/db/s3_test.clj
+++ nlg-api/test/lt/tokenmill/nlg/db/s3_test.clj
@@ -8,9 +8,7 @@
 (deftest ^:integration reading-from-s3
   (testing "List files in bucket"
     (let [result (s3/list-files config/data-bucket "example-user" 20)]
-      (println (format "Files in folder: %s" (list result)))
       (is (> (count result) 0))))
   (testing "Read data-example.csv"
     (let [result (s3/read-file config/data-bucket  "example-user/data-example.csv")]
-      (is (= "\"Product name\",\"Main Feature\",\"Secondary feature\",\"Style\",\"Lacing\"\n
-\"Nike Air\",\"comfort\",\"support\",\"with sleek update on a classic design\",\"premium lacing\"" result)))))
+      (is (= "\"Product name\",\"Main Feature\",\"Secondary feature\",\"Style\",\"Lacing\"\n\"Nike Air\",\"comfort\",\"support\",\"with sleek update on a classic design\",\"premium lacing\"" result)))))
