2247dcfd493319e423ad004e823eccd4f081ef4c
Šarūnas Navickas <sarunas.navickas@tokenmill.lt>
2019-06-18 13:40:25 +0300

    Adjusting backend

--- graphql-api/src/graphql/core.clj
+++ graphql-api/src/graphql/core.clj
@@ -8,10 +8,6 @@
             [com.walmartlabs.lacinia.parser.schema :as parser]
             [com.walmartlabs.lacinia :refer [execute]]))
 
-(defn lexicon [context arguments value]
-  (let [result (:body (lexicon/search arguments nil))]
-    (update result :items #(map (fn [{:keys [key] :as item}] (assoc item :id key)) %))))
-
 (defn dictionary [_ _ _]
   (dictionary/list-dictionary-items))
 
@@ -36,30 +32,28 @@
 (defn create-phrase-usage-model [_ {:keys [dictionaryItemId phrase defaultUsage] :or {defaultUsage :YES}} _]
   (let [usage-models (:usageModels (dictionary/dictionary-item {:id dictionaryItemId}))
         new-phrase-model-id (:id (dictionary/create-phrase-usage-model {:phrase phrase :defaultUsage defaultUsage}))]
-    (dictionary/update-dictionary-item-usage-models {:id dictionaryItemId :usageModels (conj usage-models new-phrase-model-id)})))
+    (dictionary/update-dictionary-item-usage-models {:id dictionaryItemId :phraseUsage (conj usage-models new-phrase-model-id)})))
 
 (defn delete-phrase-usage-model [_ {:keys [id]} _]
   (let [dictionary-id (dictionary/dictionary-item-id-that-contains-phrase-model {:id id})
-        usage-models (:usageModels (dictionary/dictionary-item {:id dictionary-id}))]
+        usage-models (:phraseUsage (dictionary/dictionary-item {:id dictionary-id}))]
     (dictionary/delete-phrase-usage-model {:id id})
-    (dictionary/update-dictionary-item-usage-models {:id dictionary-id :usageModels (remove #(= id %) usage-models)})))
+    (dictionary/update-dictionary-item-usage-models {:id dictionary-id :phraseUsage (remove #(= id %) usage-models)})))
 
 (def nlg-schema
   (-> "schema.graphql"
       (io/resource)
       slurp
-      (parser/parse-schema {:resolvers {:Query            {:searchLexicon  :lexicon
-                                                           :dictionary     :dictionary
-                                                           :dictionaryItem :dictionary-item}
+      (parser/parse-schema {:resolvers {:Query            {:dictionary     :dictionary
+                                                           :dictionaryResults :dictionary-results}
                                         :Mutation         {:updateReaderFlagUsage :update-reader-flag-usage
-                                                           :updatePhraseUsageModelDefault :update-phrase-usage-model
-                                                           :createPhraseUsageModel :create-phrase-usage-model
-                                                           :deletePhraseUsageModel :delete-phrase-usage-model}
-                                        :DictionaryItem   {:usageModels :phrase-usage}
-                                        :PhraseUsageModel {:readerUsage :reader-usage}
+                                                           :updatePhraseUsageDefault :update-phrase-usage-model
+                                                           :createPhraseUsage :create-phrase-usage-model
+                                                           :deletePhraseUsage :delete-phrase-usage-model}
+                                        :DictionaryItem   {:phraseUsage :phrase-usage}
+                                        :PhraseUsage      {:readerUsage :reader-usage}
                                         :ReaderFlagUsage  {:flag :reader-flag}}})
-      (util/attach-resolvers {:lexicon                   lexicon
-                              :dictionary                dictionary
+      (util/attach-resolvers {:dictionary                dictionary
                               :dictionary-item           dictionary-item
                               :phrase-usage              phrase-usage-models
                               :reader-usage              reader-usage
