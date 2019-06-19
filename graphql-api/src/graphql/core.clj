46f970acfa142f27a2113dc4ed718cafd027f79d
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2019-06-19 13:42:21 +0300

    Refactor queries and mutations by domain

--- graphql-api/src/graphql/core.clj
+++ graphql-api/src/graphql/core.clj
@@ -1,51 +1,12 @@
 (ns graphql.core
   (:require [clojure.java.io :as io]
             [clojure.tools.logging :as log]
-            [nlg.dictionary :as dictionary]
-            [translate.dictionary :as translate-dict]
+            [graphql.domain.dictionary :as dictionary-domain]
             [com.walmartlabs.lacinia.util :as util]
             [com.walmartlabs.lacinia.schema :as schema]
             [com.walmartlabs.lacinia.parser.schema :as parser]
             [com.walmartlabs.lacinia :refer [execute]]))
 
-(defn dictionary [_ _ _]
-  (dictionary/list-dictionary-items))
-
-(defn dictionary-item [_ arguments _]
-  (-> (dictionary/dictionary-item arguments)
-      (translate-dict/dictionary-item-out)))
-
-(defn dictionary-results [_ arguments _]
-  {:items (list (dictionary-item arguments))
-   :offset 0
-   :limit 0
-   :totalCount 0})
-
-(defn phrase-usage-models [_ _ value]
-  (:phrase-usage-model (dictionary/phrase-usage-models {:ids (:phrases value)})))
-
-(defn reader-usage [_ _ value]
-  (:reader-flag-usage (dictionary/reader-flag-usages {:ids (:readerUsage value)})))
-
-(defn reader-flag [_ _ value]
-  (dictionary/reader-flag {:id (:readerFlag value)}))
-
-(defn update-reader-flag-usage [_ arguments _]
-  (dictionary/update-reader-flag-usage arguments))
-
-(defn update-phrase-usage-model [_ arguments _]
-  (dictionary/update-phrase-usage arguments))
-
-(defn create-phrase-usage-model [_ {:keys [dictionaryItemId phrase defaultUsage] :or {defaultUsage :YES}} _]
-  (let [usage-models (:phrases (dictionary/dictionary-item {:id dictionaryItemId}))
-        new-phrase-model-id (:id (dictionary/create-phrase-usage-model {:phrase phrase :defaultUsage defaultUsage}))]
-    (dictionary/update-dictionary-item-usage-models {:id dictionaryItemId :phrases (conj usage-models new-phrase-model-id)})))
-
-(defn delete-phrase-usage-model [_ {:keys [id]} _]
-  (let [dictionary-id (dictionary/dictionary-item-id-that-contains-phrase-model {:id id})
-        usage-models (:phrases (dictionary/dictionary-item {:id dictionary-id}))]
-    (dictionary/delete-phrase-usage-model {:id id})
-    (dictionary/update-dictionary-item-usage-models {:id dictionary-id :phrases (remove #(= id %) usage-models)})))
 
 (def nlg-schema
   (-> "schema.graphql"
@@ -63,16 +24,16 @@
                                         :Phrase      {:readerFlagUsage :reader-usage}
                                         :ReaderFlagUsage  {:flag :reader-flag}
                                         }})
-      (util/attach-resolvers {:dictionary                dictionary
-                              :dictionary-item           dictionary-item
-                              :dictionary-results        dictionary-results
-                              :phrase-usage              phrase-usage-models
-                              :reader-usage              reader-usage
-                              :reader-flag               reader-flag
-                              :update-reader-flag-usage  update-reader-flag-usage
-                              :update-phrase-usage-model update-phrase-usage-model
-                              :create-phrase-usage-model create-phrase-usage-model
-                              :delete-phrase-usage-model delete-phrase-usage-model})
+      (util/attach-resolvers {:dictionary                dictionary-domain/dictionary
+                              :dictionary-item           dictionary-domain/dictionary-item
+                              :dictionary-results        dictionary-domain/dictionary-results
+                              :phrase-usage              dictionary-domain/phrase-usage-models
+                              :reader-usage              dictionary-domain/reader-usage
+                              :reader-flag               dictionary-domain/reader-flag
+                              :update-reader-flag-usage  dictionary-domain/update-reader-flag-usage
+                              :update-phrase-usage-model dictionary-domain/update-phrase-usage-model
+                              :create-phrase-usage-model dictionary-domain/create-phrase-usage-model
+                              :delete-phrase-usage-model dictionary-domain/delete-phrase-usage-model})
       schema/compile))
 
 (defn nlg [{:keys [query variables context] :as request}]
---
+++ graphql-api/src/graphql/domain/dictionary.clj
@@ -0,0 +1,43 @@
+(ns graphql.domain.dictionary
+  (:require [nlg.dictionary :as dictionary-api]
+            [translate.dictionary :as translate-dict]))
+
+
+(defn dictionary [_ _ _]
+  (dictionary-api/list-dictionary-items))
+
+(defn dictionary-item [_ arguments _]
+  (-> (dictionary-api/dictionary-item arguments)
+      (translate-dict/dictionary-item-out)))
+
+(defn dictionary-results [_ arguments _]
+  {:items (list (dictionary-item arguments))
+   :offset 0
+   :limit 0
+   :totalCount 0})
+
+(defn phrase-usage-models [_ _ value]
+  (:phrase-usage-model (dictionary-api/phrase-usage-models {:ids (:phrases value)})))
+
+(defn reader-usage [_ _ value]
+  (:reader-flag-usage (dictionary-api/reader-flag-usages {:ids (:readerUsage value)})))
+
+(defn reader-flag [_ _ value]
+  (dictionary-api/reader-flag {:id (:readerFlag value)}))
+
+(defn update-reader-flag-usage [_ arguments _]
+  (dictionary-api/update-reader-flag-usage arguments))
+
+(defn update-phrase-usage-model [_ arguments _]
+  (dictionary-api/update-phrase-usage arguments))
+
+(defn create-phrase-usage-model [_ {:keys [dictionaryItemId phrase defaultUsage] :or {defaultUsage :YES}} _]
+  (let [usage-models (:phrases (dictionary-api/dictionary-item {:id dictionaryItemId}))
+        new-phrase-model-id (:id (dictionary-api/create-phrase-usage-model {:phrase phrase :defaultUsage defaultUsage}))]
+    (dictionary-api/update-dictionary-item-usage-models {:id dictionaryItemId :phrases (conj usage-models new-phrase-model-id)})))
+
+(defn delete-phrase-usage-model [_ {:keys [id]} _]
+  (let [dictionary-id (dictionary-api/dictionary-item-id-that-contains-phrase-model {:id id})
+        usage-models (:phrases (dictionary-api/dictionary-item {:id dictionary-id}))]
+    (dictionary-api/delete-phrase-usage-model {:id id})
+    (dictionary-api/update-dictionary-item-usage-models {:id dictionary-id :phrases (remove #(= id %) usage-models)})))
