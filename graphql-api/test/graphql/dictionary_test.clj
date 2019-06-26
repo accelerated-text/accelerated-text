a5eb274b6f55c5112d03950a1978a3da232d0169
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2019-06-26 15:45:13 +0300

    Some test tweaks

--- graphql-api/test/graphql/dictionary_test.clj
+++ graphql-api/test/graphql/dictionary_test.clj
@@ -3,7 +3,15 @@
             [graphql.core :as graph]
             [jsonista.core :as json]
             [clojure.tools.logging :as log]
-            [clojure.set :as set]))
+            [clojure.set :as set]
+            [data-access.entities.dictionary :as dict-entity]))
+
+(defn prepare-environment [f]
+  (dict-entity/create-dictionary-item {:key "test-phrase"
+                                       :partOfSpeech "VB"
+                                       :phrases ["t1" "t2" "t3"]})
+  (f)
+  (dict-entity/delete-dictionary-item "test-phrase"))
 
 (defn normalize-resp [resp]
   (-> resp (json/write-value-as-string) (json/read-value)))
@@ -15,8 +23,7 @@
                       [k2 p2] (second pair)]
                   (log/debugf "%s vs %s" (pr-str p2) phrases-part)
                   (log/spyf "Matching result: %s" (and (= p1 (get name-part k1))
-                                                       (= (set p2) (set (get phrases-part k2))))))
-                )
+                                                       (= (set p2) (set (get phrases-part k2)))))))
               (flatten col))
       (empty?)
       (not)))
@@ -31,11 +38,6 @@
   (let [result (graph/nlg {:query "{dictionary{items{name partOfSpeech phrases{id text defaultUsage readerFlagUsage{id usage flag{id name}}}}}}"})]
     (is (nil? (:errors result)))))
 
-(deftest ^:integration list-dictionary
-  (let [result (normalize-resp (graph/nlg {:query "{dictionary{items{name} totalCount}}"}))
-        expected {"data" {"dictionary" {"totalCount" 2 "items" [{"name" "provide"} {"name" "see"}]}}}]
-    (is (= expected result))))
-
 (deftest ^:integration list-dictionary-phrases
   (let [resp (graph/nlg {:query "{dictionary{items{name phrases{text}}}}"})
         result (->> (:data resp)
@@ -43,34 +45,17 @@
                     :items
                     (partition 2))]
     (log/debugf "Result:\t %s\n" (pr-str result))
-    (is (exists-pair? result (list {:name "provide"} {:phrases '({:text"gives"} {:text "offers"} {:text "provides"})})))
-    ))
+    (is (exists-pair? result (list {:name "test-phrase"} {:phrases '({:text "t1"} {:text "t2"} {:text "t3"})})))))
 
 (deftest ^:integration get-dictionary-item
-  (let [resp (graph/nlg {:query "{dictionaryItem(id: \"provide\"){name partOfSpeech phrases{text}}}"})
+  (let [resp (graph/nlg {:query "{dictionaryItem(id: \"test-phrase\"){name partOfSpeech phrases{text}}}"})
         result (->> (:data resp)
                     :dictionaryItem)]
-    (is (= "provide" (:name result)))
+    (is (= "test-phrase" (:name result)))
     (is (= :VB  (:partOfSpeech result)))
-    (is (= #{{:text "offers"}
-             {:text "gives"}
-             {:text "provides"}}
+    (is (= #{{:text "t1"}
+             {:text "t2"}
+             {:text "t3"}}
            (set (:phrases result))))))
 
-(deftest ^:integration get-reader-flags
-  (let [resp (graph/nlg {:query "{readerFlags{flags{name}}}"})
-        result (->> (:data resp)
-                    :readerFlags
-                    :flags)]
-    (is (exists-item? result {:name "junior"}))
-    (is (exists-item? result {:name "senior"}))))
-
-(deftest ^:integration get-default-usage
-  (let [resp (graph/nlg {:query "{dictionary{items{phrases{defaultUsage}}}}"})
-        result (->> (:data resp)
-                    :dictionary
-                    :items)]
-    (is (= :YES (-> (first result)
-                    :phrases
-                    (first)
-                    :defaultUsage)))))
+(use-fixtures :once prepare-environment)
