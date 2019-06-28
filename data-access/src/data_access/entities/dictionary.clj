7304fbb163a2d3f5dae913781cc40d8c3b0335df
Šarūnas Navickas <sarunas.navickas@tokenmill.lt>
2019-06-28 11:31:35 +0300

    Refactor out db binding

--- data-access/src/data_access/entities/dictionary.clj
+++ data-access/src/data_access/entities/dictionary.clj
@@ -2,29 +2,19 @@
   (:require [data-access.db.dynamo-ops :as ops]
             [data-access.utils :as utils]))
 
-(defn list-readers
-  []
-  (let [db (ops/db-access :reader-flag)]
-    (ops/list! db 100)))
+(defn list-readers [] (ops/list! (ops/db-access :reader-flag) 100))
 
-(defn get-default-flags
-  []
-  (into {} (map (fn [r]  {(keyword (r :id)) :DONT_CARE}) (list-readers))))
+(defn get-default-flags [] (into {} (map (fn [r]  {(keyword (r :id)) :DONT_CARE}) (list-readers))))
 
 (defn get-reader
   [k]
-  (let [db (ops/db-access :reader-flag)]
-    (ops/read! db k)))
+  (ops/read! (ops/db-access :reader-flag) k))
 
-(defn list-dictionary
-  []
-  (let [db (ops/db-access :dictionary-combined)]
-    (ops/list! db 100)))
+(defn list-dictionary [] (ops/list! (ops/db-access :dictionary-combined) 100))
 
 (defn get-dictionary-item
   [k]
-  (let [db (ops/db-access :dictionary-combined)]
-    (ops/read! db k)))
+  (ops/read! (ops/db-access :dictionary-combined) k))
 
 (defn text->phrase
   [text parent-id default-usage default-flags]
@@ -34,24 +24,21 @@
 
 (defn create-dictionary-item
   [{:keys [key name phrases partOfSpeech]}]
-  (let [db (ops/db-access :dictionary-combined)
-        default-flags (get-default-flags)
+  (let [default-flags (get-default-flags)
         result {:name name
                 :partOfSpeech partOfSpeech
                 :phrases (doall (map #(text->phrase % key :YES default-flags) phrases))}]
-    (ops/write! db key result)))
+    (ops/write! (ops/db-access :dictionary-combined) key result)))
 
 (defn delete-dictionary-item
   [k]
-  (let [db (ops/db-access :dictionary-combined)]
-    (ops/delete! db k)))
+  (ops/delete! (ops/db-access :dictionary-combined) k))
 
 (defn update-dictionary-item
   [{:keys [key name phrases partOfSpeech]}]
-  (let [db (ops/db-access :dictionary-combined)]
-    (ops/update! db key {:name name
-                         :phrases (when-not (nil? phrases)
-                                    (ops/freeze! phrases))
-                         :partOfSpeech partOfSpeech})))
+  (ops/update! (ops/db-access :dictionary-combined) key {:name name
+                                                         :phrases (when-not (nil? phrases)
+                                                                    (ops/freeze! phrases))
+                                                         :partOfSpeech partOfSpeech}))
 
 
