a357370b9a08f7f9643107232052ac9b5651c4ef
Šarūnas Navickas <sarunas.navickas@tokenmill.lt>
2019-07-02 13:47:42 +0300

    Basic member ops

--- data-access/src/data_access/entities/amr.clj
+++ data-access/src/data_access/entities/amr.clj
@@ -1,11 +1,20 @@
 (ns data-access.entities.amr
-  (:require [data-class.db.dynamo-ops :as ops]
+  (:require [data-access.db.dynamo-ops :as ops]
             [data-access.utils :as utils]))
 
 (defn list-members [] (ops/list! (ops/db-access :members) 100))
 
 (defn get-member [k] (ops/read! (ops/db-access :members) k))
 
-(defn update-member [{:keys [id name grouping]}])
+(defn create-member
+  [{:keys [name grouping]}]
+  (ops/write! (ops/db-access :members) {:name name
+                                        :grouping grouping}))
 
-(defn delete-member [k])
+(defn update-member
+  [{:keys [id name grouping]}]
+  (ops/update! (ops/db-access :members) id {:id id
+                                           :name name
+                                           :grouping grouping}))
+
+(defn delete-member [k] (ops/delete! (ops/db-access :members) k))
