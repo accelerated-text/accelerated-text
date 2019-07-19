b85fa65a806e88ffd1e5064b65465ea4b74d6303
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2019-07-19 10:33:28 +0300

    Fix import

--- nlg-api/src/utils/import.clj
+++ nlg-api/src/utils/import.clj
@@ -2,19 +2,10 @@
   (:require [data-access.entities.amr :as amr-entity]
             [ccg-kit.verbnet.core :as verbnet]))
 
-(defn import-member
-  [member]
-  (amr-entity/update-member {:id (:wn member)
-                             :name (:name member)
-                             :grouping (when (not= "" (:grouping member))
-                                         (:grouping member))}))
-
 (defn import-verbnet
   []
-  (let [data (verbnet/xml->vclass "resources/verbnet/battle.xml")
-        members (map import-member (:members data))]
+  (let [data (verbnet/xml->vclass "resources/verbnet/battle.xml")]
     (amr-entity/create-verbclass {:id (:id data)
-                                  :members (map :id members)
                                   :thematic-roles (:thematic-roles data)
                                   :frames (:frames data)})))
 
