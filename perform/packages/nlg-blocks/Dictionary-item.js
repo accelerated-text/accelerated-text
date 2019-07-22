b50524bd0614fe172db8fec070e274cc57438a44
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2019-07-23 02:28:28 +0300

    feat: Removed editing from Dictionary-Item block.

--- perform/packages/nlg-blocks/Dictionary-item.js
+++ perform/packages/nlg-blocks/Dictionary-item.js
@@ -1,8 +1,10 @@
-import { BLUER }        from '../styles/blockly-colors';
+import { h, mount }         from 'dom-dom';
 
+import { BLUER }            from '../styles/blockly-colors';
 
-import Block            from './Block';
-import * as T           from './types';
+import Block                from './Block';
+import * as T               from './types';
+import toNlgJson            from './to-nlg-json';
 
 
 export default Block({
@@ -14,9 +16,34 @@ export default Block({
         output:             T.LIST,
         message0:           'Dict.: %1',
         args0: [{
-            type:           'field_input',
+            type:           'field_label',
             name:           'name',
-            text:           'see',
+            text:           '<ERROR: FIELD VALUE NOT SET!>',
         }],
+    },
+
+    domToMutation( xmlElement ) {
+
+        this.itemId =       xmlElement.getAttribute( 'id' );
+        this.itemName =     xmlElement.getAttribute( 'name' );
+
+        this.getField( 'name' )
+            .setValue( this.itemName );
+    },
+
+    mutationToDom() {
+        return mount(
+            <mutation
+                id={ this.itemId }
+                name={ this.itemName }
+            />
+        );
+    },
+
+    toNlgJson() {
+        return {
+            ...toNlgJson( this ),
+            itemId:         this.itemId,
+        };
     },
 });
