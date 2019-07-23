e4e9fc01836663ac4e8d36342ddbc38b41e75963
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2019-07-23 16:58:05 +0300

    test: Implemented a couple of simple tests for AMR UI.

--- perform/packages/tests/amr.test.js
+++ perform/packages/tests/amr.test.js
@@ -1,8 +1,20 @@
-import test                 from 'ava';
+import test                     from 'ava';
+
+import defaultResponsesPage     from './lib/default-responses-page';
+import noRecordsPage            from './lib/no-records-page';
+import { SELECTORS }            from './constants';
 
 
-test.todo( 'default elements visible' );
-test.todo( 'correct elements when no items' );
+test( 'default elements visible', defaultResponsesPage, async t => {
+
+    await t.findElement( SELECTORS.AMR_CONCEPT );
+});
+
+
+test( 'correct elements when no items', noRecordsPage, async t => {
+
+    await t.notFindElement( SELECTORS.AMR_CONCEPT );
+});
 
 test.todo( 'can expand help text' );
 test.todo( 'can drag-in blocks' );
---
+++ perform/packages/tests/data/concepts.js
@@ -0,0 +1,59 @@
+import * as NlgTypes        from '../../nlg-blocks/types';
+
+
+export const HELP_TEXT = `
+# Please help me!
+
+I am drowning in code
+`;
+
+
+export const ThematicRole = ( fieldLabel, fieldType ) => ({
+    __typename:     'ThematicRole',
+    id:             fieldLabel,
+    fieldLabel,
+    fieldType,
+});
+
+
+export const Concept = ( label, dictItemName, roles ) => ({
+    __typename:     'Concept',
+    id:             label,
+    label,
+    helpText:       HELP_TEXT,
+    dictionaryItem: {
+        __typename: 'DictionaryItem',
+        id:         dictItemName,
+        name:       dictItemName,
+    },
+    roles: Object
+        .keys( roles )
+        .map( fieldLabel => ThematicRole( fieldLabel, roles[fieldLabel])),
+});
+
+
+export const Concepts = concepts => ({
+    __typename:     'Concepts',
+    id:             'Concepts',
+    concepts:       concepts.map( c => Concept( ...c )),
+});
+
+
+export const EMPTY_CONCEPTS = {
+    concepts:       Concepts([]),
+};
+
+
+export default {
+    concepts: Concepts([
+        [ 'See', 'sees', {
+            agent:      NlgTypes.TEXT,
+            coAgent:    NlgTypes.TEXT,
+        }],
+        [ 'Arrive', 'arrived', {
+            agent:      NlgTypes.TEXT,
+            atPlace:    NlgTypes.TEXT,
+            onTime:     NlgTypes.TEXT,
+        }],
+    ]),
+};
