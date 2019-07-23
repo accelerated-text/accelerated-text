4b83aae16d3e06dcab3aa87446dc08b7ff1540fa
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2019-07-23 14:45:45 +0300

    test: Added TODOs for AMR tests.

---
+++ perform/packages/tests/amr.test.js
@@ -0,0 +1,8 @@
+import test                 from 'ava';
+
+
+test.todo( 'default elements visible' );
+test.todo( 'correct elements when no items' );
+
+test.todo( 'can expand help text' );
+test.todo( 'can drag-in blocks' );
