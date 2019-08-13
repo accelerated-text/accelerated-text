69b6e13e1bd19269fb09a538953c367c4e952a13
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2019-08-13 08:59:36 +0300

    feat: Implemented better status handling for variant views.

--- perform/packages/fake-shop/Publisher.js
+++ perform/packages/fake-shop/Publisher.js
@@ -3,10 +3,9 @@ import { path }             from 'ramda';
 
 import { composeQueries }   from '../graphql/';
 import {
-    Error,
     Info,
-    Loading,
 }                           from '../ui-messages/';
+import VariantsView         from '../variants/ViewWithStatus';
 
 import AddProduct           from './AddProduct';
 import S                    from './Publisher.sass';
@@ -48,30 +47,28 @@ export default composeQueries({
             <h3 children={ record.title } className={ S.title } />
             <p>by <i>{ record.authors }</i></p>
             <p>ISBN: { record['isbn-13'] }</p>
-            <div className={ S.description }>{
-                descriptionError
-                    ? <Error message="Error generating description" />
-                : descriptionLoading
-                    ? <Loading message="Updating description" />
-                : description
-                    ? description
-                    : <Info message="No description (yet?)" />
-            }</div>
-            { isUpToDate
-                ? <p><Info message="The product is up-to-date" /></p>
-            : product
-                ? <UpdateProduct
-                    description={ description }
-                    disabled={ loading }
-                    product={ product }
-                />
-                : <AddProduct
-                    className={ S.addProduct }
-                    description={ description }
-                    disabled={ loading }
-                    record={ record }
-                />
-            }
+            <VariantsView
+                emptyMessage="No description (yet?)"
+                loadingMessage="Updating description"
+            >
+                { () => [
+                    <div className={ S.description }>{ description }</div>,
+                    isUpToDate
+                        ? <p><Info message="The product is up-to-date" /></p>
+                    : product
+                        ? <UpdateProduct
+                            description={ description }
+                            disabled={ loading }
+                            product={ product }
+                        />
+                        : <AddProduct
+                            className={ S.addProduct }
+                            description={ description }
+                            disabled={ loading }
+                            record={ record }
+                        />,
+                ] }
+            </VariantsView>
         </div>
     );
 });
--- perform/packages/variants/ViewWithStatus.js
+++ perform/packages/variants/ViewWithStatus.js
@@ -5,32 +5,62 @@ import {
     Info,
     Loading,
 }                           from '../ui-messages/';
+import getOpenedPlan        from '../plan-list/get-opened-plan';
+import { getStatus }        from '../document-plans/functions';
 import { useStores }        from '../vesa/';
 
 
 export default useStores([
+    'documentPlans',
+    'planList',
     'variantsApi',
 ])(({
     children,
     className,
+    documentPlans,
     emptyMessage =          'No variants.',
     loadingMessage =        'Loading variants...',
-    variantsApi: {
-        error,
-        loading,
-        result,
-    },
-}) =>
-    <div className={ className }>
-        { error
-            ? <Error message={ error } />
-        : loading
-            ? <Loading message={ loadingMessage } />
-        : ( result && result.variants && result.variants.length )
-            ? children({
-                variants:   result.variants,
-            })
-            : <Info message={ emptyMessage } />
-        }
-    </div>
-);
+    planList,
+    variantsApi,
+}) => {
+    const { result } =      variantsApi;
+    const openedPlan =      getOpenedPlan({ documentPlans, planList });
+    const planStatus =
+        openedPlan
+            ? getStatus( documentPlans, openedPlan )
+            : null;
+
+    const error = (
+        variantsApi.error
+        || ( planStatus
+            ? ( planStatus.createError
+               || planStatus.readError
+               || planStatus.updateError
+            )
+            : planList.getListError
+        )
+    );
+
+    const loading = (
+        ! planStatus
+        || variantsApi.loading
+        || planStatus.createLoading
+        || planStatus.readLoading
+        || planStatus.updateLoading
+    );
+
+    return (
+        <div className={ className }>
+            { error
+                ? <Error message={ error } />
+            : loading
+                ? <Loading message={ loadingMessage } />
+            : ( result && result.variants && result.variants.length )
+                ? children({
+                    variants:   result.variants,
+                })
+                : <Info message={ emptyMessage } />
+            }
+        </div>
+    );
+});
