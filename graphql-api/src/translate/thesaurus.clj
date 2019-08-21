589601617857ed8eef53ca20430ccf4429b2a500
Rokas <rokas.ramanauskas@tokenmill.lt>
2019-08-21 16:41:44 +0300

    fix: thesaurus search, when no pos is provided

--- graphql-api/src/translate/thesaurus.clj
+++ graphql-api/src/translate/thesaurus.clj
@@ -2,24 +2,23 @@
   (:require [data-access.wordnet.core :as wn]
             [clojure.string :as string]))
 
-(defn search-thesaurus [query part-of-sepeech]
-  (let [words (wn/synonyms (wn/lookup-word (keyword part-of-sepeech) query))]
-    {:words (map (fn [{:keys [pos lemma]}]
-                   (assoc {}
-                          :id (format "%s-%s"
-                                      (name pos)
-                                      (string/replace lemma #" " "-"))
-                          :partOfSpeech (name pos)
-                          :text lemma))
-                 words)
-     :offset 0
-     :limit (count words)
+(defn search-thesaurus [query part-of-speech]
+  (let [words (mapcat wn/synonyms (if (some? part-of-speech)
+                                    (wn/lookup-words query (keyword part-of-speech))
+                                    (wn/lookup-words query)))]
+    {:words      (map (fn [{:keys [pos lemma]}]
+                        {:id           (format "%s-%s" (name pos) (string/replace lemma #" " "-"))
+                         :partOfSpeech (name pos)
+                         :text         lemma})
+                      words)
+     :offset     0
+     :limit      (count words)
      :totalCount (count words)}))
 
 (defn synonyms [word-id]
   (let [[pos & tokens] (string/split word-id #"-")
-        root-word      (string/join " " tokens)
-        words          (search-thesaurus root-word pos)]
+        root-word (string/join " " tokens)
+        words (search-thesaurus root-word pos)]
     {:rootWord {:id           word-id
                 :partOfSpeech pos
                 :text         root-word}
