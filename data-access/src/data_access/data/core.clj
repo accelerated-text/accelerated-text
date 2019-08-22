2f04ea66f9b5e061e8971fcf901799437225e0cd
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2019-08-22 15:38:18 +0300

    refactor: get-data and list-data functions

--- data-access/src/data_access/data/core.clj
+++ data-access/src/data_access/data/core.clj
@@ -3,9 +3,14 @@
             [data-access.db.s3 :as s3]
             [data-access.utils :as utils]))
 
-(defn get-data [user file])
+(defn get-data [user file]
+  (let [s3-key (str user "/" file)]
+    {:data (s3/read-csv config/data-bucket s3-key)
+     :key  s3-key}))
 
-(defn list-data [{:keys [user limit] :or {user "example-user" limit 20}}]
+(defn list-data [user limit]
+  (let [user (or user "example-user")
+        limit (or limit 20)])
   (map (fn [{s3-key :key :as instance}]
          (assoc instance :field-names (s3/get-csv-header config/data-bucket s3-key)))
        (s3/list-files config/data-bucket user limit)))
