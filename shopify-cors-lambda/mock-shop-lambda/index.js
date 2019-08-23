bcdcea3d00eb84bb8cf999c11e92d7ea735c35c8
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2019-08-23 14:27:13 +0300

    feat: Implemented mockup for createProduct mutation.

--- shopify-cors-lambda/mock-shop-lambda/index.js
+++ shopify-cors-lambda/mock-shop-lambda/index.js
@@ -1,3 +1,5 @@
+/// Constants ------------------------------------------------------------------
+
 const EXTENSIONS = {"cost":{"requestedQueryCost":252,"actualQueryCost":9,"throttleStatus":{"maximumAvailable":1000,"currentlyAvailable":991,"restoreRate":50}}};
 
 const PRODUCT = {"id":"gid://shopify/Product/2265556877408","descriptionHtml":"Manu Konchady wrote with Building Search Applications watch with Lucene, LingPipe, and Gate.","title":"Building Search Applications","__typename":"Product","images":{"edges":[{"node":{"id":"gid://shopify/ProductImage/11771933786208","altText":"Building Search Applications","src":"https://cdn.shopify.com/s/files/1/0115/7686/8960/products/content.jpg?v=1565778995","__typename":"Image"},"__typename":"ImageEdge"}],"__typename":"ImageConnection"},"variants":{"edges":[{"node":{"id":"gid://shopify/ProductVariant/20108392661088","sku":"9780615204253","__typename":"ProductVariant"},"__typename":"ProductVariantEdge"}],"__typename":"ProductVariantConnection"}}
@@ -13,37 +15,69 @@ const RESPONSE_HEADERS = {
 };
 
 
+/// Functions ------------------------------------------------------------------
+
+const graphqlResponse = data => ({
+    data,
+    extensions:             EXTENSIONS,
+});
+
 const operations = {
-    searchProducts: ({ variables: { query }}) => ({
-        data: {
-            products: {
-                __typename: 'ProductConnection',
-                edges: (
-                    query === PRODUCT_SKU_QUERY
-                        ? [{
-                            __typename: 'ProductEdge',
-                            cursor:     Math.random().toString( 36 ),
-                            node:       PRODUCT,
-                        }]
-                        : []
-                ),
-            },
-        },
-        extensions:         EXTENSIONS,
-    }),
-    updateProduct: ({ variables: { descriptionHtml }}) => ({
-        data: {
-            productUpdate: {
-                __typename: 'ProductUpdatePayload',
-                product: {
-                    ...PRODUCT,
-                    descriptionHtml,
+
+    createProduct: ({
+        descriptionHtml,
+        /// imageSrc,
+        sku,
+        title,
+    }) => graphqlResponse({
+        productCreate: {
+            __typename: 'ProductCreatePayload',
+            product: {
+                ...PRODUCT,
+                title,
+                descriptionHtml,
+                variants: {
+                    ...PRODUCT.variants,
+                    edges: [{
+                        ...PRODUCT.variants.edges[0],
+                        node: {
+                            ...PRODUCT.variants.edges[0].node,
+                            sku,
+                        },
+                    }],
                 },
             },
         },
-        extensions:         EXTENSIONS,
+    }),
+
+    searchProducts: ({ query }) => graphqlResponse({
+        products: {
+            __typename: 'ProductConnection',
+            edges: (
+                query === PRODUCT_SKU_QUERY
+                    ? [{
+                        __typename: 'ProductEdge',
+                        cursor:     Math.random().toString( 36 ),
+                        node:       PRODUCT,
+                    }]
+                    : []
+            ),
+        },
+    }),
+
+    updateProduct: ({ descriptionHtml }) => graphqlResponse({
+        productUpdate: {
+            __typename: 'ProductUpdatePayload',
+            product: {
+                ...PRODUCT,
+                descriptionHtml,
+            },
+        },
     }),
 };
+
+
+/// Handler --------------------------------------------------------------------
 
 exports.handler = ( event, _, callback ) => {
     console.log( 'EVENT', typeof event.body, event.headers );
@@ -51,11 +85,11 @@ exports.handler = ( event, _, callback ) => {
     let bodyJson =          null;
     try {
         bodyJson =          JSON.parse( event.body );
-        const fn =          operations[ bodyJson.operationName ];
+        const operation =   operations[ bodyJson.operationName ];
         callback( null, {
             isBase64Encoded:    false,
             statusCode:         200,
-            body:               JSON.stringify( fn( bodyJson )),
+            body:               JSON.stringify( operation( bodyJson.variables )),
             headers:            RESPONSE_HEADERS,
         });
     } catch( err ) {
