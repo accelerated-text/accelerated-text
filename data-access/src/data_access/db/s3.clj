83f5fd42065f27a0aaa06e5ab5cffe8c34497366
Rokas <rokas.ramanauskas@tokenmill.lt>
2019-08-26 11:58:12 +0300

    style: make s3 namespace more readable

--- data-access/src/data_access/db/s3.clj
+++ data-access/src/data_access/db/s3.clj
@@ -2,29 +2,21 @@
   (:require [clojure.tools.logging :as log]
             [data-access.db.config :as config]
             [data-access.utils :as utils])
-  (:import (com.amazonaws.services.s3 AmazonS3Client)
+  (:import (java.io File)
            (com.amazonaws ClientConfiguration)
-           (com.amazonaws.services.s3.model S3ObjectSummary AccessControlList ObjectListing)
-           (java.io File)))
+           (com.amazonaws.services.s3 AmazonS3Client)
+           (com.amazonaws.services.s3.model AccessControlList Grant ObjectListing S3Object S3ObjectSummary)))
 
-(defn build-client
-  []
-  (let [endpoint (config/s3-endpoint)
-        configuration (ClientConfiguration.)
-        client (AmazonS3Client. configuration)]
-    (when endpoint
-      (.setEndpoint client endpoint))
-    client))
+(defn build-client []
+  (let [endpoint (config/s3-endpoint)]
+    (cond-> (AmazonS3Client. (ClientConfiguration.))
+            (some? endpoint) (.setEndpoint endpoint))))
 
-(defn grant->map
-  [grant]
-  (let [grantee (.getGrantee grant)
-        permission (.getPermission grant)]
-    {:permission (.toString permission)
-     :grantee    (.getIdentifier grantee)}))
+(defn grant->map [^Grant g]
+  {:permission (str (.getPermission g))
+   :grantee    (.getIdentifier (.getGrantee g))})
 
-(defn summary->map
-  [^S3ObjectSummary s]
+(defn summary->map [^S3ObjectSummary s]
   {:bucket-name   (.getBucketName s)
    :etag          (.getETag s)
    :key           (.getKey s)
@@ -32,33 +24,28 @@
    :last-modified (str (.toInstant (.getLastModified s)))
    :size          (.getSize s)})
 
-(defn get-acl
-  [bucket path]
-  (let [acl (.getObjectAcl (build-client) bucket path)
-        grants (.getGrantsAsList acl)]
-    (map grant->map grants)))
+(defn get-acl [bucket path]
+  (map grant->map (-> (build-client)
+                      (.getObjectAcl bucket path)
+                      (.getGrantsAsList))))
 
-(defn read-file
-  [bucket path]
-  (let [s3-object (.getObject (build-client) bucket path)
-        content (.getObjectContent s3-object)]
-    (slurp content)))
+(defn read-file [bucket path]
+  (-> (build-client)
+      (.getObject bucket path)
+      (.getObjectContent)
+      (slurp)))
 
-(defn list-objects
-  [bucket path]
-  (let [^ObjectListing s3-object-list (.listObjects (build-client) bucket path)]
-    (map summary->map (.getObjectSummaries s3-object-list))))
+(defn list-objects [bucket path]
+  (map summary->map (-> (build-client)
+                        (.listObjects bucket path)
+                        (.getObjectSummaries))))
 
-(defn download-dir
-  [bucket path output]
-  (let [file-list (list-objects bucket path)
-        files (map :key file-list)]
-    (doseq [f files]
-      (log/debugf "Working with: %s" f)
-      (let [out-path (clojure.string/join "/" [output f])
-            content (read-file bucket f)]
-        (println out-path)
-        (spit out-path content)))))
+(defn download-dir [bucket path output-dir]
+  (doseq [{file :key} (list-objects bucket path)]
+    (let [output-path (clojure.string/join "/" [output-dir file])
+          content (read-file bucket file)]
+      (log/debugf "Writing file: %s" output-path)
+      (spit output-path content))))
 
 (defn read-csv [s3-bucket s3-key]
   (utils/raw-csv->maps (read-file config/data-bucket s3-key)))
