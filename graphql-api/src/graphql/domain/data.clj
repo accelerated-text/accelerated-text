8d806ebb8a4e9bb7c87d1c9a008dcaef92b0a9a6
Rokas <rokas.ramanauskas@tokenmill.lt>
2019-08-26 14:53:04 +0300

    refactor: update data domain and schema

--- graphql-api/src/graphql/domain/data.clj
+++ graphql-api/src/graphql/domain/data.clj
@@ -1,11 +1,27 @@
 (ns graphql.domain.data
-  (:require [cheshire.core :as json]
-            [clojure.set :as set]
+  (:require [clojure.set :as set]
             [data-access.data.core :as data]))
 
-(defn list-data [_ {:keys [user limit]} _]
-  {:data (map #(set/rename-keys % {:field-names :fieldNames})
-              (data/list-data user limit))})
+(defn list-data [_ {:keys [user offset limit] :or {user "example-user" offset 0 limit 20}} _]
+  (let [data-files (->> (data/list-data-files user)
+                        (map #(select-keys % [:key :file-name :field-names]))
+                        (map #(set/rename-keys % {:key         :id
+                                                  :file-name   :fileName
+                                                  :field-names :fieldNames})))]
+    {:dataFiles  (->> data-files (drop offset) (take limit))
+     :offset     offset
+     :limit      limit
+     :totalCount (count data-files)}))
 
-(defn get-data [_ {:keys [user file]} _]
-  (update (data/get-data user file) :data json/encode))
+(defn get-data [_ {:keys [user file recordOffset recordLimit] :or {user "example-user" recordOffset 0 recordLimit 20}} _]
+  (let [{:keys [key records field-names]} (data/read-csv-file (str user "/" file))]
+    {:id         key
+     :fieldNames field-names
+     :records    (for [[row record] (->> records (interleave (range)) (partition 2) (drop recordOffset) (take recordLimit))]
+                   (for [[column field-name value] (partition 3 (interleave (range) field-names record))]
+                     {:id        (str key "-" (inc row) "-" (inc column))
+                      :fieldName field-name
+                      :value     value}))
+     :recordOffset     recordOffset
+     :recordLimit      recordLimit
+     :totalCount (count records)}))
