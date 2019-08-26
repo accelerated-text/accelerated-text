79c4ce13a821a4d7686bbe9cb0ed70f8eb210858
Rokas <rokas.ramanauskas@tokenmill.lt>
2019-08-26 14:49:20 +0300

    refactor: update data core functions

--- data-access/src/data_access/data/core.clj
+++ data-access/src/data_access/data/core.clj
@@ -1,16 +1,15 @@
 (ns data-access.data.core
   (:require [data-access.db.config :as config]
             [data-access.db.s3 :as s3]
-            [data-access.utils :as utils]))
+            [data-access.utils :as utils]
+            [clojure.data.csv :as csv]))
 
-(defn get-data [user file]
-  (let [s3-key (str user "/" file)]
-    {:data (s3/read-csv config/data-bucket s3-key)
-     :key  s3-key}))
+(defn read-csv-file [s3-key]
+  (let [raw-csv-string (s3/read-file config/data-bucket s3-key)
+        rows (csv/read-csv raw-csv-string)]
+    {:records     (rest rows)
+     :field-names (first rows)
+     :key         s3-key}))
 
-(defn list-data [user limit]
-  (let [user (or user "example-user")
-        limit (or limit 20)])
-  (map (fn [{s3-key :key :as instance}]
-         (assoc instance :field-names (s3/get-csv-header config/data-bucket s3-key)))
-       (s3/list-files config/data-bucket user limit)))
+(defn list-data-files [user]
+  (s3/list-objects config/data-bucket user))
