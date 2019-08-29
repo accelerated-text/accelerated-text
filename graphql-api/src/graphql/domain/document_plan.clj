71ade5a82cc313ed3497bd1b6b63e6e3b3b5a696
Rokas <rokas.ramanauskas@tokenmill.lt>
2019-08-29 17:26:18 +0300

    refactor: use string timestamp in document plan domain; parse 'blocks' when creating or updating

--- graphql-api/src/graphql/domain/document_plan.clj
+++ graphql-api/src/graphql/domain/document_plan.clj
@@ -1,21 +1,32 @@
 (ns graphql.domain.document-plan
-  (:require [data-access.entities.document-plan :as document-plan]))
+  (:require [cheshire.core :as json]
+            [data-access.entities.document-plan :as dp]
+            [data-access.utils :refer [ts-to-string]]))
+
+(defn- document-plan-ts->string [document-plan]
+  (-> document-plan
+      (update :createdAt ts-to-string)
+      (update :updatedAt ts-to-string)))
 
 (defn get-document-plan [_ {:keys [id]} _]
-  (document-plan/get id))
+  (document-plan-ts->string
+    (dp/get-document-plan id)))
 
 (defn delete-document-plan [_ {:keys [id]} _]
-  (document-plan/delete id))
+  (dp/delete-document-plan id)
+  true)
 
 (defn list-document-plans [_ {:keys [offset limit] :or {offset 0 limit 20}} _]
-  (let [items (document-plan/list)]
-    {:items      (->> items (drop offset) (take limit))
+  (let [items (dp/list-document-plans)]
+    {:items      (->> items (drop offset) (take limit) (map document-plan-ts->string))
      :limit      limit
      :offset     offset
      :totalCount (count items)}))
 
 (defn add-document-plan [_ {:keys [id] :as args} _]
-  (document-plan/add id args))
+  (document-plan-ts->string
+    (dp/add-document-plan id (update args :blocks #(json/parse-string % true)))))
 
 (defn update-document-plan [_ {:keys [id] :as args} _]
-  (document-plan/update id args))
+  (document-plan-ts->string
+    (dp/update-document-plan id (update args :blocks #(json/parse-string % true)))))
