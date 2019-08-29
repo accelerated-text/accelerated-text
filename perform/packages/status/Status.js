8ce44bcc6642d7dbeaa75dddb457a7c5adb35e04
Emilis Dambauskas (Tokenmill) <emilis.dambauskas@tokenmill.lt>
2019-08-29 14:59:37 +0300

    refactor: Switched to using graphql values for dataFile status in Status widget.

--- perform/packages/status/Status.js
+++ perform/packages/status/Status.js
@@ -1,8 +1,9 @@
-import { h }                from 'preact';
+import { h, Component }     from 'preact';
 import { props }            from 'ramda';
 
 import { composeQueries  }  from '../graphql';
 import { concepts }         from '../graphql/queries.graphql';
+import DocumentPlansContext from '../document-plans/Context';
 import Error                from '../ui-messages/Error';
 import Loading              from '../ui-messages/Loading';
 import { useStores }        from '../vesa/';
@@ -11,61 +12,65 @@ import { useStores }        from '../vesa/';
 export default composeQueries({
     concepts,
 })( useStores([
-    'dataSamples',
     'documentPlans',
     'planList',
     'variantsApi',
-])(({
-    className,
-    concepts: {
-        concepts:           conceptsData,
-        error:              conceptsError,
-        loading:            conceptsLoading,
-    },
-    dataSamples,
-    documentPlans,
-    planList,
-    variantsApi,
-}) => {
-    const dataStatuses =    props( dataSamples.fileIds, dataSamples.statuses );
-    const planStatuses =    props( planList.uids, documentPlans.statuses );
+])( class Status extends Component {
 
-    const isError = (
-        conceptsError
-        || ( ! conceptsData && ! conceptsLoading )
-        || dataSamples.getListError
-        || planList.addCheckError
-        || planList.getListError
-        || variantsApi.error
-    );
+    static contextType =    DocumentPlansContext;
 
-    const isLoading = (
-        conceptsLoading
-        || variantsApi.loading
-        || dataSamples.getListLoading
-        || planList.getListLoading
-        || dataStatuses.find( status => status.getDataLoading )
-        || planStatuses.find( status => (
-            status.createLoading
-            || status.readLoading
-            || status.updateLoading
-            || status.deleteLoading
-        ))
-    );
+    render({
+        className,
+        concepts: {
+            concepts:           conceptsData,
+            error:              conceptsError,
+            loading:            conceptsLoading,
+        },
+        documentPlans,
+        planList,
+        variantsApi,
+    }, _, {
+        openedDataFileError,
+        openedDataFileLoading,
+    }) {
+        const planStatuses =    props( planList.uids, documentPlans.statuses );
 
-    const isReady = !isError && !isLoading;
+        const isError = (
+            conceptsError
+            || ( ! conceptsData && ! conceptsLoading )
+            || openedDataFileError
+            || planList.addCheckError
+            || planList.getListError
+            || variantsApi.error
+        );
 
-    return (
-        <div className={ className }>
-            { isLoading &&
-                <Loading justIcon message="Syncing... " />
-            }
-            { isError &&
-                <Error justIcon message="There are some errors." />
-            }
-            { isReady &&
-                <span>✅</span>
-            }
-        </div>
-    );
+        const isLoading = (
+            conceptsLoading
+            || variantsApi.loading
+            || openedDataFileLoading
+            || planList.getListLoading
+            || planStatuses.find( status => (
+                status.createLoading
+                || status.readLoading
+                || status.updateLoading
+                || status.deleteLoading
+            ))
+        );
+
+        const isReady = !isError && !isLoading;
+
+        return (
+            <div className={ className }>
+                { isLoading &&
+                    <Loading justIcon message="Syncing... " />
+                }
+                { isError &&
+                    <Error justIcon message="There are some errors." />
+                }
+                { isReady &&
+                    <span>✅</span>
+                }
+            </div>
+        );
+    }
 }));
