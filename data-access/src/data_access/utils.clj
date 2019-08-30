e1a99f62d5fe45f701111940fe28e6f4c0cb397c
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2019-08-30 14:43:53 +0300

    refactor: use integer type timestamps

--- data-access/src/data_access/utils.clj
+++ data-access/src/data_access/utils.clj
@@ -1,19 +1,11 @@
 (ns data-access.utils
-  (:require [clj-time.core :as time]
-            [clj-time.coerce :as tc]
-            [clojure.data.csv :as csv]
-            [clojure.string :as str]
-            [clojure.tools.logging :as log])
+  (:require [clojure.data.csv :as csv]
+            [clojure.string :as str])
   (:import (java.util UUID)
            (java.time Instant)))
 
-(defn gen-uuid [] (str (java.util.UUID/randomUUID)))
+(defn gen-uuid []
+  (str (java.util.UUID/randomUUID)))
 
-(defn ts-now [] (tc/to-long (time/now)))
-
-(defn raw-csv->maps [input]
-  (let [[header & data] (csv/read-csv input)]
-    (mapv zipmap (repeat header) data)))
-
-(defn ts-to-string [ts]
-  (when ts (str (Instant/ofEpochMilli ts))))
+(defn ts-now []
+  (int (.getEpochSecond (Instant/now))))
--- graphql-api/src/graphql/domain/document_plan.clj
+++ graphql-api/src/graphql/domain/document_plan.clj
@@ -3,14 +3,8 @@
             [data-access.entities.document-plan :as dp]
             [data-access.utils :refer [ts-to-string]]))
 
-(defn- document-plan-ts->string [document-plan]
-  (-> document-plan
-      (update :createdAt ts-to-string)
-      (update :updatedAt ts-to-string)))
-
 (defn get-document-plan [_ {:keys [id]} _]
-  (document-plan-ts->string
-    (dp/get-document-plan id)))
+  (dp/get-document-plan id))
 
 (defn delete-document-plan [_ {:keys [id]} _]
   (dp/delete-document-plan id)
@@ -18,15 +12,13 @@
 
 (defn list-document-plans [_ {:keys [offset limit] :or {offset 0 limit 20}} _]
   (let [items (dp/list-document-plans)]
-    {:items      (->> items (drop offset) (take limit) (map document-plan-ts->string))
+    {:items      (->> items (drop offset) (take limit))
      :limit      limit
      :offset     offset
      :totalCount (count items)}))
 
 (defn add-document-plan [_ {:keys [id] :as args} _]
-  (document-plan-ts->string
-    (dp/add-document-plan id (update args :blocks #(json/parse-string % true)))))
+  (dp/add-document-plan id (update args :blocks #(json/parse-string % true))))
 
 (defn update-document-plan [_ {:keys [id] :as args} _]
-  (document-plan-ts->string
-    (dp/update-document-plan id (update args :blocks #(json/parse-string % true)))))
+  (dp/update-document-plan id (update args :blocks #(json/parse-string % true))))
