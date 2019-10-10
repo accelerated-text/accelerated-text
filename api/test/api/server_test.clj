fba15e20403a22857b07025e972899e9e953e26e
Rokas <rokas.ramanauskas@tokenmill.lt>
2019-10-10 10:36:31 +0300

    test: add test for nlg results

--- api/test/api/server_test.clj
+++ api/test/api/server_test.clj
@@ -17,4 +17,11 @@
                            :documentPlanId   "test"
                            :readerFlagValues {}})]
       (is (= 200 status))
+      (is (some? result-id))))
+  (testing "NLG results"
+    (let [{{result-id :resultId} :body status :status}
+          (q "/nlg" :post {:documentPlanId "070cd048-b2b9-4890-bdcc-4420ac449c86"
+                           :readerFlagValues {}
+                           :dataId "example-user/books.csv"})]
+      (is (= 200 status))
       (is (some? result-id)))))
