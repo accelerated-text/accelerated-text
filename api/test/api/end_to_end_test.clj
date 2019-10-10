e3f13957bc4daece17ca79727fa83124133dfcb2
Zygimantas Medelis <zygimantas.medelis@tokenmill.lt>
2019-10-10 15:21:34 +0300

    test: add test for failing simple plan

--- api/test/api/end_to_end_test.clj
+++ api/test/api/end_to_end_test.clj
@@ -1,16 +1,13 @@
 (ns api.end-to-end-test
-  (:require [cheshire.core :as cheshire]
-            [clojure.test :refer [deftest is testing use-fixtures]]
+  (:require [clojure.test :refer [deftest is testing use-fixtures]]
             [api.test-utils :refer [q]]
             [data.db.dynamo-ops :as ops]
             [data.entities.document-plan :as dp]
             [clojure.string :as string]))
 
-
 (defn prepare-environment [f]
   (System/setProperty  "aws.region" "eu-central-1")
-  (ops/write! (ops/db-access :blockly)
-              "1"
+  (ops/write! (ops/db-access :blockly) "1"
               {:uid        "01"
                :name       "authorship"
                :blocklyXml (slurp "test/resources/blockly/authorship.xml")
@@ -18,33 +15,57 @@
                {:segments
                 [{:children
                   [{:type      "AMR"
-                    :srcId     "W}lSg%-S(rQ*nmVp3fFV"
+                    :srcId     "WlSg"
                     :conceptId "author"
                     :dictionaryItem
-                    {:name   "author"
-                     :type   "Dictionary-item"
-                     :srcId  "jirq-V{WgCd+u[sdVWpd"
-                     :itemId "VB-author"}
+                    {:name  "author" :type   "Dictionary-item"
+                     :srcId "jirq"   :itemId "VB-author"}
                     :roles
-                    [{:name "agent"
-                      :children
-                      [{:name "authors" :type "Cell" :srcId "uakxT`=!W@8/xV#^orHk"}]}
-                     {:name "co-agent"
-                      :children
-                      [{:name "title" :type "Cell" :srcId "X_CwogT+.Z)N@;Mhz.j/"}]}
+                    [{:name     "agent"
+                      :children [{:name "authors" :type "Cell" :srcId "uakxT"}]}
+                     {:name     "co-agent"
+                      :children [{:name "title" :type "Cell" :srcId "X_Cw"}]}
                      {:name "theme" :children [nil]}]}]
-                  :type  "Segment"
-                  :srcId "}0Ci`hF%i?izegwAT[@J"}]
-                :type  "Document-plan"
-                :srcId "eoPNHZ1PSV{MJBwehL^Z"}}
+                  :type "Segment" :srcId "0Ci"}]
+                :type "Document-plan" :srcId "eoPNHZ1"}}
+              true)
+  (ops/write! (ops/db-access :blockly) "2"
+              {:uid        "02"
+               :name       "title-only"
+               :blocklyXml (slurp "test/resources/blockly/title-only.xml")
+               :documentPlan
+               {:segments [{:children [{:name "title" :type "Cell" :srcId "isiyTw"}]
+                            :type     "Segment" :srcId "%!Y"}]
+                :type     "Document-plan"
+                :srcId    "G=Rh"}}
               true)
   (f)
-  (dp/delete-document-plan "1"))
+  (dp/delete-document-plan "1")
+  (dp/delete-document-plan "2"))
 
 (use-fixtures :each prepare-environment)
 
-(deftest full-document-plan-generation
-  (testing "NLG results"
+(deftest single-element-plan-generation
+  (testing "Single title element plan"
+    (let [{{result-id :resultId} :body status :status}
+          (q "/nlg" :post {:documentPlanId "2"
+                           :readerFlagValues {}
+                           :dataId "example-user/books.csv"})]
+
+      (Thread/sleep 1000)
+      (is (= 200 status))
+      (is (some? result-id))
+      (let [generation-results (q (str "/nlg/" result-id) :get nil)]
+        (is (not(string/blank?
+                 (->> (get-in generation-results [:body :variants])
+                      (first) (:children)
+                      (first) (:children)
+                      (first) (:children)
+                      (map :text)
+                      (string/join " ")))))))))
+
+(deftest authorship-document-plan-generation
+  (testing "Authorship plan"
     (let [{{result-id :resultId} :body status :status}
           (q "/nlg" :post {:documentPlanId "1"
                            :readerFlagValues {}
