c9114b9f81000983379f48a612f75754cceb633c
Zygimantas Medelis <zygimantas.medelis@tokenmill.lt>
2019-10-17 13:40:39 +0300

    refactor: remove logging

--- core/src/acc_text/nlg/combinator.clj
+++ core/src/acc_text/nlg/combinator.clj
@@ -40,19 +40,18 @@
 (defn additional-signs
   "Additional words to be included when building sentence"
   [^Grammar grammar]
-  (let [words (list "and" "," "." "the" "is")
-        word-fn (partial utils/word->sign grammar)
+  (let [words    ["and" "," "." "the" "is"]
+        word-fn  (partial utils/word->sign grammar)
         implicit (->> (.lexicon grammar)
                       (.getWords)
                       (extract-words)
                       (filter filter-implicit)
                       (map word-fn)
                       (flatten))
-        sign-fn (partial utils/str->sign grammar)]
-    (log/debugf "Implicit words: %s" (pr-str (map utils/sign->str implicit)))
+        sign-fn  (partial utils/str->sign grammar)]
     (concat
-     (flatten (map sign-fn words))
-     implicit)))
+      (flatten (map sign-fn words))
+      implicit)))
 
 (defn prune
   "Remove dupe pair"
@@ -73,7 +72,6 @@
    (let [signs (->> (concat initial-signs (additional-signs grammar))
                     (remove nil?)
                     (distinct))
-         _ (log/debugf "%d initial signs" (count signs))
          ident (set (map #(vec [% %]) signs))]
      (combinate grammar nil signs ident 0 max-depth)))
 
