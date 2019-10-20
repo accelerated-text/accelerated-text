caa4d05a4de714477b18f3c6fe28878d937800b8
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2019-10-21 00:00:23 +0300

    test: update parser ng integration tests

--- api/test/api/nlg/generator/parser_ng_test.clj
+++ api/test/api/nlg/generator/parser_ng_test.clj
@@ -4,27 +4,27 @@
             [clojure.test :refer [deftest is]]))
 
 (deftest simple-plan-parser
-  (is (= [[[{:dynamic [{:name  {:cell :product-name :dyn-name "$1"}
-                        :attrs {:type :product :source :cell}}]}]]]
+  (is (= {:static  []
+          :dynamic [{:name  {:cell :product-name :dyn-name "$1"}
+                     :attrs {:type :product :source :cell}}]}
          (sut/parse-document-plan (load-test-document-plan "single-subj") nil nil)))
 
-  (is (= [[[{:dynamic [{:name  {:cell :product-name :dyn-name "$1"}
-                        :attrs {:source :cell
-                                :type   :cell}}]}]]]
+  (is (= {:static  []
+          :dynamic [{:name  {:cell :product-name :dyn-name "$1"}
+                     :attrs {:source :cell
+                             :type   :cell}}]}
          (sut/parse-document-plan (load-test-document-plan "simple-plan") nil nil))))
 
 (deftest ^:integration amr-plan-parser
-  (let [[{quotes-dynamic :dynamic} {agent-dynamic :dynamic} {coagent-dynamic :dynamic}]
-        (-> (load-test-document-plan "plain-amr")
-            (sut/parse-document-plan  nil nil)
-            first first)]
-    (is (= [{:name  {:cell :actor :dyn-name "$2"}
-             :attrs {:amr true :title "Agent" :source :cell :type :cell}}]
+  (let [[quotes-dynamic agent-dynamic coagent-dynamic]
+        (:dynamic (sut/parse-document-plan (load-test-document-plan "plain-amr") nil nil))]
+    (is (= {:name  {:cell :actor :dyn-name "$2"}
+            :attrs {:amr true :title "Agent" :source :cell :type :cell}}
            agent-dynamic))
-    (is (= [{:name {:cell :co-actor :dyn-name "$3"}
-             :attrs {:amr true :title "co-Agent" :source :cell :type :cell}}]
+    (is (= {:name  {:cell :co-actor :dyn-name "$3"}
+            :attrs {:amr true :title "co-Agent" :source :cell :type :cell}}
            coagent-dynamic))
-    (let [{:keys [attrs name]} (first quotes-dynamic)]
+    (let [{:keys [attrs name]} quotes-dynamic]
       (is (= {:source :quotes :type :amr} attrs))
       (is (= "$1" (:dyn-name name)))
       (is (= #{"$2 watches $3" "$2 observe $3" "$2 sees $3"} (set (map :value (:quotes name))))))))
