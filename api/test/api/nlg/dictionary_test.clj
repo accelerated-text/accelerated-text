9f0a55c544a7c75ed3d2a9c96d3c83296053f4c5
Dainius Jocas <dainius@tokenmill.lt>
2019-10-22 17:04:07 +0300

    fix: dictionary-tests to use fixtures

--- api/test/api/nlg/dictionary_test.clj
+++ api/test/api/nlg/dictionary_test.clj
@@ -1,17 +1,21 @@
 (ns api.nlg.dictionary-test
-  (:require [api.nlg.dictionary :as dictionary]
-            [clojure.test :refer [deftest testing is]]))
+  (:require [api.graphql.ddb-fixtures :as ddb-fixtures]
+            [api.nlg.dictionary :as dictionary]
+            [api.test-utils :refer [q]]
+            [clojure.test :refer [deftest testing is use-fixtures]]))
 
-(deftest test-filter-with-not
-  (let [data (list {:text "test1" :flags {:default :YES :f1 :DONT_CARE :f2 :DONT_CARE}}
-                   {:text "test2" :flags {:default :YES :f1 :YES :f2 :NO}}
-                   {:text "test3" :flags {:default :YES :f1 :YES :f2 :DONT_CARE}}
-                   {:text "test4" :flags {:default :NO :f1 :DONT_CARE :f2 :YES}})]
-    (testing "Should filter out 'test2' because f2 = NO"
-      (is (= #{"test1" "test3", "test4"} (set (dictionary/filter-by-profile data {:f1 true :f2 true})))))
-    (testing "Should return all values, because we don't give f2 profile"
-      (is (= #{"test1" "test2" "test3"} (set (dictionary/filter-by-profile data {:f1 true :f2 false})))))))
+(use-fixtures :each ddb-fixtures/wipe-ddb-tables)
 
 (deftest ^:integration query-database
-  (let [result (dictionary/search "see" {:junior true :senior true})]
+  (let [query "mutation CreateDictionaryItem($name:String! $partOfSpeech:PartOfSpeech){createDictionaryItem(name:$name partOfSpeech:$partOfSpeech){name partOfSpeech}}"
+        _ (q "/_graphql" :post {:query query :variables {:name "see"}})
+        query "mutation CreatePhrase($dictionaryItemId:ID! $text:String! $defaultUsage:DefaultUsage){createPhrase(dictionaryItemId:$dictionaryItemId text:$text defaultUsage:$defaultUsage){phrases{id text}}}"
+        _ (q "/_graphql" :post {:query query :variables {:dictionaryItemId "see"
+                                                         :text             "see"
+                                                         :defaultUsage     "YES"
+                                                         :readerFlagUsage []}})
+        _ (q "/_graphql" :post {:query query :variables {:dictionaryItemId "see"
+                                                         :text             "watch"
+                                                         :defaultUsage     "YES"}})
+        result (dictionary/search "see" {:junior true :senior true})]
     (is (= #{"see" "watch"} (set result)))))
