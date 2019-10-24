8280dde505c7eaffc54dd4a8d99dd5c01457d3d8
Dainius Jocas <dainius@tokenmill.lt>
2019-10-24 14:37:37 +0300

    refactor: mount verbclass db

--- api/src/data/entities/amr.clj
+++ api/src/data/entities/amr.clj
@@ -1,17 +1,20 @@
 (ns data.entities.amr
   (:require [data.db.amr-rules :as amr-rules]
-            [data.db.dynamo-ops :as ops]))
+            [data.db.dynamo-ops :as ops]
+            [mount.core :refer [defstate]]))
+
+(defstate verbclass-db :start (ops/db-access :verbclass))
 
 (defn list-verbclasses []
   (concat
-    (ops/list! (ops/db-access :verbclass) 100)
+    (ops/list! verbclass-db 100)
     (amr-rules/list-all)))
 
 (defn get-verbclass [k]
   (if (contains? amr-rules/rules (keyword k))
     (get amr-rules/rules (keyword k))
-    (ops/read! (ops/db-access :verbclass) k)))
+    (ops/read! verbclass-db k)))
 
 (defn create-verbclass
   [{:keys [id _ _ _] :as req}]
-  (ops/write! (ops/db-access :verbclass) id req))
+  (ops/write! verbclass-db   id req))
