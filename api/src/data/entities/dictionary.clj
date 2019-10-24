7bdf4d30f398872688f7b53b12f1b6db5f3d2b13
Dainius Jocas <dainius@tokenmill.lt>
2019-10-24 14:41:54 +0300

    refactor: mount dictionary db

--- api/src/data/entities/dictionary.clj
+++ api/src/data/entities/dictionary.clj
@@ -1,10 +1,14 @@
 (ns data.entities.dictionary
   (:require [clojure.string :as str]
             [data.db.dynamo-ops :as ops]
-            [data.utils :as utils]))
+            [data.utils :as utils]
+            [mount.core :refer [defstate]]))
+
+(defstate reader-flags-db :start (ops/db-access :reader-flag))
+(defstate dictionary-compined-db :start (ops/db-access :dictionary-combined))
 
 (defn list-readers []
-  (ops/list! (ops/db-access :reader-flag) 100))
+  (ops/list! reader-flags-db 100))
 
 (defn get-default-flags []
   (->> (list-readers)
@@ -12,14 +16,14 @@
        (into {})))
 
 (defn get-reader [key]
-  (ops/read! (ops/db-access :reader-flag) key))
+  (ops/read! reader-flags-db key))
 
 (defn list-dictionary []
-  (ops/list! (ops/db-access :dictionary-combined) 100))
+  (ops/list! dictionary-compined-db 100))
 
 (defn get-dictionary-item [key]
   (when-not (str/blank? key)
-    (ops/read! (ops/db-access :dictionary-combined) key)))
+    (ops/read! dictionary-compined-db key)))
 
 (defn text->phrase
   ([text parent-id default-usage]
@@ -31,12 +35,12 @@
 
 (defn create-dictionary-item [{:keys [key name phrases partOfSpeech]}]
   (when-not (str/blank? name)
-    (ops/write! (ops/db-access :dictionary-combined) key {:name         name
-                                                          :partOfSpeech partOfSpeech
-                                                          :phrases      (map #(text->phrase % key :YES) phrases)})))
+    (ops/write! dictionary-compined-db key {:name         name
+                                            :partOfSpeech partOfSpeech
+                                            :phrases      (map #(text->phrase % key :YES) phrases)})))
 
 (defn delete-dictionary-item [key]
-  (ops/delete! (ops/db-access :dictionary-combined) key))
+  (ops/delete! dictionary-compined-db key))
 
 (defn update-dictionary-item [item]
-  (ops/update! (ops/db-access :dictionary-combined) (:key item) (dissoc item :key)))
+  (ops/update! dictionary-compined-db (:key item) (dissoc item :key)))
