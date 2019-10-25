932e1094e5fec3f37e1ec2ceb529d175135ed532
Zygimantas Medelis <zygimantas.medelis@tokenmill.lt>
2019-10-25 12:02:02 +0300

    feat: more fully define AMR spec

--- api/src/api/nlg/parser.clj
+++ api/src/api/nlg/parser.clj
@@ -3,39 +3,41 @@
             [clojure.spec.alpha :as s]
             [clojure.spec.gen.alpha :as gen]))
 
-(s/def ::concept/id string?)
+(s/def :acctext.amr/id aphanumeric?)
+(s/def :acctext.amr/name string?)
 
-(s/def ::concept/type #{:root :segment :amr :relationship :data :quote :dictionary-item :unk})
+;;FIXME
+;; - 'root' is should be renamed to 'document-plan'
+;; - 'relationship' is not really a concept node type?
+;; - 'unk' should not be allowed, if we want to model it  do it via (s/or :valid ... :bad ...)construction
+(s/def :acctext.amr/type #{:root :segment :amr :relationship :data :quote :dictionary-item :unk})
 
-(s/def ::concept (s/keys :req [:id :type]))
+(s/def :acctext.amr/concept (s/keys :req [:acctext.amr/id :acctext.amr/type]))
 
-(s/def ::concepts (s/coll-of ::concept ::gen-max 5))
+(s/def :acctext.amr/concepts (s/coll-of :acctext.amr/concept ::gen-max 5))
 
+(s/def :acctext.amr/role
+  (s/or :core (s/with-gen keyword?
+                #(gen/fmap (fn [idx] (keyword (str "ARG" (Math/abs ^Integer idx)))) (gen/int)))
+        :non-core #{:segment :instance :relationship :modifier}))
 
-(s/def ::role/core
-  (s/with-gen keyword?
-              #(gen/fmap (fn [idx] (keyword (str "ARG" (Math/abs ^Integer idx)))) (gen/int))))
+(s/def :acctext.amr/attributes
+  (s/or :has-attrs (s/keys :req [:acctext.amr/name])
+        :no-attrs nil?))
 
-(s/def ::role/non-core #{:segment :instance :relationship :modifier})
+(s/def :acctext.amr/relation
+  (s/cat :from :acctext.amr/id
+         :to :acctext.amr/id
+         :role :acctext.amr/role
+         :attributes :acctext.amr/attributes))
 
-(s/def ::relation/role (s/or :core :core :non-core :non-core))
+(s/def :acctext.amr/relations (s/coll-of ::relation :gen-max 5))
 
-(s/def ::relation/attributes (s/keys :opt [:name]))
-
-(s/def ::relation
-  (s/cat :from ::concept/id
-         :to ::concept/id
-         :role :role
-         :attributes :attributes))
-
-(s/def ::relations (s/coll-of ::relation :gen-max 5))
-
-
-(s/def ::amr (s/keys :req [::relations ::concepts]))
+(s/def :acctext.amr/graph (s/keys :req [:acctext.amr/relations :acctext.amr/concepts]))
 
 (defn parse-document-plan [document-plan]
   (parser/parse document-plan))
 
 (s/fdef parse-document-plan
         :args (s/cat :document-plan any?)
-        :ret ::amr)
+        :ret :acctext.amr/graph)
