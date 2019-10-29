e0866ad6df781060cebadc430c24b1f001aa400e
Rokas <rokas.ramanauskas@tokenmill.lt>
2019-10-29 12:10:54 +0200

    refactor: add specs to document plan parser tests

--- api/test/api/nlg/parser_test.clj
+++ api/test/api/nlg/parser_test.clj
@@ -11,9 +11,9 @@
                                         :itemId "NN-good"}]
                             :type     "Segment"}]
                 :type     "Document-plan"}
-          {:keys [concepts relations]} (parser/parse-document-plan root)]
-      (is (set/subset? #{:document-plan :segment :data :dictionary-item} (set (map :type concepts))))
-      (is (set/subset? #{:segment :instance :modifier} (set (map :role relations))))))
+          {concepts :acctext.amr/concepts relations :acctext.amr/relations} (parser/parse-document-plan root)]
+      (is (set/subset? #{:document-plan :segment :data :dictionary-item} (set (map :acctext.amr/type concepts))))
+      (is (set/subset? #{:segment :instance :modifier} (set (map :acctext.amr/role relations))))))
   (testing "AMR parsing"
     (let [root {:roles          [{:children [{:name "authors" :type "Cell"}]
                                   :name     "agent"}
@@ -25,9 +25,9 @@
                                  :itemId "written"}
                 :type           "AMR"
                 :conceptId      "author"}
-          {:keys [concepts relations]} (parser/parse-document-plan root)]
-      (is (set/subset? #{:data :dictionary-item} (set (map :type concepts))))
-      (is (set/subset? #{:ARG0 :ARG1} (set (map :role relations))))))
+          {concepts :acctext.amr/concepts relations :acctext.amr/relations} (parser/parse-document-plan root)]
+      (is (set/subset? #{:data :dictionary-item} (set (map :acctext.amr/type concepts))))
+      (is (set/subset? #{:ARG0 :ARG1} (set (map :acctext.amr/role relations))))))
   (testing "Modifier parsing"
     (let [root {:name   "good"
                 :type   "Dictionary-item-modifier"
@@ -36,7 +36,7 @@
                          :child  {:name "authors" :type "Cell"}
                          :itemId "famous"}
                 :itemId "good"}
-          {:keys [concepts relations]} (parser/parse-document-plan root)]
-      (is (set/subset? #{:data :dictionary-item} (set (map :type concepts))))
-      (is (set/subset? #{:modifier} (set (map :role relations))))
+          {concepts :acctext.amr/concepts relations :acctext.amr/relations} (parser/parse-document-plan root)]
+      (is (set/subset? #{:data :dictionary-item} (set (map :acctext.amr/type concepts))))
+      (is (set/subset? #{:modifier} (set (map :acctext.amr/role relations))))
       (is (= 2 (count relations))))))
