11e2969f84e59c5bc3f7932ec462b25d471208d8
Rokas Ramanauskas <rokasramas@gmail.com>
2019-10-30 16:11:11 +0200

    169423118 generate document plan instances (#20)
    * add document plan context building functions
    * add document plan instance building functions
    * keep dictionary api search results in vec when building dictionary

---
+++ api/src/api/nlg/instances.clj
@@ -0,0 +1,39 @@
+(ns api.nlg.instances
+  (:require [api.nlg.dictionary :as dictionary-api]
+            [clojure.string :as str]))
+
+(defn get-dictionary-items [document-plan]
+  (->> (get document-plan :acctext.amr/concepts)
+       (filter (fn [{type :acctext.amr/type}]
+                 (= type :dictionary-item)))
+       (map :acctext.amr/value)
+       (set)))
+
+(defn build-dictionary-for-profile [dictionary-items reader-profile]
+  (reduce (fn [m value]
+            (assoc m value (dictionary-api/search (str/lower-case value) reader-profile)))
+          {}
+          dictionary-items))
+
+(defn build-dictionary [dictionary-items reader-profiles]
+  (reduce (fn [m reader-profile]
+            (assoc m reader-profile (build-dictionary-for-profile dictionary-items reader-profile)))
+          {}
+          reader-profiles))
+
+(defmulti add-context (fn [concept _] (get concept :acctext.amr/type)))
+
+(defmethod add-context :default [concept _] concept)
+
+(defmethod add-context :dictionary-item [{value :acctext.amr/value :as concept} {:keys [dictionary reader-profile]}]
+  (-> concept
+      (assoc :acctext.amr/members (get dictionary value))
+      (assoc-in [:acctext.amr/attributes :acctext.amr/reader-profile] reader-profile)))
+
+(defn build-instances [document-plan reader-profiles]
+  (let [dictionary-items (get-dictionary-items document-plan)
+        dictionary (build-dictionary dictionary-items reader-profiles)]
+    (for [reader-profile reader-profiles]
+      (let [context {:reader-profile reader-profile
+                     :dictionary     (get dictionary reader-profile)}]
+        (update document-plan :acctext.amr/concepts #(map (fn [concept] (add-context concept context)) %))))))
---
+++ api/test/api/nlg/instances_test.clj
@@ -0,0 +1,65 @@
+(ns api.nlg.instances-test
+  (:require [api.graphql.ddb-fixtures :as ddb-fixtures]
+            [api.nlg.instances :as instances]
+            [api.nlg.parser :as parser]
+            [api.test-utils :refer [q load-test-document-plan]]
+            [clojure.test :refer [deftest is testing use-fixtures]]))
+
+(defn prepare-environment [f]
+  (let [create-dict-item-query "mutation CreateDictionaryItem($name:String! $partOfSpeech:PartOfSpeech){createDictionaryItem(name:$name partOfSpeech:$partOfSpeech){name partOfSpeech}}"
+        create-phrase-query "mutation CreatePhrase($dictionaryItemId:ID! $text:String! $defaultUsage:DefaultUsage){createPhrase(dictionaryItemId:$dictionaryItemId text:$text defaultUsage:$defaultUsage){phrases{id text}}}"]
+    (q "/_graphql" :post {:query create-dict-item-query :variables {:name "good"}})
+    (q "/_graphql" :post {:query create-dict-item-query :variables {:name "written"}})
+    (q "/_graphql" :post {:query create-phrase-query :variables {:dictionaryItemId "good"
+                                                                 :text             "excellent"
+                                                                 :defaultUsage     "YES"}})
+    (q "/_graphql" :post {:query create-phrase-query :variables {:dictionaryItemId "written"
+                                                                 :text             "authored"
+                                                                 :defaultUsage     "YES"}})
+    (f)))
+
+(use-fixtures :each ddb-fixtures/wipe-ddb-tables prepare-environment)
+
+(deftest ^:integration dictionary-item-extraction
+  (let [document-plan (parser/parse-document-plan (load-test-document-plan "author-amr-with-adj"))]
+    (is (= #{"good" "written"} (instances/get-dictionary-items document-plan)))))
+
+(deftest ^:integration dictionary-building
+  (let [dictionary-items #{"good" "written"}]
+    (is (= {:default {"good"    ["excellent"]
+                      "written" ["authored"]}
+            :senior  {"good"    ["excellent"]
+                      "written" ["authored"]}}
+           (instances/build-dictionary dictionary-items [:default :senior])))))
+
+(deftest ^:integration context-adding
+  (testing "Dictionary item context adding"
+    (let [document-plan (parser/parse-document-plan (load-test-document-plan "author-amr-with-adj"))]
+      (is (= #{#:acctext.amr{:attributes #:acctext.amr{:name           "written"
+                                                       :reader-profile :default}
+                             :id         :03
+                             :members    ["authored"]
+                             :type       :dictionary-item
+                             :value      "written"}
+               #:acctext.amr{:attributes #:acctext.amr{:name           "good"
+                                                       :reader-profile :default}
+                             :id         :05
+                             :members    ["excellent"]
+                             :type       :dictionary-item
+                             :value      "good"}
+               #:acctext.amr{:attributes #:acctext.amr{:name           "written"
+                                                       :reader-profile :senior}
+                             :id         :03
+                             :members    ["authored"]
+                             :type       :dictionary-item
+                             :value      "written"}
+               #:acctext.amr{:attributes #:acctext.amr{:name           "good"
+                                                       :reader-profile :senior}
+                             :id         :05
+                             :members    ["excellent"]
+                             :type       :dictionary-item
+                             :value      "good"}}
+             (->> (instances/build-instances document-plan [:default :senior])
+                  (mapcat :acctext.amr/concepts)
+                  (filter #(= (:acctext.amr/type %) :dictionary-item))
+                  (set)))))))
