f1e097b17c6130a375e675d1b00c6c82dc37ac0e
Dainius Jocas <dainius@tokenmill.lt>
2019-10-30 11:55:38 +0200

    feat: multimethods for dataomic reads and writes

--- api/src/data/datomic/impl.clj
+++ api/src/data/datomic/impl.clj
@@ -8,19 +8,31 @@
 (defstate conn
   :start (d/connect (:db-uri conf)))
 
-(defn transact-item [resource-type key data-item]
+(defmulti transact-item (fn [resource-type _ _] resource-type))
+
+(defmethod transact-item :data-files [_ key data-item]
   (d/transact conn
               [{:data-file/id       key
                 :data-file/filename (:filename data-item)
                 :data-file/content  (:content data-item)}]))
 
-(defn pull-entity [resource-type key]
-  (log/trace key)
+(defmethod transact-item :default [resource-type key _]
+  (log/warnf "Default implementation of transact-item for the '%s' with key '%s'"
+             resource-type key))
+
+(defmulti pull-entity (fn [resource-type _] resource-type))
+
+(defmethod pull-entity :data-files [resource-type key]
+  (log/debugf "Pulling entity %s with key %s" resource-type key)
   (let [df (ffirst (d/q '[:find (pull ?e [*])
                           :where
                           [?e :data-file/id ?key]]
                         (d/db conn)))]
     {:content (:data-file/content df)}))
+
+(defmethod pull-entity :default [resource-type key]
+  (log/warnf "Default implementation of pull-entity for the '%s' with key '%s'"
+             resource-type key))
 
 (defn db-access
   [resource-type config]
