eff5c1c8bd3866c0adf226a3165822f8a7189b1c
Rokas <rokas.ramanauskas@tokenmill.lt>
2019-10-30 09:45:30 +0200

    refactor: create environment prepare function for context tests

--- api/test/api/nlg/context_test.clj
+++ api/test/api/nlg/context_test.clj
@@ -5,35 +5,28 @@
             [api.test-utils :refer [q load-test-document-plan]]
             [clojure.test :refer [deftest is use-fixtures]]))
 
-(use-fixtures :each ddb-fixtures/wipe-ddb-tables)
+(defn prepare-environment [f]
+  (let [create-dict-item-query "mutation CreateDictionaryItem($name:String! $partOfSpeech:PartOfSpeech){createDictionaryItem(name:$name partOfSpeech:$partOfSpeech){name partOfSpeech}}"
+        create-phrase-query "mutation CreatePhrase($dictionaryItemId:ID! $text:String! $defaultUsage:DefaultUsage){createPhrase(dictionaryItemId:$dictionaryItemId text:$text defaultUsage:$defaultUsage){phrases{id text}}}"]
+    (q "/_graphql" :post {:query create-dict-item-query :variables {:name "good"}})
+    (q "/_graphql" :post {:query create-dict-item-query :variables {:name "written"}})
+    (q "/_graphql" :post {:query create-phrase-query :variables {:dictionaryItemId "good"
+                                                                 :text             "excellent"
+                                                                 :defaultUsage     "YES"}})
+    (q "/_graphql" :post {:query create-phrase-query :variables {:dictionaryItemId "written"
+                                                                 :text             "authored"
+                                                                 :defaultUsage     "YES"}})
+    (f)))
+
+(use-fixtures :each ddb-fixtures/wipe-ddb-tables prepare-environment)
 
 (deftest dictionary-building
-  (let [document-plan (parser/parse-document-plan (load-test-document-plan "author-amr-with-adj"))
-        query "mutation CreateDictionaryItem($name:String! $partOfSpeech:PartOfSpeech){createDictionaryItem(name:$name partOfSpeech:$partOfSpeech){name partOfSpeech}}"
-        _ (q "/_graphql" :post {:query query :variables {:name "good"}})
-        _ (q "/_graphql" :post {:query query :variables {:name "written"}})
-        query "mutation CreatePhrase($dictionaryItemId:ID! $text:String! $defaultUsage:DefaultUsage){createPhrase(dictionaryItemId:$dictionaryItemId text:$text defaultUsage:$defaultUsage){phrases{id text}}}"
-        _ (q "/_graphql" :post {:query query :variables {:dictionaryItemId "good"
-                                                         :text             "excellent"
-                                                         :defaultUsage     "YES"}})
-        _ (q "/_graphql" :post {:query query :variables {:dictionaryItemId "written"
-                                                         :text             "authored"
-                                                         :defaultUsage     "YES"}})]
+  (let [document-plan (parser/parse-document-plan (load-test-document-plan "author-amr-with-adj"))]
     (is (= {"good"    ["excellent"]
             "written" ["authored"]} (context/build-dictionary document-plan :default)))))
 
 (deftest context-building
-  (let [document-plan (parser/parse-document-plan (load-test-document-plan "author-amr-with-adj"))
-        query "mutation CreateDictionaryItem($name:String! $partOfSpeech:PartOfSpeech){createDictionaryItem(name:$name partOfSpeech:$partOfSpeech){name partOfSpeech}}"
-        _ (q "/_graphql" :post {:query query :variables {:name "good"}})
-        _ (q "/_graphql" :post {:query query :variables {:name "written"}})
-        query "mutation CreatePhrase($dictionaryItemId:ID! $text:String! $defaultUsage:DefaultUsage){createPhrase(dictionaryItemId:$dictionaryItemId text:$text defaultUsage:$defaultUsage){phrases{id text}}}"
-        _ (q "/_graphql" :post {:query query :variables {:dictionaryItemId "good"
-                                                         :text             "excellent"
-                                                         :defaultUsage     "YES"}})
-        _ (q "/_graphql" :post {:query query :variables {:dictionaryItemId "written"
-                                                         :text             "authored"
-                                                         :defaultUsage     "YES"}})]
+  (let [document-plan (parser/parse-document-plan (load-test-document-plan "author-amr-with-adj"))]
     (is (= [#:acctext.amr{:dictionary {"written" ["authored"]
                                        "good"    ["excellent"]}
                           :data       {"authors" "X1" "title" "Y1"}}
