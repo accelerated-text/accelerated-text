64c6d023fa9d59c65f150b7df5683d8b12b4de2c
Dainius Jocas <dainius@tokenmill.lt>
2019-10-31 15:59:03 +0200

    feat: deletion of document plans

--- api/src/data/datomic/impl.clj
+++ api/src/data/datomic/impl.clj
@@ -1,10 +1,10 @@
 (ns data.datomic.impl
   (:require [api.config :refer [conf]]
+            [clojure.tools.logging :as log]
             [data.protocol :as protocol]
             [data.utils :as utils]
             [datomic.api :as d]
-            [mount.core :refer [defstate]]
-            [clojure.tools.logging :as log]))
+            [mount.core :refer [defstate]]))
 
 (defstate conn
   :start (d/connect (:db-uri conf)))
@@ -55,13 +55,14 @@
 (defmulti pull-entity (fn [resource-type _] resource-type))
 
 (defmethod pull-entity :data-files [_ key]
-  (let [df (ffirst (d/q '[:find (pull ?e [*])
-                          :where
-                          [?e :data-file/id ?key]]
-                        (d/db conn)
-                        key))]
-    {:filename (:data-file/filename df)
-     :content (:data-file/content df)}))
+  (let [data-file (ffirst (d/q '[:find (pull ?e [*])
+                                 :where
+                                 [?e :data-file/id ?key]]
+                               (d/db conn)
+                               key))]
+    (when data-file
+      {:filename (:data-file/filename data-file)
+       :content  (:data-file/content data-file)})))
 
 (defmethod pull-entity :dictionary-combined [_ key]
   (let [dictionary-entry (ffirst (d/q '[:find (pull ?e [*])
@@ -69,24 +70,26 @@
                                         :where [?e :dictionary-combined/id ?key]]
                                       (d/db conn)
                                       key))]
-    {:key     (:dictionary-combined/id dictionary-entry)
-     :name    (:dictionary-combined/name dictionary-entry)
-     :phrases (:dictionary-combined/phrases dictionary-entry)}))
+    (when dictionary-entry
+      {:key     (:dictionary-combined/id dictionary-entry)
+       :name    (:dictionary-combined/name dictionary-entry)
+       :phrases (:dictionary-combined/phrases dictionary-entry)})))
 
 (defmethod pull-entity :blockly [_ key]
   (let [document-plan (ffirst (d/q '[:find (pull ?e [*])
-                                        :in $ ?key
-                                        :where [?e :document-plan/id ?key]]
-                                      (d/db conn)
-                                      key))]
-    {:id           (:document-plan/id document-plan)
-     :uid          (:document-plan/uid document-plan)
-     :name         (:document-plan/name document-plan)
-     :blocklyXml   (:document-plan/blockly-xml document-plan)
-     :documentPlan (:document-plan/document-plan document-plan)
-     :createdAt    (:document-plan/created-at document-plan)
-     :updatedAt    (:document-plan/updated-at document-plan)
-     :updateCount  (:document-plan/update-count document-plan)}))
+                                     :in $ ?key
+                                     :where [?e :document-plan/id ?key]]
+                                   (d/db conn)
+                                   key))]
+    (when document-plan
+      {:id           (:document-plan/id document-plan)
+       :uid          (:document-plan/uid document-plan)
+       :name         (:document-plan/name document-plan)
+       :blocklyXml   (:document-plan/blockly-xml document-plan)
+       :documentPlan (:document-plan/document-plan document-plan)
+       :createdAt    (:document-plan/created-at document-plan)
+       :updatedAt    (:document-plan/updated-at document-plan)
+       :updateCount  (:document-plan/update-count document-plan)})))
 
 (defmethod pull-entity :default [resource-type key]
   (log/warnf "Default implementation of pull-entity for the '%s' with key '%s'"
@@ -132,6 +135,16 @@
              resource-type opts)
   (throw (RuntimeException. "NOT IMPLEMENTED")))
 
+(defmulti delete (fn [resource-type _] resource-type))
+
+(defmethod delete :blockly [_ key]
+  @(d/transact conn [[:db.fn/retractEntity [:document-plan/id key]]]))
+
+(defmethod delete :default [resource-type opts]
+  (log/warnf "Default implementation of DELETE for the '%s' with key '%s'"
+             resource-type opts)
+  (throw (RuntimeException. "NOT IMPLEMENTED")))
+
 (defn db-access
   [resource-type config]
   (log/debugf "Datomic for: %s with config %s" resource-type config)
@@ -142,7 +155,7 @@
     (write-item [this key data update-count?]
       (transact-item resource-type key data))
     (update-item [this key data] (throw (RuntimeException. "NOT IMPLEMENTED")))
-    (delete-item [this key] (throw (RuntimeException. "NOT IMPLEMENTED")))
+    (delete-item [this key] (delete resource-type key))
     (list-items [this limit] (pull-n resource-type limit))
     (scan-items [this opts] (scan resource-type opts))
     (batch-read-items [this ids] (throw (RuntimeException. "NOT IMPLEMENTED")))))
