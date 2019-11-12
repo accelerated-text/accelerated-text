82057eb8677e7d16db0a17fe855f5940402277d9
Šarūnas Navickas <sarunas.navickas@tokenmill.lt>
2019-11-12 15:35:22 +0200

    delete old stuff

--- core/test/acc_text/nlg/verbnet/grammar_patterns_test.clj
+++
@@ -1,9 +0,0 @@
-(ns acc-text.nlg.verbnet.grammar-patterns-test
-  (:require [acc-text.nlg.verbnet.grammar-patterns :as sut]
-            [clojure.test :refer [deftest is]]))
-
-(deftest vnet-id->predicate
-  (is (= "aaa" (sut/drop-vnet-id "aaa")))
-  (is (= "aaa" (sut/drop-vnet-id "aaa-1")))
-  (is (= "aaa" (sut/drop-vnet-id "aaa-1.1"))))
-
