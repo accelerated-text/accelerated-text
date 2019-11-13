d4fc15cea8f83fda86289388d1369501e645b4d7
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2019-11-13 09:45:53 +0200

    Trying to fix mess

--- core/gf/server.py
+++ core/gf/server.py
@@ -3,45 +3,18 @@ import json
 import argparse
 import subprocess
 
+from wsgiref.util import setup_testing_defaults
+from wsgiref.simple_server import make_server
+
 from backports.tempfile import TemporaryDirectory
-
-import pgf
-
-try:
-    import SocketServer as socketserver
-    from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
-except ImportError:
-    import socketserver
-    from http.server import HTTPServer, BaseHTTPRequestHandler
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-
-def setup_headers(fn):
-    def wrapper(self, *args, **kwargs):
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.end_headers()
-        return fn(self, *args, **kwargs)
-    return wrapper
-
-
-def json_response(fn):
-    def wrapper(self, *args, **kwargs):
-        logger.info("Doing json response")
-        result = fn(self, *args, **kwargs)
-        self.wfile.write(json.dumps(result).encode("UTF-8"))
-    return wrapper
-
-
-def json_request(fn):
-    def wrapper(self, *args, **kwargs):
-        content_length = int(self.headers["Content-Length"])
-        post_data = json.loads(self.rfile.read(content_length).decode("UTF-8"))
-        logger.debug("Got: {}".format(post_data))
-        return fn(self, post_data, *args, **kwargs)
-    return wrapper
+try:
+    import pgf
+except ImportError:
+    logger.exception("Failed to import module 'pgf'. It's GrammaticalFramework runtime library which needs to be compiled and installed")
 
 
 def compile_grammar(raw):
@@ -71,38 +44,81 @@ def compile_grammar(raw):
             return grammar
 
 
-class GFHandler(BaseHTTPRequestHandler):
-    @setup_headers
-    def do_HEAD(self):
-        pass
+def response_404(environ, start_response):
+    status = "404 NOT FOUND"
+    response_headers = []
+    start_response(status, response_headers)
+    return ""
 
-    @json_response
-    @setup_headers
-    @json_request
-    def do_POST(self, data):
-        grammar = compile_grammar(data["content"])
-        if grammar:
-            logger.info("Generating")
-            results = []
-            try:
-                logger.debug("Start category: {}".format(grammar.startCat))
-                expressions = list(grammar.generateAll(grammar.startCat))
-                lang = grammar.languages["grammar"]
-                logger.debug("Expressions: {}".format(expressions))
-                results = list([r
-                                for (_, e) in expressions
-                                for r in lang.linearizeAll(e)])
-            except Exception as ex:
-                logger.exception(ex)
-
-            logger.debug("Results: {}".format(results))
-            return {"results": results}
+def post_request(fn):
+    def wrapper(environ, *args, **kwargs):
+        setup_testing_defaults(environ)
+        if environ["REQUEST_METHOD"] == "POST":
+            return fn(environ, *args, **kwargs)
         else:
-            return {"results": []}
+            return response_404(environ, *args, **kwargs)
 
+    return wrapper
+
+
+def json_request(fn):
+    def wrapper(environ, start_response):
+        try:
+            request_body_size = int(environ.get("CONTENT_LENGTH", 0))
+        except ValueError:
+            request_body_size = 0
+
+        request_body = environ["wsgi.input"].read(request_body_size)
+        return fn(environ, start_response, json.loads(request_body))
+        
+    return wrapper
+
+
+def json_response(fn):
+    def wrapper(environ, start_response, *args):
+        status = "200 OK"
+        try:
+            response = fn(environ, start_response, *args)
+        except Exception as ex:
+            response = {"error": True, "message": str(ex)}
+
+        output = json.dumps(response).encode("UTF-8")
+        response_headers = [
+            ("Content-Type", "application/json"),
+            ("Content-Length", str(len(output)))
+        ]
+        start_response(status, response_headers)
+
+        return [output]
+    return wrapper
+
+
+@post_request
+@json_request
+@json_response
+def application(environ, start_response, data):
+    grammar = compile_grammar(data["content"])
+    if grammar:
+        logger.info("Generating")
+        results = []
+        try:
+            logger.debug("Start category: {}".format(grammar.startCat))
+            expressions = list(grammar.generateAll(grammar.startCat))
+            lang = grammar.languages["grammar"]
+            logger.debug("Expressions: {}".format(expressions))
+            results = list([r
+                            for (_, e) in expressions
+                            for r in lang.linearizeAll(e)])
+        except Exception as ex:
+            logger.exception(ex)
+
+        logger.debug("Results: {}".format(results))
+        return {"results": results}
+    else:
+        return {"results": []}
 
 def main(args):
-    httpd = socketserver.TCPServer(("", args.port), GFHandler)
+    httpd = make_server("", args.port, application)
     logger.info("Serving on port: {}".format(args.port))
     try:
         httpd.serve_forever()
