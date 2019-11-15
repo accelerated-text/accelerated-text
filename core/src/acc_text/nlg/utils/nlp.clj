dbb39ef0bb67fb83c3289dfb24b286bb14d06185
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2019-11-15 11:23:49 +0200

    chore: add annotate fn to nlp ns

--- core/src/acc_text/nlg/utils/nlp.clj
+++ core/src/acc_text/nlg/utils/nlp.clj
@@ -7,6 +7,9 @@
 (defn tokenize [s]
   (map first (re-seq #"([\w+'`]+|[^\s\w+'`]+)" s)))
 
+(defn tokenize-incl-space [s]
+  (map first (re-seq #"([\w+'`]+|[\s]+|[^\s\w+'`]+)" s)))
+
 (defn word? [s]
   (some? (re-seq #"\w" s)))
 
@@ -15,3 +18,14 @@
 (defn process-sentence [s]
   (reduce str (when-not (str/blank? s)
                 (str/join [(str/capitalize (first s)) (apply str (rest s)) \.]))))
+
+(defn annotate [text]
+  {:text   text
+   :tokens (loop [[token & tokens] (tokenize-incl-space text)
+                  idx 0
+                  annotations []]
+             (if (nil? token)
+               annotations
+               (recur tokens (+ idx (count token)) (cond-> annotations
+                                                           (re-matches #"[^\s]+" token) (conj {:text token
+                                                                                               :idx  idx})))))})
