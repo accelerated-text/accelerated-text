d534e079e4f3961e1727fb16ee0afddd9f2648a6
Šarūnas Navickas <sarunas.navickas@tokenmill.lt>
2019-11-19 14:54:21 +0200

    Initial functions

--- core/src/acc_text/nlg/gf/translate_gf.clj
+++ core/src/acc_text/nlg/gf/translate_gf.clj
@@ -1,4 +1,7 @@
-(ns acc-text.nlg.gf.translate-gf)
+(ns acc-text.nlg.gf.translate-gf
+  (:require [acc-text.nlg.gf.grammar :as grammar]))
 
+(defn abstract->gf [{abstract :acc-text.nlg.gf.grammar/abstract-grammar}]
+  )
 
-
+(defn concrete->gf [{abstract :acc-text.nlg.gf.grammar/abstract-grammar} data])
