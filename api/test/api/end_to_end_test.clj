32a21b1c0018c6b124fd9173e1a504032cdb469a
Rokas <rokas.ramanauskas@tokenmill.lt>
2019-11-19 11:33:12 +0200

    refactor: check all generated text variants in end to end tests

--- api/test/api/end_to_end_test.clj
+++ api/test/api/end_to_end_test.clj
@@ -7,13 +7,6 @@
             [data.entities.data-files :as data-files]
             [data.entities.dictionary :as dictionary]))
 
-(defn valid-sentence?
-  "Test validity of the sentence.
-  String is considered a valid sentence if it:
-  1. ends with one of the following punctuation symbols - .!?
-  2. has at least two alphanumeric characters."
-  [txt] (re-matches #".*\w{2,}.*[.?!]" txt))
-
 (defn prepare-environment [f]
   (dictionary/create-dictionary-item {:key          "cut"
                                       :name         "cut"
@@ -47,12 +40,14 @@
   (while (false? (get-in (q (str "/nlg/" result-id) :get nil) [:body :ready]))
     (Thread/sleep 100)))
 
-(defn get-first-variant [result-id]
+(defn get-variants [result-id]
   (when (some? result-id)
     (wait-for-results result-id)
     (let [response (q (str "/nlg/" result-id) :get nil)]
-      (str/join " " (for [{sentence-annotations :children} (get-in response [:body :variants 0 :children 0 :children])]
-                      (rebuild-sentence sentence-annotations))))))
+      (set
+        (for [{[{segments :children}] :children} (get-in response [:body :variants])]
+          (str/join " " (for [{sentence-annotations :children} segments]
+                          (rebuild-sentence sentence-annotations))))))))
 
 (deftest ^:integration single-element-plan-generation
   (let [data-file-id (data-files/store!
@@ -64,7 +59,7 @@
                           :dataId           data-file-id})]
     (is (= 200 status))
     (is (some? result-id))
-    (is (valid-sentence? (get-first-variant result-id)))))
+    (is (= #{"Building Search Applications."} (get-variants result-id)))))
 
 (deftest ^:integration authorship-document-plan-generation
   (let [data-file-id (data-files/store!
@@ -76,7 +71,8 @@
                           :dataId           data-file-id})]
     (is (= 200 status))
     (is (some? result-id))
-    (is (valid-sentence? (get-first-variant result-id)))))
+    (is (= #{"Building Search Applications is author by Manu Konchady."
+             "Manu Konchady is the author of Building Search Applications."} (get-variants result-id)))))
 
 (deftest ^:integration adjective-phrase-document-plan-generation
   (let [data-file-id (data-files/store!
@@ -88,8 +84,7 @@
                           :dataId           data-file-id})]
     (is (= 200 status))
     (is (some? result-id))
-    (is (contains? #{"Building Search Applications." "Good Building Search Applications."}
-                   (get-first-variant result-id)))))
+    (is (= #{"Good Building Search Applications."} (get-variants result-id)))))
 
 (deftest ^:integration author-amr-plan-generation
   (let [data-file-id (data-files/store!
@@ -101,7 +96,7 @@
                           :dataId           data-file-id})]
     (is (= 200 status))
     (is (some? result-id))
-    (is (valid-sentence? (get-first-variant result-id)))))
+    (is (= #{"Agent is the author of co- Agent."} (get-variants result-id)))))
 
 (deftest ^:integration single-quote-plan-generation
   (let [data-file-id (data-files/store!
@@ -113,8 +108,7 @@
                           :dataId           data-file-id})]
     (is (= 200 status))
     (is (some? result-id))
-    (is (= "This is a very good book: Building Search Applications." (get-first-variant result-id)))))
-
+    (is (= #{"This is a very good book: Building Search Applications."} (get-variants result-id)))))
 
 (deftest ^:integration single-modifier-plan-generation
   (let [data-file-id (data-files/store!
@@ -126,7 +120,7 @@
                           :dataId           data-file-id})]
     (is (= 200 status))
     (is (some? result-id))
-    (is (= "Good." (get-first-variant result-id)))))
+    (is (= #{"Good."} (get-variants result-id)))))
 
 (deftest ^:integration complex-amr-plan-generation
   (let [data-file-id (data-files/store!
@@ -138,7 +132,7 @@
                           :dataId           data-file-id})]
     (is (= 200 status))
     (is (some? result-id))
-    (is (= "Carol cut envelope to into pieces with knife." (get-first-variant result-id)))))
+    (is (= #{"Carol cut envelope to into pieces with knife."} (get-variants result-id)))))
 
 (deftest ^:integration multiple-modifier-plan-generation
   (let [data-file-id (data-files/store!
@@ -150,7 +144,7 @@
                           :dataId           data-file-id})]
     (is (= 200 status))
     (is (some? result-id))
-    (is (= "Noted author Manu Konchady." (get-first-variant result-id)))))
+    (is (= #{"Noted author Manu Konchady."} (get-variants result-id)))))
 
 (deftest ^:integration sequence-block-plan-generation
   (let [data-file-id (data-files/store!
@@ -162,7 +156,7 @@
                           :dataId           data-file-id})]
     (is (= 200 status))
     (is (some? result-id))
-    (is (= "1 2 3." (get-first-variant result-id)))))
+    (is (= #{"1 2 3."} (get-variants result-id)))))
 
 (deftest ^:integration random-sequence-block-plan-generation
   (let [data-file-id (data-files/store!
@@ -174,7 +168,7 @@
                           :dataId           data-file-id})]
     (is (= 200 status))
     (is (some? result-id))
-    (is (contains? #{"1 2 3." "1 3 2." "2 1 3." "2 3 1." "3 2 1." "3 1 2."} (get-first-variant result-id)))))
+    (is (= #{"1 2 3." "1 3 2." "2 1 3." "2 3 1." "3 2 1." "3 1 2."} (get-variants result-id)))))
 
 (deftest ^:integration one-of-synonyms-plan-generation
   (let [data-file-id (data-files/store!
@@ -186,7 +180,7 @@
                           :dataId           data-file-id})]
     (is (= 200 status))
     (is (some? result-id))
-    (is (contains? #{"Good." "Excellent."} (get-first-variant result-id)))))
+    (is (= #{"Good." "Excellent."} (get-variants result-id)))))
 
 (deftest ^:integration multiple-segments-plan-generation
   (let [data-file-id (data-files/store!
@@ -198,9 +192,9 @@
                           :dataId           data-file-id})]
     (is (= 200 status))
     (is (some? result-id))
-    (is (contains? #{"Manu Konchady is the author of Building Search Applications. Rarely is so much learning displayed with so much grace and charm."
-                     "Building Search Applications is written by Manu Konchady. Rarely is so much learning displayed with so much grace and charm."}
-                   (get-first-variant result-id)))))
+    (is (= #{"Manu Konchady is the author of Building Search Applications. Rarely is so much learning displayed with so much grace and charm."
+             "Building Search Applications is written by Manu Konchady. Rarely is so much learning displayed with so much grace and charm."}
+           (get-variants result-id)))))
 
 (deftest ^:integration sequence-with-empty-shuffle-plan-generation
   (let [data-file-id (data-files/store!
@@ -212,7 +206,7 @@
                           :dataId           data-file-id})]
     (is (= 200 status))
     (is (some? result-id))
-    (is (= "1." (get-first-variant result-id)))))
+    (is (= #{"1."} (get-variants result-id)))))
 
 (deftest ^:integration sequence-with-shuffle-plan-generation
   (let [data-file-id (data-files/store!
@@ -224,7 +218,7 @@
                           :dataId           data-file-id})]
     (is (= 200 status))
     (is (some? result-id))
-    (is (contains? #{"1 3 2." "1 2 3."} (get-first-variant result-id)))))
+    (is (= #{"1 3 2." "1 2 3."} (get-variants result-id)))))
 
 (deftest ^:integration sequence-with-shuffle-and-empty-synonyms-plan-generation
   (let [data-file-id (data-files/store!
@@ -236,7 +230,7 @@
                           :dataId           data-file-id})]
     (is (= 200 status))
     (is (some? result-id))
-    (is (contains? #{"1 3 2." "1 2 3."} (get-first-variant result-id)))))
+    (is (= #{"1 3 2." "1 2 3."} (get-variants result-id)))))
 
 (deftest ^:integration sequence-with-shuffle-and-synonyms-plan-generation
   (let [data-file-id (data-files/store!
@@ -248,4 +242,5 @@
                           :dataId           data-file-id})]
     (is (= 200 status))
     (is (some? result-id))
-    (is (contains? #{"1 2 3 4."} (get-first-variant result-id)))))
+    (is (= #{"1 2 3 4." "1 2 3 5." "1 2 4 3." "1 2 5 3." "1 3 2 4." "1 3 2 5."
+             "1 3 4 2." "1 3 5 2." "1 4 2 3." "1 4 3 2." "1 5 2 3." "1 5 3 2."} (get-variants result-id)))))
