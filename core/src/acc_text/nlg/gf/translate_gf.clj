34a28dffd9e2f5fdb8cd2b81d926425af9228762
Šarūnas Navickas <sarunas.navickas@tokenmill.lt>
2019-11-19 15:58:38 +0200

    Cover simple case

--- core/src/acc_text/nlg/gf/translate_gf.clj
+++ core/src/acc_text/nlg/gf/translate_gf.clj
@@ -1,11 +1,21 @@
 (ns acc-text.nlg.gf.translate-gf
-  (:require [acc-text.nlg.gf.grammar :as grammar]))
+  (:require [acc-text.nlg.gf.grammar :as grammar]
+            [clojure.string :as string]))
 
 
 (defn wrap-abstract [name body]
-  (format "abstract %s {\n%s\n}\n" name body))
+  (format "abstract %s {\n%s\n}" name (string/join ";\n" body)))
 
-(defn abstract->gf [{module-name :acc-text.nlg.gf.grammar/module-name}]
-  (wrap-abstract module-name ""))
-
-(defn concrete->gf [{abstract :acc-text.nlg.gf.grammar/abstract-grammar} data])
+(defn abstract->gf [{module-name :acc-text.nlg.gf.grammar/module-name
+                     flags       :acc-text.nlg.gf.grammar/flags
+                     categories  :acc-text.nlg.gf.grammar/categories
+                     functions   :acc-text.nlg.gf.grammar/functions}]
+  (wrap-abstract module-name (list
+                              (format "flags %s" (string/join ", " (map (fn [[label category]] (format "%s = %s" (name label) category)) flags)))
+                              (format "cat\n%s" (string/join "; " categories))
+                              (format "fun\n%s;" (string/join ";\n" (for [{name      :acc-text.nlg.gf.grammar/name
+                                                                          arguments :acc-text.nlg.gf.grammar/arguments
+                                                                          return    :acc-text.nlg.gf.grammar/return} functions]
+                                                                     (if (seq arguments)
+                                                                       (format "%s : %s -> %s" name (string/join " -> " arguments) return)
+                                                                       (format "%s : %s" name return))))))))
--- core/test/acc_text/nlg/gf/translate_gf_test.clj
+++ core/test/acc_text/nlg/gf/translate_gf_test.clj
@@ -1,6 +1,29 @@
 (ns acc-text.nlg.gf.translate-gf-test
   (:require [acc-text.nlg.gf.translate-gf :as sut]
-            [clojure.test :refer [is deftest]]))
+            [acc-text.nlg.gf.grammar :as grammar]
+            [clojure.test :refer [is deftest]]
+            [clojure.string :as string]))
 
-(deftest build-empty-abstract-grammar
-  (is (= "abstract Test {\n\n}\n" (sut/abstract->gf {:acc-text.nlg.gf.grammar/module-name "Test"}))))
+(def good-title-abstract
+  (string/join "\n"
+               ["abstract GoodTitle {"
+                "flags startcat = Sentence;"
+                "cat"
+                "Sentence; Data; Modifier;"
+                "fun"
+                "GoodTitle : Modifier -> Data -> Sentence;"
+                "DataTitle : Data;"
+                "GoodModifier : Modifier;"
+                "}"]))
+
+(deftest build-simple-abstract-grammar
+  (is (= good-title-abstract (sut/abstract->gf #::grammar{:module-name "GoodTitle"
+                                                          :flags {:startcat "Sentence"}
+                                                          :categories ["Sentence" "Data" "Modifier"]
+                                                          :functions [#::grammar{:name "GoodTitle"
+                                                                                 :arguments ["Modifier" "Data"]
+                                                                                 :return "Sentence"}
+                                                                      #::grammar{:name "DataTitle"
+                                                                                 :return "Data"}
+                                                                      #::grammar{:name "GoodModifier"
+                                                                                 :return "Modifier"}]}))))
