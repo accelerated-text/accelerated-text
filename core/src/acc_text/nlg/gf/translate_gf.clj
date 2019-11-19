d30f913e50d27ed7b45d9db479151022fe3b472f
Šarūnas Navickas <sarunas.navickas@tokenmill.lt>
2019-11-19 15:04:52 +0200

    Simple dummy test

--- core/src/acc_text/nlg/gf/translate_gf.clj
+++ core/src/acc_text/nlg/gf/translate_gf.clj
@@ -1,7 +1,11 @@
 (ns acc-text.nlg.gf.translate-gf
   (:require [acc-text.nlg.gf.grammar :as grammar]))
 
-(defn abstract->gf [{abstract :acc-text.nlg.gf.grammar/abstract-grammar}]
-  )
+
+(defn wrap-abstract [name body]
+  (format "abstract %s {\n%s\n}\n" name body))
+
+(defn abstract->gf [{module-name :acc-text.nlg.gf.grammar/module-name}]
+  (wrap-abstract module-name ""))
 
 (defn concrete->gf [{abstract :acc-text.nlg.gf.grammar/abstract-grammar} data])
--- core/test/acc_text/nlg/gf/translate_gf_test.clj
+++ core/test/acc_text/nlg/gf/translate_gf_test.clj
@@ -1,4 +1,6 @@
 (ns acc-text.nlg.gf.translate-gf-test
   (:require [acc-text.nlg.gf.translate-gf :as sut]
-            [clojure.test :as t]))
+            [clojure.test :refer [is deftest]]))
 
+(deftest build-empty-abstract-grammar
+  (is (= "abstract Test {\n\n}\n" (sut/abstract->gf {:acc-text.nlg.gf.grammar/module-name "Test"}))))
