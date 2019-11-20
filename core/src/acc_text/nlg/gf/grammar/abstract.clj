f00dbd5e838e9fdf936f6ba9da2e500ad4de6fdd
Žygimantas Medelis <zygimantas.medelis@tokenmill.lt>
2019-11-20 14:07:28 +0200

    refactor: use namespace alias

--- core/src/acc_text/nlg/gf/grammar/abstract.clj
+++ core/src/acc_text/nlg/gf/grammar/abstract.clj
@@ -1,19 +1,20 @@
 (ns acc-text.nlg.gf.grammar.abstract
-  (:require [acc-text.nlg.semantic-graph :as sg]))
+  (:require [acc-text.nlg.gf.grammar :as grammar]
+            [acc-text.nlg.semantic-graph :as sg]))
 
 (defn concept->name [{id ::sg/id type ::sg/type}]
   (format "%s-%s" (name type) (name id)))
 
 (defn build-grammar-function [{type ::sg/type :as concept} relations]
-  #:acc-text.nlg.gf.grammar{:function-name (concept->name concept)
-                            :arguments     (map ::sg/role relations)
-                            :return        type})
+  #::grammar{:function-name (concept->name concept)
+             :arguments     (map ::sg/role relations)
+             :return        type})
 
 (defn build [name {relations ::sg/relations concepts ::sg/concepts}]
   (let [relation-map (group-by ::sg/from relations)]
-    #:acc-text.nlg.gf.grammar{:module-name name
-                              :flags       {:startcat (when (seq concepts) (::sg/type (first concepts)))}
-                              :categories  (->> concepts (map ::sg/type) (sort) (dedupe))
-                              :functions   (map (fn [{id ::sg/id :as concept}]
-                                                  (build-grammar-function concept (get relation-map id)))
-                                                concepts)}))
+    #::grammar{:module-name name
+               :flags       {:startcat (when (seq concepts) (::sg/type (first concepts)))}
+               :categories  (->> concepts (map ::sg/type) (sort) (dedupe))
+               :functions   (map (fn [{id ::sg/id :as concept}]
+                                   (build-grammar-function concept (get relation-map id)))
+                                 concepts)}))
--- core/src/acc_text/nlg/gf/grammar/concrete.clj
+++ core/src/acc_text/nlg/gf/grammar/concrete.clj
@@ -1,6 +1,7 @@
 (ns acc-text.nlg.gf.grammar.concrete
   (:require [acc-text.nlg.gf.grammar.abstract :refer [concept->name]]
             [acc-text.nlg.semantic-graph :as sg]
+            [acc-text.nlg.gf.grammar :as grammar]
             [clojure.math.combinatorics :refer [permutations]]
             [clojure.string :as str]
             [acc-text.nlg.gf.string-utils :as su]))
@@ -8,44 +9,44 @@
 (defmulti build-lin (fn [{type ::sg/type} _ _ _] type))
 
 (defmethod build-lin :document-plan [concept children _ _]
-  #:acc-text.nlg.gf.grammar{:function-name (concept->name concept)
-                            :syntax        (for [child-concept children]
-                                             #:acc-text.nlg.gf.grammar{:role  :function
-                                                                       :value (concept->name child-concept)})})
+  #::grammar{:function-name (concept->name concept)
+             :syntax        (for [child-concept children]
+                              #::grammar{:role  :function
+                                         :value (concept->name child-concept)})})
 
 (defmethod build-lin :segment [concept children _ _]
-  #:acc-text.nlg.gf.grammar{:function-name (concept->name concept)
-                            :syntax        (for [child-concept children]
-                                             #:acc-text.nlg.gf.grammar{:role  :function
-                                                                       :value (concept->name child-concept)})})
+  #::grammar{:function-name (concept->name concept)
+             :syntax        (for [child-concept children]
+                              #::grammar{:role  :function
+                                         :value (concept->name child-concept)})})
 
 (defmethod build-lin :data [{value ::sg/value :as concept} children _ _]
-  #:acc-text.nlg.gf.grammar{:function-name (concept->name concept)
-                            :syntax        (concat
-                                             (for [child-concept children]
-                                               #:acc-text.nlg.gf.grammar{:role  :function
-                                                                         :value (concept->name child-concept)})
-                                             [#:acc-text.nlg.gf.grammar{:role  :literal
-                                                                        :value (format "{{%s}}" value)}])})
+  #::grammar{:function-name (concept->name concept)
+             :syntax        (concat
+                             (for [child-concept children]
+                               #::grammar{:role  :function
+                                          :value (concept->name child-concept)})
+                             [#::grammar{:role  :literal
+                                         :value (format "{{%s}}" value)}])})
 
 (defmethod build-lin :quote [{value ::sg/value :as concept} children _ _]
-  #:acc-text.nlg.gf.grammar{:function-name (concept->name concept)
-                            :syntax        (concat
-                                             (for [child-concept children]
-                                               #:acc-text.nlg.gf.grammar{:role  :function
-                                                                         :value (concept->name child-concept)})
-                                             [#:acc-text.nlg.gf.grammar{:role  :literal
-                                                                        :value (su/escape-string value)}])})
+  #::grammar{:function-name (concept->name concept)
+             :syntax        (concat
+                             (for [child-concept children]
+                               #::grammar{:role  :function
+                                          :value (concept->name child-concept)})
+                             [#::grammar{:role  :literal
+                                         :value (su/escape-string value)}])})
 
 (defmethod build-lin :dictionary-item [{value ::sg/value {attr-name ::sg/name} ::sg/attributes :as concept} children _ {dictionary :dictionary}]
-  #:acc-text.nlg.gf.grammar{:function-name (concept->name concept)
-                            :syntax        (concat
-                                             (for [value (set (cons attr-name (get dictionary value)))]
-                                               #:acc-text.nlg.gf.grammar{:role  :literal
-                                                                         :value (su/escape-string value)})
-                                             (for [child-concept children]
-                                               #:acc-text.nlg.gf.grammar{:role  :function
-                                                                         :value (concept->name child-concept)}))})
+  #::grammar{:function-name (concept->name concept)
+             :syntax        (concat
+                             (for [value (set (cons attr-name (get dictionary value)))]
+                               #::grammar{:role  :literal
+                                          :value (su/escape-string value)})
+                             (for [child-concept children]
+                               #::grammar{:role  :function
+                                          :value (concept->name child-concept)}))})
 
 (defmethod build-lin :amr [{value ::sg/value :as concept} children relations {amr :amr}]
   (let [function-concept (some (fn [[role concept]]
@@ -53,61 +54,61 @@
                                (zipmap (map ::sg/role relations) children))
         role-map (reduce (fn [m [{role ::sg/role {attr-name ::sg/name} ::sg/attributes} concept]]
                            (cond-> m
-                                   (and (not= :function role)
-                                        (some? attr-name)) (assoc (str/lower-case attr-name) (concept->name concept))))
+                             (and (not= :function role)
+                                  (some? attr-name)) (assoc (str/lower-case attr-name) (concept->name concept))))
                          {}
                          (zipmap relations children))]
     (for [syntax (->> (keyword value) (get amr) (:frames) (map :syntax))]
-      #:acc-text.nlg.gf.grammar{:function-name (concept->name concept)
-                                :syntax        (for [{value :value pos :pos} syntax]
-                                                 (let [role (when value (str/lower-case value))]
-                                                   (cond
-                                                     (contains? role-map role) #:acc-text.nlg.gf.grammar{:role  :function
-                                                                                                         :value (get role-map role)}
-                                                     (some? value) #:acc-text.nlg.gf.grammar{:role  :literal
-                                                                                             :value (su/escape-string value)}
-                                                     (= pos :VERB) #:acc-text.nlg.gf.grammar{:role  :function
-                                                                                             :value (concept->name function-concept)}
-                                                     :else #:acc-text.nlg.gf.grammar{:role  :literal
-                                                                                     :value ""})))})))
+      #::grammar{:function-name (concept->name concept)
+                 :syntax        (for [{value :value pos :pos} syntax]
+                                  (let [role (when value (str/lower-case value))]
+                                    (cond
+                                      (contains? role-map role) #::grammar{:role  :function
+                                                                           :value (get role-map role)}
+                                      (some? value) #::grammar{:role  :literal
+                                                               :value (su/escape-string value)}
+                                      (= pos :VERB) #::grammar{:role  :function
+                                                               :value (concept->name function-concept)}
+                                      :else #::grammar{:role  :literal
+                                                       :value ""})))})))
 
 (defmethod build-lin :sequence [concept children _ _]
-  #:acc-text.nlg.gf.grammar{:function-name (concept->name concept)
-                            :syntax        (for [child-concept children]
-                                             #:acc-text.nlg.gf.grammar{:role  :function
-                                                                       :value (concept->name child-concept)})})
+  #::grammar{:function-name (concept->name concept)
+             :syntax        (for [child-concept children]
+                              #::grammar{:role  :function
+                                         :value (concept->name child-concept)})})
 
 (defmethod build-lin :shuffle [concept children _ _]
   (for [child-permutation (permutations children)]
-    #:acc-text.nlg.gf.grammar{:function-name (concept->name concept)
-                              :syntax        (for [child-concept child-permutation]
-                                               #:acc-text.nlg.gf.grammar{:role  :function
-                                                                         :value (concept->name child-concept)})}))
+    #::grammar{:function-name (concept->name concept)
+               :syntax        (for [child-concept child-permutation]
+                                #::grammar{:role  :function
+                                           :value (concept->name child-concept)})}))
 
 (defmethod build-lin :sequence [concept children _ _]
-  #:acc-text.nlg.gf.grammar{:function-name (concept->name concept)
-                            :syntax        (for [child-concept children]
-                                             #:acc-text.nlg.gf.grammar{:role  :function
-                                                                       :value (concept->name child-concept)})})
+  #::grammar{:function-name (concept->name concept)
+             :syntax        (for [child-concept children]
+                              #::grammar{:role  :function
+                                         :value (concept->name child-concept)})})
 
 (defmethod build-lin :synonyms [concept children _ _]
   (for [child-concept children]
-    #:acc-text.nlg.gf.grammar{:function-name (concept->name concept)
-                              :syntax        [#:acc-text.nlg.gf.grammar{:role  :function
-                                                                        :value (concept->name child-concept)}]}))
+    #::grammar{:function-name (concept->name concept)
+               :syntax        [#::grammar{:role  :function
+                                          :value (concept->name child-concept)}]}))
 
 (defn build [parent-name name {relations ::sg/relations concepts ::sg/concepts} context]
-  #:acc-text.nlg.gf.grammar{:module-name name
-                            :of          parent-name
-                            :lin-types   (reduce (fn [m {type ::sg/type}]
-                                                   (assoc m type [:s :str]))
-                                                 {}
-                                                 concepts)
-                            :lins        (let [concept-map (zipmap (map ::sg/id concepts) concepts)
-                                               relation-map (group-by ::sg/from relations)]
-                                           (flatten
-                                             (map (fn [{id ::sg/id :as concept}]
-                                                    (let [relations (get relation-map id)
-                                                          children (map #(get concept-map (::sg/to %)) relations)]
-                                                      (build-lin concept children relations context)))
-                                                  concepts)))})
+  #::grammar{:module-name name
+             :of          parent-name
+             :lin-types   (reduce (fn [m {type ::sg/type}]
+                                    (assoc m type [:s :str]))
+                                  {}
+                                  concepts)
+             :lins        (let [concept-map (zipmap (map ::sg/id concepts) concepts)
+                                relation-map (group-by ::sg/from relations)]
+                            (flatten
+                             (map (fn [{id ::sg/id :as concept}]
+                                    (let [relations (get relation-map id)
+                                          children (map #(get concept-map (::sg/to %)) relations)]
+                                      (build-lin concept children relations context)))
+                                  concepts)))})
