03cebe6377fb9bf1a4ceb1b3b1ca8030aec5ed5d
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2019-11-20 09:37:52 +0200

    Load actual files from resources for testing

--- core/test/acc_text/nlg/gf/translate_gf_test.clj
+++ core/test/acc_text/nlg/gf/translate_gf_test.clj
@@ -2,38 +2,25 @@
   (:require [acc-text.nlg.gf.translate-gf :as sut]
             [acc-text.nlg.gf.grammar :as grammar]
             [clojure.test :refer [is deftest]]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [clojure.java.io :as io]))
 
-(def good-title-abstract
-  (string/join "\n"
-               ["abstract GoodTitle = {"
-                "flags startcat = Sentence;"
-                "cat"
-                "Sentence; Data; Modifier;"
-                "fun"
-                "GoodTitle : Modifier -> Data -> Sentence;"
-                "DataTitle : Data;"
-                "GoodModifier : Modifier;"
-                "}"]))
 
-(def good-title-concrete
-  (string/join "\n"
-               ["concrete GoodTitle01 of GoodTitle = {"
-                ""
-                "}"]))
+(defn load-gf [fname]
+  (slurp (io/file (format "test/resources/gf/%s" fname))))
 
 (deftest build-simple-abstract-grammar
-  (is (= good-title-abstract (sut/abstract->gf #::grammar{:module-name "GoodTitle"
-                                                          :flags {:startcat "Sentence"}
-                                                          :categories ["Sentence" "Data" "Modifier"]
-                                                          :functions [#::grammar{:name "GoodTitle"
-                                                                                 :arguments ["Modifier" "Data"]
-                                                                                 :return "Sentence"}
-                                                                      #::grammar{:name "DataTitle"
-                                                                                 :return "Data"}
-                                                                      #::grammar{:name "GoodModifier"
-                                                                                 :return "Modifier"}]}))))
+  (is (= (load-gf "GoodBook.gf") (sut/abstract->gf #::grammar{:module-name "GoodTitle"
+                                                              :flags {:startcat "Sentence"}
+                                                              :categories ["Sentence" "Data" "Modifier"]
+                                                              :functions [#::grammar{:name "GoodTitle"
+                                                                                     :arguments ["Modifier" "Data"]
+                                                                                     :return "Sentence"}
+                                                                          #::grammar{:name "DataTitle"
+                                                                                     :return "Data"}
+                                                                          #::grammar{:name "GoodModifier"
+                                                                                     :return "Modifier"}]}))))
 
 (deftest build-simple-concrete-grammar
-  (is (= good-title-concrete (sut/concrete->gf #::grammar{:module-name "GoodTitle01"
-                                                          :of          "GoodTitle"}))))
+  (is (= (load-gf "GoodBookEng.gf") (sut/concrete->gf #::grammar{:module-name "GoodTitle01"
+                                                                 :of          "GoodTitle"}))))
