48723d50c4042f5bc1ddaafaef6691c59c0bc67f
Zygimantas Medelis <zygimantas.medelis@tokenmill.lt>
2019-11-21 13:39:20 +0200

    refactor: make lines shorter

--- core/test/acc_text/nlg/gf/grammar/gf_test.clj
+++ core/test/acc_text/nlg/gf/grammar/gf_test.clj
@@ -8,37 +8,39 @@
   (slurp (io/file (format "test/resources/gf/%s" fname))))
 
 (deftest build-simple-abstract-grammar
-  (is (= (load-gf "GoodBook.gf") (sut/abstract->gf #::grammar{:module-name "GoodBook"
-                                                              :flags {:startcat "Sentence"}
-                                                              :categories ["Sentence" "Data" "Modifier"]
-                                                              :functions [#::grammar{:function-name "GoodTitle"
-                                                                                     :arguments ["Modifier" "Data"]
-                                                                                     :return "Sentence"}
-                                                                          #::grammar{:function-name "DataTitle"
-                                                                                     :return "Data"}
-                                                                          #::grammar{:function-name "GoodModifier"
-                                                                                     :return "Modifier"}]}))))
+  (is (= (load-gf "GoodBook.gf")
+         (sut/abstract->gf #::grammar{:module-name "GoodBook"
+                                      :flags       {:startcat "Sentence"}
+                                      :categories  ["Sentence" "Data" "Modifier"]
+                                      :functions   [#::grammar{:function-name "GoodTitle"
+                                                               :arguments     ["Modifier" "Data"]
+                                                               :return        "Sentence"}
+                                                    #::grammar{:function-name "DataTitle"
+                                                               :return        "Data"}
+                                                    #::grammar{:function-name "GoodModifier"
+                                                               :return        "Modifier"}]}))))
 
 (deftest build-simple-concrete-grammar
-  (is (= (load-gf "GoodBookEng.gf") (sut/concrete->gf #::grammar{:module-name "GoodBookEng"
-                                                                 :of          "GoodBook"
-                                                                 :lin-types   {:Sentence [:s :Str]
-                                                                               :Data     [:s :Str]
-                                                                               :Modifier [:s :Str]}
-                                                                 :lins        [#::grammar{:function-name "GoodTitle"
-                                                                                          :syntax [#::grammar{:role :function
-                                                                                                              :value "Modifier"}
-                                                                                                   #::grammar{:role :operation
-                                                                                                              :value "++"}
-                                                                                                   #::grammar{:role :function
-                                                                                                              :value "Data"}]}
-                                                                               #::grammar{:function-name "DataTitle"
-                                                                                          :syntax [#::grammar{:role :literal
-                                                                                                              :value "{{TITLE}}"}]}
-                                                                               #::grammar{:function-name "GoodModifier"
-                                                                                          :syntax [#::grammar{:role :literal
-                                                                                                              :value "good"}
-                                                                                                   #::grammar{:role :operation
-                                                                                                              :value "|"}
-                                                                                                   #::grammar{:role :literal
-                                                                                                              :value "nice"}]}]}))))
+  (is (= (load-gf "GoodBookEng.gf")
+         (sut/concrete->gf #::grammar{:module-name "GoodBookEng"
+                                      :of          "GoodBook"
+                                      :lin-types   {:Sentence [:s :Str]
+                                                    :Data     [:s :Str]
+                                                    :Modifier [:s :Str]}
+                                      :lins        [#::grammar{:function-name "GoodTitle"
+                                                               :syntax        [#::grammar{:role  :function
+                                                                                          :value "Modifier"}
+                                                                               #::grammar{:role  :operation
+                                                                                          :value "++"}
+                                                                               #::grammar{:role  :function
+                                                                                          :value "Data"}]}
+                                                    #::grammar{:function-name "DataTitle"
+                                                               :syntax        [#::grammar{:role  :literal
+                                                                                          :value "{{TITLE}}"}]}
+                                                    #::grammar{:function-name "GoodModifier"
+                                                               :syntax        [#::grammar{:role  :literal
+                                                                                          :value "good"}
+                                                                               #::grammar{:role  :operation
+                                                                                          :value "|"}
+                                                                               #::grammar{:role  :literal
+                                                                                          :value "nice"}]}]}))))
