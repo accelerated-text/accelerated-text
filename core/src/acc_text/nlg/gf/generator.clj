251cfa573f4c5631d5f4515ee316544ea1189f01
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2019-11-21 09:23:31 +0200

    Communication with new GF-service

--- core/src/acc_text/nlg/gf/generator.clj
+++ core/src/acc_text/nlg/gf/generator.clj
@@ -4,17 +4,28 @@
 
 (def read-mapper (json/object-mapper {:decode-key-fn true}))
 
-(defn compile-request [grammar]
+(defn compile-request [name abstract concrete]
   @(client/request {:url     (or (System/getenv "GF_ENDPOINT") "http://localhost:8001")
                     :method  :post
                     :headers {"Content-type" "application/json"}
-                    :body    (json/write-value-as-string {:content (reduce str grammar)})}))
+                    :body    (json/write-value-as-string {:name name
+                                                          :abstract {:content (apply str abstract)}
+                                                          :concrete (map-indexed
+                                                                     (fn [idx c] {:key idx
+                                                                                  :content (apply str c)})
+                                                                     concrete)})}))
 
-(defn generate [grammar]
-  (-> grammar
-      (compile-request)
+
+(defn flatten-results [results]
+  ;; {:results [(key1, array1), (key2, array2)]} -> array
+  (flatten
+   (map (fn [[_ r]] r) results)))
+
+(defn generate [name abstract concrete]
+  (-> (compile-request name abstract concrete)
       (get :body)
       (json/read-value read-mapper)
       (get :results)
+      (flatten-results)
       (sort)
       (dedupe)))
