1b893617bf21d08ec8d81590a81f97d80740c2fe
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2019-11-21 09:39:38 +0200

    More typos

--- core/src/acc_text/nlg/core.clj
+++ core/src/acc_text/nlg/core.clj
@@ -18,8 +18,9 @@
 (defn generate-text [semantic-graph context data]
   (let [parent-name (:acc-text.nlg.semantic-graph/id semantic-graph)
         abstract (abstract-grammar/build semantic-graph)
-        concrete (concrete-grammar/build parent-name (format "%s-1" parent-name) sematic-graph context)])
-  (->> (generator/generate parent-name abstract (list [1 concrete]))
+        concrete (concrete-grammar/build parent-name (format "%s-1" parent-name) semantic-graph context)]
+    (->> (generator/generate parent-name abstract (list [1 concrete]))
        (map #(realize % data))
        (map nlp/process-sentence)
-       (map nlp/annotate)))
+       (map nlp/annotate))))
+  
