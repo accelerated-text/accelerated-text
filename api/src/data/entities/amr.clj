e98d2940cb0399b00d9cd3fb3de364ee8cc02883
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2019-11-25 15:30:46 +0200

    A bit hackish way to initialize

--- api/src/data/entities/amr.clj
+++ api/src/data/entities/amr.clj
@@ -1,18 +1,28 @@
 (ns data.entities.amr
   (:require [clojure.java.io :as io]
             [clojure.string :as string]
-            [data.utils :as utils]))
+            [data.utils :as utils]
+            [data.entities.dictionary :as dictionary]))
+
+(defn prepare-dictionary-item [k members]
+  (when-not (dictionary/get-dictionary-item k)
+    (dictionary/create-dictionary-item
+     {:key k
+      :name k
+      :phrases members})))
 
 (defn read-amr [f]
   (let [{:keys [members roles frames]} (utils/read-yaml f)]
-    {:id                 (utils/get-name f)
-     :dictionary-item-id (first members)
-     :thematic-roles     (map (fn [role] {:type role}) roles)
-     :frames             (map (fn [{:keys [syntax example]}]
-                                {:examples [example]
-                                 :syntax   (for [instance syntax]
-                                             (into {} (update instance :pos keyword)))})
-                              frames)}))
+    (let [amr-key (utils/get-name f)]
+      (prepare-dictionary-item amr-key members)
+      {:id                 amr-key
+       :dictionary-item-id amr-key
+       :thematic-roles     (map (fn [role] {:type role}) roles)
+       :frames             (map (fn [{:keys [syntax example]}]
+                                  {:examples [example]
+                                   :syntax   (for [instance syntax]
+                                               (into {} (update instance :pos keyword)))})
+                                frames)})))
 
 (defn list-package [package]
   (let [abs-path (.getParent (io/file package))]
