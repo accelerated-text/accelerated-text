1d854c68900142d354b33c95f8ecb62736e8d1e8
Šarūnas Navickas <zaibacu@users.noreply.github.com>
2019-11-25 08:46:00 +0200

    Move amrs
    * Move to root path
    * Mount grammar
    * Loading grammar from mounted dir
    * Use grammar packages

--- api/src/data/entities/amr.clj
+++ api/src/data/entities/amr.clj
@@ -1,5 +1,6 @@
 (ns data.entities.amr
   (:require [clojure.java.io :as io]
+            [clojure.string :as string]
             [data.utils :as utils]))
 
 (defn read-amr [f]
@@ -13,12 +14,16 @@
                                              (into {} (update instance :pos keyword)))})
                               frames)}))
 
+(defn list-package [package]
+  (let [abs-path (.getParent (io/file package))]
+    (->> package
+         (utils/read-yaml)
+         :includes
+         (map (fn [p] (string/join "/" [abs-path p])))
+         (map io/file))))
+
 (defn list-amr-files []
-  (->> "amr"
-       (io/resource)
-       (io/file)
-       (file-seq)
-       (filter #(= ".yaml" (utils/get-ext %)))))
+  (list-package (or (System/getenv "GRAMMAR_PACKAGE") "test/resources/grammar/default.yaml")))
 
 (defn load-single [id]
   (when-let [f (some #(when (= (name id) (utils/get-name %)) %) (list-amr-files))]
--- api/test/api/graphql/concept_test.clj
+++ api/test/api/graphql/concept_test.clj
@@ -11,6 +11,8 @@
         _ (q "/_graphql" :post {:query query :variables {:name "provide"}})
         _ (q "/_graphql" :post {:query query :variables {:name "see"}})
         _ (q "/_graphql" :post {:query query :variables {:name "cut"}})
+        _ (q "/_graphql" :post {:query query :variables {:name "is"}})
+        _ (q "/_graphql" :post {:query query :variables {:name "published"}})
         query "{concepts{id concepts{id label roles{id fieldType fieldLabel} dictionaryItem{name phrases{text}} helpText}}}"
         {{{{:keys [id concepts]} :concepts} :data errors :errors} :body} (q "/_graphql" :post {:query query})]
     (is (nil? errors))
