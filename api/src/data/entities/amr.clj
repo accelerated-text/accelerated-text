860405bb7c9e458d6b1da3d61f784cf9aa6e1736
Žygimantas Medelis <zygimantas.medelis@tokenmill.lt>
2019-12-09 11:26:49 +0200

    Object description (X in Y with Z) AMR
    * feat: object description AMR
    * feat: add 'at' description

--- api/src/data/entities/amr.clj
+++ api/src/data/entities/amr.clj
@@ -24,9 +24,8 @@
   (let [abs-path (.getParent (io/file package))]
     (->> package
          (utils/read-yaml)
-         :includes
-         (map (fn [p] (string/join "/" [abs-path p])))
-         (map io/file))))
+         (:includes)
+         (map (fn [p] (io/file (string/join "/" [abs-path p])))))))
 
 (defn list-amr-files []
   (list-package (or (System/getenv "GRAMMAR_PACKAGE") "../grammar/all.yaml")))
@@ -35,8 +34,7 @@
   (when-let [f (some #(when (= (name id) (utils/get-name %)) %) (list-amr-files))]
     (read-amr f)))
 
-(defn load-all []
-  (map read-amr (list-amr-files)))
+(defn load-all [] (map read-amr (list-amr-files)))
 
 (defn initialize []
   (doseq [f (list-amr-files)]
