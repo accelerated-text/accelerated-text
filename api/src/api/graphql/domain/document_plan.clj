bfa54f3d170b7aa695aafdf1f9148ca7a21e7a6a
Rokas Ramanauskas <rokasramas@gmail.com>
2019-12-17 12:08:33 +0200

    Ensure simple environment setup for reaction commerce
    * chore: add makefile rule for document plan export

--- api/src/api/graphql/domain/document_plan.clj
+++ api/src/api/graphql/domain/document_plan.clj
@@ -1,15 +1,14 @@
 (ns api.graphql.domain.document-plan
   (:require [api.graphql.translate.document-plan :as translate-dp]
+            [clojure.string :as str]
             [com.walmartlabs.lacinia.resolve :refer [resolve-as]]
             [data.entities.document-plan :as dp]))
 
 (defn- resolve-as-not-found-document-plan [id]
-  (resolve-as nil {:message (format "Cannot find document plan with id `%s`." id)}))
+  (resolve-as nil {:message (format "Cannot find document plan `%s`." id)}))
 
-(defn get-document-plan [_ {:keys [id]} _]
-  (if-let [document-plan (dp/get-document-plan id)]
-    (resolve-as (translate-dp/dp->schema document-plan))
-    (resolve-as-not-found-document-plan id)))
+(defn- resolve-as-id-not-provided []
+  (resolve-as nil {:message "Either document plan `id` or `name` must be provided."}))
 
 (defn delete-document-plan [_ {:keys [id]} _]
   (dp/delete-document-plan id)
@@ -30,6 +29,17 @@
        (dp/add-document-plan)
        (translate-dp/dp->schema)
        (resolve-as)))
+
+(defn get-document-plan [_ args _]
+  (let [id (when-not (str/blank? (:id args)) (:id args))
+        name (when-not (str/blank? (:name args)) (:name args))
+        document-plan (cond
+                        (some? id) (dp/get-document-plan id)
+                        (some? name) (some #(when (= name (:name %)) %) (dp/list-document-plans)))]
+    (cond
+      (some? document-plan) (resolve-as (translate-dp/dp->schema document-plan))
+      (or id name) (resolve-as-not-found-document-plan (or id name))
+      :else (resolve-as-id-not-provided))))
 
 (defn update-document-plan [_ {:keys [id] :as args} _]
   (if-let [document-plan (dp/update-document-plan id (translate-dp/schema->dp args))]
