e56a61688c3d130fa1a859d7f3b640da04b909e6
Rokas <rokas.ramanauskas@tokenmill.lt>
2019-12-17 14:45:41 +0200

    refactor: disable dynamo db cleaning in tests

--- api/test/api/db_fixtures.clj
+++ api/test/api/db_fixtures.clj
@@ -1,11 +1,8 @@
 (ns api.db-fixtures
-  (:require [api.ddb-fixtures :as ddb]
-            [api.datomic-fixtures :as datomic]
+  (:require [api.datomic-fixtures :as datomic]
             [data.entities.amr :as amr]))
 
 (defn clean-db [f]
-  (when-not (System/getProperty "skip-dynamo")
-    (ddb/wipe-ddb-tables f))
   (when-not (System/getProperty "skip-datomic")
     (datomic/datomix-fixture f)))
 
