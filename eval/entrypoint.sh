0538f1437d0e30714c27521dfa4f106717920407
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2020-01-02 10:39:03 +0200

    Setup docker

--- eval/entrypoint.sh
+++ eval/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+set -e
+
+# Prepare document plan for eval
+
 echo "Uploading data to: ${ACC_TEXT_URL}"
 curl -XPOST ${ACC_TEXT_URL}/_graphql -H 'Content-Type: application/json' -d @data/bleu-plan.json
+
+exec "$@"
