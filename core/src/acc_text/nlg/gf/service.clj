9f50ffcb888f6146973ce8fd49b2c1f9616317e7
Rokas Ramanauskas <rokasramas@gmail.com>
2020-01-03 14:16:59 +0200

    Use instance name from generated grammar

--- core/src/acc_text/nlg/gf/service.clj
+++ core/src/acc_text/nlg/gf/service.clj
@@ -3,11 +3,11 @@
             [jsonista.core :as json]
             [org.httpkit.client :as client]))
 
-(defn compile-request [module abstract-grammar concrete-grammar]
+(defn compile-request [module instance abstract-grammar concrete-grammar]
   (let [request-url (or (System/getenv "GF_ENDPOINT") "http://localhost:8001")
         request-content {:name     (name module)
                          :abstract {:content abstract-grammar}
-                         :concrete [{:key     1
+                         :concrete [{:key     (name instance)
                                      :content concrete-grammar}]}]
     (log/debugf "Compiling grammar via %s:\n%s\n%s" request-url abstract-grammar concrete-grammar)
     (log/tracef "Request:\n curl -X POST -H \"Content-Type: application/json\"  %s -d '%s'"
