c8df084c8116e5c777b339a3680c0db1421edf52
Rokas Ramanauskas <rokasramas@gmail.com>
2020-01-29 15:32:33 +0200

    Provide data values via interface instead of using placeholders

--- core/src/acc_text/nlg/core.clj
+++ core/src/acc_text/nlg/core.clj
@@ -2,20 +2,9 @@
   (:require [acc-text.nlg.gf.generator :as generator]
             [acc-text.nlg.gf.grammar :as grammar]
             [acc-text.nlg.semantic-graph.conditions :as conditions]
-            [acc-text.nlg.utils.nlp :as nlp]
-            [clojure.string :as str]))
+            [acc-text.nlg.utils.nlp :as nlp]))
 
-(defn realize [text placeholders]
-  (when-not (str/blank? text)
-    (reduce-kv (fn [s k v]
-                 (let [pattern (re-pattern (format "(?i)\\{\\{%s\\}\\}" (name k)))]
-                   (str/replace s pattern v)))
-               text
-               placeholders)))
-
-(defn generate-text [semantic-graph context data]
-  (->> (grammar/build :grammar :1 (conditions/select semantic-graph data) context)
+(defn generate-text [semantic-graph {data :data :as context}]
+  (->> (grammar/build "Default" "Instance" (conditions/select semantic-graph data) context)
        (generator/generate)
-       (map #(realize % data))
-       (map nlp/process-sentence)
-       (map nlp/annotate)))
+       (map (comp nlp/annotate nlp/process-sentence))))
