e4f02eacc9b7d89ac88a5d00c927c1b4ff53df4c
Žygimantas Medelis <zygimantas.medelis@tokenmill.lt>
2020-01-30 08:55:42 +0200

    chore: add grammar logging

--- core/src/acc_text/nlg/gf/service.clj
+++ core/src/acc_text/nlg/gf/service.clj
@@ -6,7 +6,12 @@
 (defn compile-request [module instance content]
   (let [request-url (or (System/getenv "GF_ENDPOINT") "http://localhost:8001")
         request-content {:module module :instance instance :content content}]
-    (log/debugf "Compiling grammar via %s:\n%s\n%s" request-url (get content (str module "Body")) (get content (str module "Lex" instance)))
+    (log/debugf "Compiling grammar via %s" request-url)
+    (log/debugf "** Abstract **\n%s\n" (get content module))
+    (log/debugf "** Incomplete concrete **\n%s\n" (get content (str module "Body")))
+    (log/debugf "** Concrete **\n%s" (get content (str module instance)))
+    (log/debugf "** Lex interface **\n%s\n" (get content (str module "Lex")))
+    (log/debugf "** Lex data **\n%s\n" (get content (str module "Lex" instance)))
     (log/tracef "Request:\n curl -X POST -H \"Content-Type: application/json\"  %s -d '%s'"
                 request-url (json/write-value-as-string request-content))
     @(client/request {:url     request-url
