ab8fb875b09344c2d007cdd92a3cd44b13f48dd9
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2020-02-03 15:28:36 +0200

    Simplify AMR upload

--- eval/entrypoint.sh
+++ eval/entrypoint.sh
@@ -11,13 +11,9 @@ while [[ "$(curl --insecure -s -o /dev/null -w ''%{http_code}'' ${ACC_TEXT_URL}/
 echo "Uploading required AMRs"
 
 for f in data/*.yaml; do
-  echo $f
   filepath=$(basename -- "$f")
   filename="${filepath%.*}"
-  export TITLE=$filename
-  export CONTENT=$(cat "$f" | sed 's/"/\\"/g')
-  req_content=$(envsubst < "data/bleu-amr.json")
-  curl -XPOST ${ACC_TEXT_URL}/_graphql -H 'Content-Type: application/json' -d '$req_content'
+  curl -XPOST ${ACC_TEXT_URL}/amr/${filename} -H 'Content-Type: application/yaml' -d @${f}
 done
 
 echo "Uploading data to: ${ACC_TEXT_URL}"
