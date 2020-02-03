9226a2bfd826e9ede027dc9afee807b1d408477a
Sarunas Navickas <sarunas.navickas@tokenmill.lt>
2020-02-03 15:04:11 +0200

    Add AMR upload step

--- eval/entrypoint.sh
+++ eval/entrypoint.sh
@@ -8,6 +8,18 @@ echo "Waiting for ${ACC_TEXT_URL}/health"
 
 while [[ "$(curl --insecure -s -o /dev/null -w ''%{http_code}'' ${ACC_TEXT_URL}/health)" != "200" ]]; do sleep 1; done
 
+echo "Uploading required AMRs"
+
+for f in data/*.yaml; do
+  echo $f
+  filepath=$(basename -- "$f")
+  filename="${filepath%.*}"
+  export TITLE=$filename
+  export CONTENT=$(cat "$f" | sed 's/"/\\"/g')
+  req_content=$(envsubst < "data/bleu-amr.json")
+  curl -XPOST ${ACC_TEXT_URL}/_graphql -H 'Content-Type: application/json' -d '$req_content'
+done
+
 echo "Uploading data to: ${ACC_TEXT_URL}"
 
 
