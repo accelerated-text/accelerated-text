f13846ddb6c542ed7b6243435ebeb33c742655cc
Šarūnas Navickas <sarunas.navickas@tokenmill.lt>
2020-02-04 11:42:58 +0200

    Basic wireup

--- core/src/acc_text/nlg/core.clj
+++ core/src/acc_text/nlg/core.clj
@@ -2,9 +2,18 @@
   (:require [acc-text.nlg.gf.generator :as generator]
             [acc-text.nlg.gf.grammar :as grammar]
             [acc-text.nlg.semantic-graph.conditions :as conditions]
-            [acc-text.nlg.utils.nlp :as nlp]))
+            [acc-text.nlg.utils.nlp :as nlp]
+            [acc-text.nlg.enrich.core :as enrich]))
 
 (defn generate-text [semantic-graph {data :data :as context}]
   (->> (grammar/build "Default" "Instance" (conditions/select semantic-graph data) context)
        (generator/generate)
        (map (comp nlp/annotate nlp/process-sentence))))
+
+(defn enrich-text
+  [context text]
+  (let [result (enrich/enrich-request context text)]
+    (when (get result :error false)
+      {:original text})
+    {:original text
+     :enriched (:result result)}))
