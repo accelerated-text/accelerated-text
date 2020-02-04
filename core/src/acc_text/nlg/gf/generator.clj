680cf49c759af0b621b9cccb098947eb183b877e
Rokas <rokas.ramanauskas@tokenmill.lt>
2020-02-04 16:32:24 +0200

    chore: remove old concept net amrs and add new ones

--- core/src/acc_text/nlg/gf/generator.clj
+++ core/src/acc_text/nlg/gf/generator.clj
@@ -121,12 +121,16 @@
             "cat" (parse-cat flags functions)
             "fun" (parse-fun functions))))
 
+(def imports ["LangFunctionsEng" "SyntaxEng" "ParadigmsEng"
+              "AtLocationEng" "CapableOfEng" "HasAEng"
+              "HasPropertyEng" "IsAEng" "LocatedNearEng" "MadeOfEng"])
+
 (defn ->incomplete [{::grammar/keys [module functions]}]
   (format "incomplete concrete %sBody of %s = open %sLex, %s in {%s\n}"
           module
           module
           module
-          "LangFunctionsEng, ConceptNetEng, SyntaxEng, ParadigmsEng"
+          (str/join ", " imports)
           (join-body
             "param" (parse-param functions)
             "lincat" (parse-lincat functions)
