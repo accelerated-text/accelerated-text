a3af77c63560dbf54e7c05e1608f6fb95407a950
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2020-02-10 17:17:48 +0200

    fix: prevent nil concept roles

--- api/src/api/graphql/translate/concept.clj
+++ api/src/api/graphql/translate/concept.clj
@@ -2,9 +2,10 @@
   (:require [clojure.string :as string]))
 
 (defn- role->schema [{:keys [type label]}]
-  {:fieldType  ["Str" "List" type]
+  {:fieldType  (cond-> ["Str" "List"]
+                       (some? type) (conj type))
    :id         type
-   :fieldLabel (or label type)})
+   :fieldLabel (or label type "UNK")})
 
 (defn- frames->help-text [frames]
   (->> frames
@@ -15,7 +16,12 @@
 (defn amr->schema [{:keys [id kind roles frames label name]}]
   {:id       id
    :kind     kind
-   :roles    (map role->schema roles)
+   :roles    (->> roles
+                  (map role->schema)
+                  (group-by :id)
+                  (vals)
+                  (map first)
+                  (remove (comp nil? :id)))
    :helpText (frames->help-text frames)
    :label    (or label id)
    :name     (or name id)})
