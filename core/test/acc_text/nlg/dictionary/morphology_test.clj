c72f949dc768647acd2a30f7639ca51ae44dcbb0
Zygimantas Medelis <zygimantas.medelis@tokenmill.lt>
2020-02-20 11:53:36 +0200

    test: add word definition examples

---
+++ core/test/acc_text/nlg/dictionary/morphology_test.clj
@@ -0,0 +1,27 @@
+(ns acc-text.nlg.dictionary.morphology-test
+  (:require [acc-text.nlg.dictionary.morphology :as m]
+            [clojure.test :refer [deftest is testing]]
+            [clojure.spec.alpha :as s]))
+
+(deftest word-definition-samples
+  (testing "cases of wrong word definition"
+    (is (not (s/valid? ::m/word-def #::m{:base-form "dog"})))
+    (is (not (s/valid? ::m/word-def #::m{:base-form "dog" :language :en}))))
+
+  (testing "correct word definition"
+    ;;absolutely minimal word definition
+    (is (s/valid? ::m/word-def #::m{:base-form "dog"
+                                    :pos :n
+                                    :language :en}))
+    ;;word definitions with optional parts
+    (is (s/valid? ::m/word-def #::m{:base-form "dog"
+                                    :pos :n
+                                    :language :en
+                                    :gender :m}))
+    (is (s/valid? ::m/word-def #::m{:base-form "dog"
+                                    :pos :n
+                                    :language :en
+                                    :gender :m
+                                    :inflections {[:nom :sg] "dog"
+                                                  [:nom :pl] "dogs"}}))))
+
