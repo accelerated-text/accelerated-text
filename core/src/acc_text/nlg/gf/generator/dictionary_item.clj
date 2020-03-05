8f53a3f6fbc2f95f7f6d6d982038b27cc7ede392
Rokas <rokas.ramanauskas@tokenmill.lt>
2020-03-05 16:10:06 +0200

    feat: partial russian dictionary item support

--- core/src/acc_text/nlg/gf/generator/dictionary_item.clj
+++ core/src/acc_text/nlg/gf/generator/dictionary_item.clj
@@ -1,10 +1,15 @@
 (ns acc-text.nlg.gf.generator.dictionary-item
   (:require [acc-text.nlg.dictionary.item :as dictionary-item]
             [acc-text.nlg.gf.generator.utils :as gen-utils]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.tools.logging :as log]))
 
 (defmulti build-dictionary-item (fn [type {::dictionary-item/keys [category language]}]
                                   (str/join "/" [language type category])))
+
+(defmethod build-dictionary-item :default [type {::dictionary-item/keys [category language]}]
+  (log/errorf "Unknown dictionary-item type pair for %s: %s/%s" language type category)
+  "\"\"")
 
 (defn join-forms [forms]
   (->> forms
@@ -13,6 +18,9 @@
 
 (defmethod build-dictionary-item "Eng/V/V" [_ {::dictionary-item/keys [forms]}]
   (format "(ParadigmsEng.mkV %s)" (join-forms forms)))
+
+(defmethod build-dictionary-item "Eng/A/A" [_ {::dictionary-item/keys [forms]}]
+  (format "(ParadigmsEng.mkA %s)" (join-forms forms)))
 
 (defmethod build-dictionary-item "Eng/V2/V" [_ {::dictionary-item/keys [forms]}]
   (format "(ParadigmsEng.mkV2 (ParadigmsEng.mkV %s))" (join-forms forms)))
@@ -27,3 +35,14 @@
   (if (contains? attributes "Gender")
     (format "(ParadigmsEng.mkN ParadigmsEng.%s (ParadigmsEng.mkN %s))" (get attributes "Gender") (join-forms forms))
     (format "(ParadigmsEng.mkN %s)" (join-forms forms))))
+
+(defmethod build-dictionary-item "Rus/A/A" [_ {::dictionary-item/keys [forms]}]
+  (format "(ParadigmsRus.mkA %s)" (join-forms forms)))
+
+(defmethod build-dictionary-item "Rus/N/N" [_ {::dictionary-item/keys [forms attributes]}]
+  (format "(ParadigmsRus.mkN %s MorphoRus.%s MorphoRus.%s)"
+          (join-forms forms) (get attributes "Gender") (get attributes "Animacy")))
+
+(defmethod build-dictionary-item "Rus/V2/V" [_ {::dictionary-item/keys [forms attributes]}]
+  (format "(ParadigmsRus.mkV2 (ParadigmsRus.mkV MorphoRus.%s %s) \"\" ParadigmsRus.%s)"
+          (get attributes "Aspect") (join-forms forms) (get attributes "Case")))
