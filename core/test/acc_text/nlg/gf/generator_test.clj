cca4461e31cb086597e5e63869b5f9e4366cf461
Rokas <rokas.ramanauskas@tokenmill.lt>
2020-03-05 12:06:05 +0200

    test: dictionary item generate

--- core/test/acc_text/nlg/gf/generator_test.clj
+++ core/test/acc_text/nlg/gf/generator_test.clj
@@ -81,3 +81,11 @@
              (grammar/build "Nested" "AMR"
                             (utils/load-test-semantic-graph "nested-amr")
                             ctx))))))
+
+(deftest ^:integration dictionary
+  (let []
+    (is (= ["it publishes author."]
+           (generate
+             (grammar/build "Default" "Instance"
+                            (utils/load-test-semantic-graph "dictionary-test")
+                            (utils/load-test-context "dictionary-test")))))))
