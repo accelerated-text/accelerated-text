98883055927a0e39242ae8217ab2d249e87e5925
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2020-03-17 10:55:37 +0200

    refactor: treat shuffle more like synonyms

--- core/src/acc_text/nlg/grammar/impl.clj
+++ core/src/acc_text/nlg/grammar/impl.clj
@@ -10,6 +10,7 @@
             [acc-text.nlg.graph.utils :refer [find-root-id get-successors get-in-edge add-concept-position prune-graph]]
             [acc-text.nlg.graph.variables :refer [resolve-variables]]
             [acc-text.nlg.semantic-graph.utils :refer [semantic-graph->ubergraph]]
+            [clojure.math.combinatorics :refer [permutations]]
             [clojure.string :as str]
             [loom.alg :refer [pre-traverse]]
             [loom.attr :refer [attrs]]))
@@ -38,7 +39,7 @@
      :fun    {cat (->> successors (remove-data-types graph) (map #(node->cat graph %)))}
      :lincat {cat "Str"}
      :lin    {cat [(str/join " ++ " (map #(cond-> (node->cat graph %)
-                                            (s-node? graph %) (str ".s"))
+                                                  (s-node? graph %) (str ".s"))
                                          successors))]}}))
 
 (defmethod build-node :frame [graph node-id]
@@ -85,18 +86,19 @@
                        successors)}}))
 
 (defmethod build-node :shuffle [graph node-id]
-  (let [children (:children (attrs graph node-id))
+  (let [successors (get-successors graph node-id)
+        category (:category (attrs graph node-id))
         cat (node->cat graph node-id)]
     {:cat    [cat]
-     :fun    {cat (->> children (first) (remove-data-types graph) (map #(node->cat graph %)))}
-     :lincat {cat "Str"}
+     :fun    {cat (->> successors (remove-data-types graph) (map #(node->cat graph %)))}
+     :lincat {cat (or category "Str")}
      :lin    {cat [(str/join " | " (map (fn [group]
                                           (->> (map #(cond-> (node->cat graph %)
-                                                       (s-node? graph %) (str ".s"))
+                                                             (and (s-node? graph %) (nil? category)) (str ".s"))
                                                     group)
                                                (str/join " ++ ")
                                                (format "(%s)")))
-                                        children))]}}))
+                                        (permutations successors)))]}}))
 
 (defn ->graph [semantic-graph context]
   (-> semantic-graph
--- core/src/acc_text/nlg/graph/lists.clj
+++ core/src/acc_text/nlg/graph/lists.clj
@@ -1,30 +1,15 @@
 (ns acc-text.nlg.graph.lists
-  (:require [acc-text.nlg.graph.utils :refer [find-nodes get-in-edge get-successors]]
-            [clojure.math.combinatorics :refer [permutations]]
+  (:require [acc-text.nlg.graph.utils :refer [find-nodes get-in-edge]]
             [loom.attr :refer [attrs]]
             [loom.graph :as graph]))
 
-(defn resolve-synonyms [g]
+(defn resolve-lists [g]
   (reduce (fn [g [node-id _]]
             (let [{category :category :as in-edge-attrs} (attrs g (get-in-edge g node-id))]
               (reduce (fn [g edge]
                         (assoc-in g [:attrs (:id edge)] in-edge-attrs))
                       (cond-> g
-                        (some? category) (update-in [:attrs node-id] #(assoc % :category category)))
+                              (some? category) (update-in [:attrs node-id] #(assoc % :category category)))
                       (graph/out-edges g node-id))))
           g
-          (find-nodes g {:type :synonyms})))
-
-
-(defn resolve-shuffle [g]
-  (reduce (fn [g [node-id _]]
-            (let [edges (get-successors g node-id)
-                  variants (permutations edges)]
-              (update-in g [:attrs node-id] #(assoc % :children variants))))
-          g
-          (find-nodes g {:type :shuffle})))
-
-(defn resolve-lists [g]
-  (-> g
-      (resolve-synonyms)
-      (resolve-shuffle)))
+          (concat (find-nodes g {:type :synonyms}) (find-nodes g {:type :shuffle}))))
