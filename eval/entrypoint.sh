fd8d193774b9bda66aeda33100f833afa9ad8717
Sarunas Navickas <zaibacu@gmail.com>
2020-03-26 08:45:55 +0200

    Wait for API to be ready, fix graphql URL

--- eval/entrypoint.sh
+++ eval/entrypoint.sh
@@ -2,4 +2,9 @@
 
 set -e
 
+
+echo "Waiting for ${ACC_TEXT_URL}/health"
+
+while [[ "$(curl --insecure -s -o /dev/null -w ''%{http_code}'' ${ACC_TEXT_URL}/health)" != "200" ]]; do sleep 1; done
+
 exec "$@"
