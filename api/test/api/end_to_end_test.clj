c398dfbac06bbe37f9f88eaf2d861988ec53bf5b
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2020-04-03 20:41:24 +0300

    test: generate and generate bulk end to end

---
+++ api/test/api/end_to_end_test.clj
@@ -0,0 +1,11 @@
+(ns api.end-to-end-test
+  (:require [api.db-fixtures :as db]
+            [api.test-utils :refer [generate-text generate-text-bulk]]
+            [clojure.test :refer [deftest is testing use-fixtures]]))
+
+(use-fixtures :each db/clean-db)
+
+(deftest text-generation-with-default-plan
+  (is (= [] (generate-text {:document-plan-name "Untitled plan"})))
+  (is (= {"test" []} (generate-text-bulk {:document-plan-name "Untitled plan"
+                                          :data-rows          {"test" {}}}))))
