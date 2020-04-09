aaabb31f068b7cf9bfc509b0fa067f61c0043df6
Sarunas Navickas <zaibacu@gmail.com>
2020-04-09 15:28:07 +0300

    Some progress

--- utils/src/utils/queries.clj
+++ utils/src/utils/queries.clj
@@ -3,7 +3,6 @@
             [graphql-builder.core :as core]
             [jsonista.core :as json]
             [clojure.string :as str]
-            [clojure.tools.logging :as log]
             [data.utils :refer [list-files]]))
 
 (defn graphql-file-filter
@@ -13,18 +12,13 @@
 (def graphql-files
   (->> (list-files "resources/")
        (map #(.getName %))
-       #_(filter graphql-file-filter)))
+       (filter graphql-file-filter)))
 
 (def query-map
   (->> graphql-files
        (map read-file)
        (str/join "\n")
-       (log/spyf :trace "Content: %s")
        (parse)
-       (log/spyf :trace "Parsed: %s")
        (core/query-map)))
 
 (def export-document-plans-query (get-in query-map [:query :export-document-plans]))
-
-(def export-document-plan-query (get-in query-map [:query :export-document-plan]))
-
