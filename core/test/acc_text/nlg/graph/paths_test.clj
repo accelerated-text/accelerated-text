15802173624e62735d7c6a893affba57a26b4fc1
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2020-04-10 13:24:30 +0300

    test: update generation results after changing default paths

--- core/test/acc_text/nlg/graph/paths_test.clj
+++ core/test/acc_text/nlg/graph/paths_test.clj
@@ -6,15 +6,14 @@
 (deftest ^:integration path-completion
   (let [semantic-graph (load-test-semantic-graph "path-test")
         context (load-test-context "path-test")]
-    (is (= #{"Be made of steel!"
-             "Be made!"
-             "Make yourself!"
+    (is (= #{"It is made of steel."
+             "It is made."
+             "It makes itself."
              "Of steel."
              "Of."
              "Refrigerator is made of steel."
              "Refrigerator was made of steel."
              "Refrigerator will be made of steel."
-             "There is a refrigerator."
-             "There is a steel."
+             "There is refrigerator."
              "There is steel."}
            (set (map :text (core/generate-text semantic-graph context "Eng")))))))
