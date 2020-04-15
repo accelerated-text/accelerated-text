13ad049160cd27547f831a68d1153833e0dd6886
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2020-04-15 18:25:12 +0300

    fix: set vars appear as amr roles

--- api/src/data/entities/amr.clj
+++ api/src/data/entities/amr.clj
@@ -1,6 +1,7 @@
 (ns data.entities.amr
   (:require [acc-text.nlg.semantic-graph :as sg]
             [api.nlg.parser :refer [document-plan->semantic-graph]]
+            [clojure.set :as set]
             [data.entities.document-plan :as dp]))
 
 (defn get-relation-categories [{relations ::sg/relations}]
@@ -9,6 +10,9 @@
                     (some? category) (assoc concept-id category)))
           {}
           relations))
+
+(defn find-unrelated-concepts [{::sg/keys [concepts relations]}]
+  (set/difference (set (map :id concepts)) (set (map :from relations))))
 
 (defn document-plan->amr [{:keys [id name documentPlan examples] :as entity}]
   (let [{concepts ::sg/concepts :as semantic-graph} (document-plan->semantic-graph
@@ -19,8 +23,11 @@
      :kind           "Str"
      :semantic-graph semantic-graph
      :frames         [{:examples (or examples [])}]
-     :roles          (let [categories (get-relation-categories semantic-graph)]
-                       (loop [[reference & rs] (filter #(= :reference (:type %)) concepts)
+     :roles          (let [categories (get-relation-categories semantic-graph)
+                           unrelated-concepts (find-unrelated-concepts semantic-graph)]
+                       (loop [[reference & rs] (filter (fn [{:keys [id type]}]
+                                                         (and (= :reference type) (contains? unrelated-concepts id)))
+                                                       concepts)
                               index 0 vars #{} roles []]
                          (if-not (some? reference)
                            roles
