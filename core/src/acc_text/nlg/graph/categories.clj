6976e4e3bf73a3e4a27bb79f4a37de3564148880
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2020-04-17 14:38:10 +0300

    fix: category resolution on nested amrs

--- core/src/acc_text/nlg/graph/categories.clj
+++ core/src/acc_text/nlg/graph/categories.clj
@@ -1,22 +1,23 @@
 (ns acc-text.nlg.graph.categories
   (:require [acc-text.nlg.graph.utils :as utils]
-            [loom.graph :as graph]
-            [loom.alg :as alg]))
+            [loom.alg :as alg]
+            [loom.graph :as graph]))
 
 (defn set-category [g node-id category]
-  (->> (graph/successors g node-id)
-       (cons node-id)
-       (mapcat #(cons % (map :id (graph/out-edges g %))))
-       (reduce (fn [g node-or-edge-id]
-                 (let [{current-category :category type :type} (get-in g [:attrs node-or-edge-id])]
-                   (cond-> g
-                           (and (nil? current-category) (not (contains? #{:quote :data :dictionary-item} type)))
-                           (assoc-in [:attrs node-or-edge-id :category] category))))
-               g)))
+  (reduce (fn [g node-or-edge-id]
+            (let [{current-category :category type :type} (get-in g [:attrs node-or-edge-id])]
+              (cond-> g
+                      (and (nil? current-category) (not (contains? #{:quote :data :dictionary-item} type)))
+                      (assoc-in [:attrs node-or-edge-id :category] category))))
+          g
+          (cons node-id (map :id (graph/out-edges g node-id)))))
+
+(defn get-in-categories [g node-id]
+  (set (remove nil? (map #(get-in g [:attrs (:id %) :category]) (graph/in-edges g node-id)))))
 
 (defn resolve-categories [g]
   (reduce (fn [g node-id]
-            (let [categories (set (remove nil? (map #(get-in g [:attrs (:id %) :category]) (graph/in-edges g node-id))))]
+            (let [categories (get-in-categories g node-id)]
               (case (count categories)
                 0 g
                 1 (set-category g node-id (first categories))
