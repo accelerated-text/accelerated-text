f0c6c97641b7c7af42a6155a8949c0246b1823df
Šarūnas Navickas <zaibacu@gmail.com>
2020-04-21 15:18:57 +0300

    Fake failing integration test

--- api/test/api/end_to_end_test.clj
+++ api/test/api/end_to_end_test.clj
@@ -32,3 +32,6 @@
                          :data-file-name     "books.csv"
                          :reader-flags       {"English" true
                                               "German"  true}}))))
+
+(deftest ^:integration failing-test
+  (is false))
