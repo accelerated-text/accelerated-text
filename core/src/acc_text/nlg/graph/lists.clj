db9864d20d30f7354562aad17eb18e0d7a70afa0
Žygimantas Medelis <zygimantas.medelis@tokenmill.lt>
2020-04-21 10:54:03 +0300

    fix: graph node creation

--- core/src/acc_text/nlg/graph/lists.clj
+++ core/src/acc_text/nlg/graph/lists.clj
@@ -1,24 +1,31 @@
 (ns acc-text.nlg.graph.lists
-  (:require [acc-text.nlg.graph.utils :as gut :refer [find-nodes get-in-edge add-edges]]
+  (:require [acc-text.nlg.graph.utils :as gut :refer [find-nodes]]
             [clojure.tools.logging :as log]
+            [ubergraph.core :as uber]
             [loom.attr :as attr]
             [loom.graph :as graph])
   (:import java.util.UUID))
 
-(defn clone-node [g new-id source]
-  (graph/add-nodes g [^:node new-id (attr/attrs g source)]))
+(defn add-node [g node attrs]
+  (uber/add-nodes-with-attrs g [node attrs]))
 
 (defn resolve-lists [g]
-  ;;i (UUID/randomUUID)
-  (reduce
+  (reduce ;;first, iterate over all list nodes
    (fn [g [list-node-id _]]
-     (reduce (fn [g parent]
-               (reduce (fn [g child]
-                         (log/debugf "Pred: %s" (attrs g parent))
-                         (log/debugf "Succ: %s" (attrs g child))
-                         (if (= :operation (:type (attrs g parent)))
-                           (graph/add-edges g [^:edge parent child {:X 110000}])
-                           g))
-                       g (gut/get-successors g list-node-id)))
-             g (gut/get-predecessors g list-node-id)))
+     (reduce ;;then iterate over parents of each a list node
+      (fn [g parent]
+        (reduce ;;lastly go over children of list node and connect with parent
+         (fn [g child]
+           (log/debugf "Pred: %s" (attr/attrs g parent))
+           (log/debugf "Succ: %s" (attr/attrs g child))
+           (if (= :operation (:type (attr/attrs g parent)))
+             (let [new-id (UUID/randomUUID)
+                   new-attrs (attr/attrs g parent)]
+               (-> g
+                   (add-node new-id new-attrs)
+                   (graph/add-edges
+                    [^:edge new-id child {:X 22}])))
+             g))
+         g (gut/get-successors g list-node-id)))
+      g (gut/get-predecessors g list-node-id)))
    g (concat (find-nodes g {:type :synonyms}) (find-nodes g {:type :shuffle}))))
