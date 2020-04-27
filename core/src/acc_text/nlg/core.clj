74a4ae61b17cfe78675083c3c90e59ed29d0aee4
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2020-04-27 17:18:07 +0300

    refactor: use nlp utils on results

--- core/src/acc_text/nlg/core.clj
+++ core/src/acc_text/nlg/core.clj
@@ -1,12 +1,18 @@
 (ns acc-text.nlg.core
   (:require [acc-text.nlg.gf.service :as gf-service]
             [acc-text.nlg.grammar :as grammar]
-            [clojure.tools.logging :as log]))
+            [clojure.tools.logging :as log]
+            [acc-text.nlp.utils :as nlp]))
 
 (defn generate-text [semantic-graph context lang]
   (log/debugf "Processing generate request for `%s`..." lang)
   (log/debugf "Semantic graph: %s" semantic-graph)
   (log/debugf "Context: %s" (assoc context :constants {"*Language" lang}))
-  (-> semantic-graph
-      (grammar/build-grammar (assoc context :constants {"*Language" lang}))
-      (gf-service/request lang)))
+  (map (fn [{:keys [text]}]
+         (let [text (nlp/process-sentence text)]
+           {:text     text
+            :language lang
+            :tokens   (nlp/annotate text)}))
+       (-> semantic-graph
+           (grammar/build-grammar (assoc context :constants {"*Language" lang}))
+           (gf-service/request lang))))
