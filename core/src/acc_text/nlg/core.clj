5e977c97e6dd9fe5f19b000d5623de8b287b4062
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2020-04-27 17:08:12 +0300

    refactor: skip nlp step in core when producing results for now

--- core/src/acc_text/nlg/core.clj
+++ core/src/acc_text/nlg/core.clj
@@ -1,18 +1,12 @@
 (ns acc-text.nlg.core
-  (:require [acc-text.nlg.generator :as generator]
-            [acc-text.nlg.gf.service :as service]
+  (:require [acc-text.nlg.gf.service :as gf-service]
             [acc-text.nlg.grammar :as grammar]
-            [acc-text.nlp.utils :as nlp]
             [clojure.tools.logging :as log]))
 
 (defn generate-text [semantic-graph context lang]
   (log/debugf "Processing generate request for `%s`..." lang)
   (log/debugf "Semantic graph: %s" semantic-graph)
   (log/debugf "Context: %s" (assoc context :constants {"*Language" lang}))
-  (map #(let [output %
-              tree (:tree output)
-              text (nlp/process-sentence (:result output))]
-          {:text text :language lang :tokens (nlp/annotate text) :tree tree})
-       (-> (grammar/build-grammar semantic-graph (assoc context :constants {"*Language" lang}))
-           (generator/generate lang)
-           (service/request lang))))
+  (-> semantic-graph
+      (grammar/build-grammar (assoc context :constants {"*Language" lang}))
+      (gf-service/request lang)))
