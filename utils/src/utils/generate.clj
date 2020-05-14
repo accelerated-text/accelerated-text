8db2231287e4763a69a2742f6e4469ceda652f76
Žygimantas Medelis <zygimantas.medelis@tokenmill.lt>
2020-05-14 11:53:06 +0300

    feat: utility to generate variants for csv data files

--- utils/src/utils/generate.clj
+++ utils/src/utils/generate.clj
@@ -1,6 +1,12 @@
 (ns utils.generate
   (:require [clojure.data.csv :as csv]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [org.httpkit.client :as http]
+            [jsonista.core :as json]
+            [clojure.tools.logging :as log]
+            [clojure.string :as string]))
+
+(def read-mapper (json/object-mapper {:decode-key-fn true}))
 
 (defn read-data [csv-file]
   (with-open [reader (io/reader csv-file)]
@@ -9,31 +15,50 @@
            (repeat (first csv-data))
            (rest csv-data)))))
 
-(def colog [{"Department Brand" "Acqua di Parma" "Division" "toiletries" "Subclass" "shaving cream" "Color (UDA)" "" "Gender" "man" "Class" "cologne"}])
+(defn- post-generate [body]
+  (-> @(http/post "http://localhost:3001/nlg/_bulk/"
+                  {:headers {"Content-Type" "application/json"}
+                   :body    (json/write-value-as-string body)})
+      :body (json/read-value read-mapper) :resultIds))
 
-(defn write-variants-to-file [variants id-column output-dir]
-  (prn "Writing: " (get variants id-column))
-  (-> output-dir (io/file) .mkDirs)
-  (doseq [line variants]
-    (spit (format "%s/%s.txt" output-dir (get variants id-column))
-          (str line "\n")
-          :append true)))
+(defn- get-results [id]
+  [id (-> (format "http://localhost:3001/nlg/%s?format=raw" id)
+          http/get deref
+          :body (json/read-value read-mapper)
+          :variants)])
 
-(defn generate-bulk [document-plan-name data]
-  (let [ids (take (count data) (repeatedly #(str (java.util.UUID/randomUUID))))]
-    (api.nlg.service/generate-request-bulk
-     {:documentPlanName document-plan-name
-      :dataRows         (zipmap ids data)
-      :readerFlagValues {"English" true}})
-    (map (fn [id data-row]
-           (let [{{variants :variants} :body} (api.nlg.service/get-result
-                                               {:parameters {:path  {:id (str id)}
-                                                             :query {:format "raw"}}})]
-             (assoc data-row "results" (clojure.string/join "\n\n" variants))))
-         ids data)))
+(defn generate-bulk
+  "Generate text variants for data rows. Return collection of tuples where
+  first entry is original data and the second is a collection of variants."
+  [document-plan data]
+  (let [ids      (take (count data) (repeatedly #(str (java.util.UUID/randomUUID))))
+        id->data (zipmap ids data)]
+    (->> {:documentPlanName document-plan
+          :dataRows         id->data
+          :readerFlagValues {"English" true}}
+         (post-generate)
+         (map get-results)
+         (map (fn [[id variants]]
+                [(get id->data id) variants])))))
 
-(defn save-results-to-dir [data-file output-dir document-plan id-column]
+(defn save-data-with-variants
+  "Merge data rows and text variantas. For each text variant for the data item
+  create a new row with all the data points duplicated"
+  [out-file results]
+  (let [header (vec (map name (-> results first first keys)))
+        csv-data (mapcat (fn [[data variations]]
+                        (let [data-row (vec (map (fn [col] (get data col)) header))]
+                          (map (fn [variant]
+                                 (conj data-row variant))
+                               variations)))
+                      results)]
+    (with-open [writer (io/writer out-file)]
+      (csv/write-csv writer
+                     (cons (conj (vec (map name (-> results first first keys))) "Variants")
+                           csv-data)))))
+
+(defn data->text [document-plan data-file output-file]
   (->> data-file
-      (read-data)
-      (generate-bulk document-plan)
-      (write-variants-to-file id-column output-dir)))
+       (read-data)
+       (generate-bulk document-plan)
+       (save-data-with-variants output-file)))
