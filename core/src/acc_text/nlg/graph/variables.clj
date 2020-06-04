6a6d100de8a6d7e3f587bd58487a08a3b316700c
Rokas Ramanauskas <rokasramas@gmail.com>
2020-06-04 12:48:03 +0300

    fix: multiple variable definitions do not produce variatons

--- core/src/acc_text/nlg/graph/variables.clj
+++ core/src/acc_text/nlg/graph/variables.clj
@@ -1,16 +1,9 @@
 (ns acc-text.nlg.graph.variables
-  (:require [acc-text.nlg.graph.utils :refer [add-edges find-nodes get-successors]]
-            [loom.attr :refer [attrs]]
+  (:require [acc-text.nlg.graph.utils :refer [find-nodes]]
             [loom.graph :as graph]))
 
 (defn resolve-variables [g]
   (reduce (fn [g [node-id _]]
-            (let [in-edges (graph/in-edges g node-id)
-                  out-edges (graph/out-edges g node-id)]
-              (cond-> (graph/remove-nodes g node-id)
-                      (some? out-edges) (add-edges
-                                          (for [in-edge in-edges
-                                                out-edge (mapcat #(graph/out-edges g %) (get-successors g node-id))]
-                                            [^:edge (graph/src in-edge) (graph/dest out-edge) (attrs g in-edge)])))))
+            (cond-> g (zero? (graph/out-degree g node-id)) (graph/remove-nodes g node-id)))
           g
           (find-nodes g {:type :reference})))
