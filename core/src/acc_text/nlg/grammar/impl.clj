e19320053970d2d13300203760ef975f4e144b13
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2020-06-04 12:29:29 +0300

    chore: fix lint warnings

--- core/src/acc_text/nlg/grammar/impl.clj
+++ core/src/acc_text/nlg/grammar/impl.clj
@@ -10,7 +10,6 @@
             [acc-text.nlg.graph.paths :refer [resolve-paths]]
             [acc-text.nlg.graph.polarity :refer [resolve-polarity]]
             [acc-text.nlg.graph.utils :refer [add-concept-position find-root-id get-in-edge get-successors prune-graph]]
-            [acc-text.nlg.graph.variables :refer [resolve-variables]]
             [acc-text.nlg.semantic-graph.utils :refer [semantic-graph->ubergraph]]
             [clojure.math.combinatorics :refer [permutations]]
             [clojure.string :as str]
