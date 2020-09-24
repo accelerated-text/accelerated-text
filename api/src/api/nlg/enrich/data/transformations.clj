d8042c576b50fdfa851dfbdc6f8986409fa19a11
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2020-09-24 11:25:12 +0300

    fix: approximate

--- api/src/api/nlg/enrich/data/transformations.clj
+++ api/src/api/nlg/enrich/data/transformations.clj
@@ -3,4 +3,5 @@
 
 (defn approximate [s {:keys [language scale]
                       :or   {language :en, scale 1/4}}]
-  (:numwords/text (:numwords/around (nw/approximations language (Float/valueOf ^String s) scale))))
+  (let [approx (nw/approximations language (Float/valueOf s) scale)]
+    (:numwords/text (some #(get approx %) [:numwords/equal :numwords/around :numwords/more-than :numwords/less-than]))))
