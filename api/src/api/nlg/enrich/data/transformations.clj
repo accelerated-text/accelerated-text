bc26d7a5c6df00dac454cc778d4e099d23071dad
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2020-09-24 11:47:38 +0300

    refactor: check if string is not blank when approximating

--- api/src/api/nlg/enrich/data/transformations.clj
+++ api/src/api/nlg/enrich/data/transformations.clj
@@ -1,7 +1,9 @@
 (ns api.nlg.enrich.data.transformations
-  (:require [numberwords.core :as nw]))
+  (:require [clojure.string :as str]
+            [numberwords.core :as nw]))
 
 (defn approximate [s {:keys [language scale]
                       :or   {language :en, scale 1/4}}]
-  (let [approx (nw/approximations language (Float/valueOf s) scale)]
-    (:numwords/text (some #(get approx %) [:numwords/equal :numwords/around :numwords/more-than :numwords/less-than]))))
+  (when-not (str/blank? s)
+    (let [approx (nw/approximations language (Float/valueOf s) scale)]
+      (:numwords/text (some #(get approx %) [:numwords/equal :numwords/around :numwords/more-than :numwords/less-than])))))
