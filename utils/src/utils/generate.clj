3d827e2ac3186d7b1836b45d834564ff6e417a6b
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2020-09-25 14:13:22 +0300

    refactor: generate util

--- utils/src/utils/generate.clj
+++ utils/src/utils/generate.clj
@@ -1,38 +1,41 @@
 (ns utils.generate
   (:require [clojure.data.csv :as csv]
             [clojure.java.io :as io]
-            [org.httpkit.client :as http]
-            [jsonista.core :as json]
+            [clojure.string :as str]
             [clojure.tools.logging :as log]
-            [clojure.string :as str]))
+            [jsonista.core :as json]
+            [org.httpkit.client :as http])
+  (:import (java.io File)))
 
 (def read-mapper (json/object-mapper {:decode-key-fn true}))
 
 (defn read-data [csv-file]
   (with-open [reader (io/reader csv-file)]
-    (let [csv-data (doall (csv/read-csv reader))]
-      (map zipmap
-           (repeat (first csv-data))
-           (rest csv-data)))))
+    (doall (csv/read-csv reader))))
 
 (defn- post-generate [body]
-  (let [{:keys [status body]}
-        @(http/post "http://localhost:3001/nlg/_bulk/"
-                    {:headers {"Content-Type" "application/json"}
-                     :body    (json/write-value-as-string body)})
-        body (json/read-value body read-mapper)]
-    (if (= status 200)
-      (:resultIds body)
-      (log/error (:message body)))))
+  @(http/request {:url     "http://localhost:3001/nlg/_bulk/"
+                  :method  :post
+                  :headers {"Content-Type" "application/json"}
+                  :body    (json/write-value-as-string body)}
+                 (fn [{:keys [status body]}]
+                   (let [{result-ids :resultIds message :message} (json/read-value body read-mapper)]
+                     (if (= status 200)
+                       result-ids
+                       (log/error message))))))
+
+(defn- fetch-results [id]
+  @(http/request {:url (format "http://localhost:3001/nlg/%s?format=raw" id)}
+                 (fn [{:keys [status body]}]
+                   (if (= status 200)
+                     (let [{ready? :ready :as response} (json/read-value body read-mapper)]
+                       (when-not ready?
+                         (Thread/sleep 1000))
+                       response)
+                     (log/error "Failed to fetch result with status %d" status)))))
 
 (defn- get-results [id]
-  (->> (repeatedly #(let [{ready? :ready :as response}
-                          (-> (format "http://localhost:3001/nlg/%s?format=raw" id)
-                              http/get deref
-                              :body (json/read-value read-mapper))]
-                      (when-not ready?
-                        (Thread/sleep 1000))
-                      response))
+  (->> (repeatedly #(fetch-results id))
        (filter :ready)
        (first)
        (:variants)
@@ -51,17 +54,14 @@
          (post-generate)
          (map get-results)
          (map-indexed (fn [i [id variants]]
-                        (log/infof "%d out of %s instances ready" i (count ids))
+                        (log/infof "%.2f%% ready" (float (* 100 (/ (inc i) (count ids)))))
                         [(get id->data id) variants])))))
 
 (defn save-data-with-variants
   "Merge data rows and text variants. For each text variant for the data item
   create a new row with all the data points duplicated"
-  [out-file results]
-  (log/infof "Total %s results to save" (count results))
-  (let [header (vec (map name (-> results first first keys)))
-        csv-data (mapcat (fn [[data variations]]
-                           (log/infof "Data %s has %s variations" (get data (first header)) (count variations))
+  [out-file results header]
+  (let [csv-data (mapcat (fn [[data variations]]
                            (let [data-row (vec (map (fn [col] (get data col)) header))]
                              (map (fn [variant]
                                     (conj data-row variant))
@@ -69,18 +69,16 @@
                          results)]
     (when (seq results)
       (with-open [writer (io/writer out-file)]
-        (csv/write-csv writer
-                       (cons (conj (vec (map name (-> results first first keys))) "Variants")
-                             csv-data))))))
+        (csv/write-csv writer (cons (conj (vec header) "Variants") csv-data)))
+      (log/infof "Saved output to `%s`" (.getAbsolutePath ^File out-file)))))
 
 (defn data->text [document-plan data-file output-file language]
   (log/infof "Generating using '%s' document plan for '%s' language" document-plan language)
-  (->> data-file
-       (read-data)
-       (generate-bulk document-plan language)
-       (save-data-with-variants output-file)))
+  (let [[header & rows] (read-data data-file)
+        results (generate-bulk document-plan language (map #(zipmap header %) rows))]
+    (save-data-with-variants output-file results header)))
 
-(defn -main [& [document-plan data-file output-file language]]
-  (if (and document-plan data-file output-file)
-    (data->text document-plan data-file output-file (if-not (str/blank? language) language "Eng"))
+(defn -main [& [document-plan data-file output-path language]]
+  (if (and document-plan data-file output-path)
+    (data->text document-plan (io/file data-file) (io/file output-path) (if-not (str/blank? language) language "Eng"))
     (println "Usage: pass in four parameters: name of the document plan, path to a data file, path to an output file, and language code")))
