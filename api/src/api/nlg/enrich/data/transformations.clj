8a79441bfe4ade599bb228dfe4d130dad0e47c69
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2020-09-28 11:32:36 +0300

    feat: rough estimation fn

--- api/src/api/nlg/enrich/data/transformations.clj
+++ api/src/api/nlg/enrich/data/transformations.clj
@@ -5,5 +5,16 @@
 (defn approximate [s {:keys [language scale]
                       :or   {language :en, scale 1/4}}]
   (when-not (str/blank? s)
-    (let [approx (nw/approximations language (Float/valueOf s) scale)]
+    (let [approx (nw/approximations language (Float/valueOf ^String s) scale)]
       (:numwords/text (some #(get approx %) [:numwords/equal :numwords/around :numwords/more-than :numwords/less-than])))))
+
+(defn rough-estimation [s {}]
+  (let [n (Float/valueOf ^String s)
+        abs-n (Math/abs ^Float n)]
+    (str (when (> 0 n) "-")
+         (cond
+           (> 1000 abs-n) (str (Math/round ^Float n))
+           (> 1000000 abs-n) (str (Math/round ^Float (/ n 1000)) "K")
+           (> 1000000000 abs-n) (str (Math/round ^Float (/ n 1000000)) "M")
+           (> 1000000000000 abs-n) (str (Math/round ^Float (/ n 1000000000)) "B")
+           :else (str (Math/round ^Float (/ abs-n 1000000000000)) "T")))))
