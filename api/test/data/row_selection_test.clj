5f75cf4a8ba086127f833077ec1e6ada3b6c3fac
Šarūnas Navickas <zaibacu@gmail.com>
2021-01-15 15:44:18 +0200

    Fix tests namespace

--- api/test/data/row_selection_test.clj
+++ api/test/data/row_selection_test.clj
@@ -1,17 +1,17 @@
-(ns data.utils-test
+(ns data.row-selection-test
     (:require [clojure.test :refer [deftest is use-fixtures]]
-              [data.utils :as utils]))
+              [data.entities.data-files.row-selection :as row-selection]))
 
 (deftest test-jaccard-distance
-  (is (= 2/3 (utils/jaccard-distance
+  (is (= 2/3 (row-selection/jaccard-distance
               {:primary 1 :secondary 3}
               {:primary 2 :secondary 3}))))
 
 (deftest test-selected-rows
   (let [rows          [["test1" "test2"] ["test2" "test2"] ["test3" "test3"] ["test3" "test4"] ["test3" "test3"]]
-        m             (utils/distance-matrix rows)
-        selected-rows (utils/select-rows m rows 2)]
+        m             (row-selection/distance-matrix rows)
+        selected-rows (row-selection/select-rows m rows 2)]
     (is (= [["test1" "test2"] ["test3" "test3"]] selected-rows))))
 
 (deftest test-sample
-  (is (= [0 20 40 60 80] (utils/sample (range 100) 5))))
+  (is (= [0 20 40 60 80] (row-selection/sample (range 100) 5))))
