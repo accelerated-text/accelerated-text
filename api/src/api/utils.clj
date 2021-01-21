9a7862133bb49e96674a764089f56fd874760f06
Šarūnas Navickas <606346+zaibacu@users.noreply.github.com>
2021-01-21 14:01:25 +0200

    Allow to handle XLSX file (#250)

--- api/src/api/utils.clj
+++ api/src/api/utils.clj
@@ -1,5 +1,6 @@
 (ns api.utils
-  (:require [jsonista.core :as json])
+  (:require [jsonista.core :as json]
+            [clojure.java.io :as io])
   (:import (java.util UUID)))
 
 (def read-mapper (json/object-mapper {:decode-key-fn true}))
@@ -10,3 +11,10 @@
 (defn read-json-is [is]
   (when (some? is)
     (json/read-value is read-mapper)))
+
+(defn slurp-bytes
+  "Slurp the bytes from a slurpable thing"
+  [x]
+  (with-open [out (java.io.ByteArrayOutputStream.)]
+    (io/copy (clojure.java.io/input-stream x) out)
+    (.toByteArray out)))
