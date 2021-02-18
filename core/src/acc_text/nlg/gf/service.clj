2396d282a9846dc243d3bbe058d320b1a80bcaaf
Šarūnas Navickas <zaibacu@gmail.com>
2021-02-18 14:45:10 +0200

    It's too deep to check here.

--- core/src/acc_text/nlg/gf/service.clj
+++ core/src/acc_text/nlg/gf/service.clj
@@ -31,25 +31,24 @@
 
 (defn request
   [{::grammar/keys [module instance lincat] :as grammar} lang]
-  (when (ping)
-        (let [payload (generator/generate grammar (check-language lang))
-              request-url (or (System/getenv "GF_ENDPOINT") "http://localhost:8001")
-              request-content {:module module :instance instance :content payload}]
-          (log/debugf "Compiling grammar via %s" request-url)
-          (log/debugf "** Abstract **\n%s\n" (get payload module))
-          (log/debugf "** Incomplete concrete **\n%s\n" (get payload (str module "Body")))
-          (log/debugf "** Concrete **\n%s" (get payload (str module instance)))
-          (log/debugf "** Lex interface **\n%s\n" (get payload (str module "Lex")))
-          (log/debugf "** Lex data **\n%s\n" (get payload (str module "Lex" lang)))
-          (log/tracef "Request:\n curl -X POST -H \"Content-Type: application/json\"  %s -d '%s'"
-                      request-url (json/write-value-as-string request-content write-mapper))
-          (let [{body :body request-error :error}
-                @(client/request {:url     request-url
-                                  :method  :post
-                                  :headers {"Content-type" "application/json"}
-                                  :body    (json/write-value-as-string request-content write-mapper)})
-                {[[_ results]] :results error :error} (json/read-value body read-mapper)]
-            (cond
-             (some? request-error) (throw request-error)
-             (some? error) (throw (Exception. ^String error))
-             :else (map #(post-process lang lincat %) (distinct results)))))))
+  (let [payload (generator/generate grammar (check-language lang))
+        request-url (or (System/getenv "GF_ENDPOINT") "http://localhost:8001")
+        request-content {:module module :instance instance :content payload}]
+    (log/debugf "Compiling grammar via %s" request-url)
+    (log/debugf "** Abstract **\n%s\n" (get payload module))
+    (log/debugf "** Incomplete concrete **\n%s\n" (get payload (str module "Body")))
+    (log/debugf "** Concrete **\n%s" (get payload (str module instance)))
+    (log/debugf "** Lex interface **\n%s\n" (get payload (str module "Lex")))
+    (log/debugf "** Lex data **\n%s\n" (get payload (str module "Lex" lang)))
+    (log/tracef "Request:\n curl -X POST -H \"Content-Type: application/json\"  %s -d '%s'"
+                request-url (json/write-value-as-string request-content write-mapper))
+    (let [{body :body request-error :error}
+          @(client/request {:url     request-url
+                            :method  :post
+                            :headers {"Content-type" "application/json"}
+                            :body    (json/write-value-as-string request-content write-mapper)})
+          {[[_ results]] :results error :error} (json/read-value body read-mapper)]
+      (cond
+       (some? request-error) (throw request-error)
+       (some? error) (throw (Exception. ^String error))
+       :else (map #(post-process lang lincat %) (distinct results))))))
