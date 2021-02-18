20f0d20b8c6d3d1cdaaee2b78d422313d71b75b1
Šarūnas Navickas <zaibacu@gmail.com>
2021-02-18 15:14:42 +0200

    Fix lint

--- core/src/acc_text/nlg/gf/service.clj
+++ core/src/acc_text/nlg/gf/service.clj
@@ -24,7 +24,7 @@
 (defn ping []
   (let [service-host (or (System/getenv "GF_ENDPOINT") "http://localhost:8001")
         request-url (str service-host "/health")
-        {body :body request-error :error} @(client/request {:url request-url :method :get})]
+        {request-error :error} @(client/request {:url request-url :method :get})]
     (when (some? request-error)
       (log/errorf "GF service host: %s is unreachable. Are you sure it is started and reachable?" service-host))
     (nil? request-error)))
