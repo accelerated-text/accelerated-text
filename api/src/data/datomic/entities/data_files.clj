dbecdb6b7308789a3ac2bd6ddb5292d12d81e560
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2021-02-23 11:58:10 +0200

    refactor: do not alter datomic schema for now

--- api/src/data/datomic/entities/data_files.clj
+++ api/src/data/datomic/entities/data_files.clj
@@ -2,26 +2,31 @@
   (:require [data.spec.data-file :as data-file]
             [datomic.api :as d]))
 
-(def pattern [::data-file/id
-              ::data-file/name
-              ::data-file/timestamp
-              ::data-file/content])
+(def pattern [:data-file/id
+              :data-file/filename
+              :data-file/content])
+
+(defn translate [{:data-file/keys [id filename content] :as data-file}]
+  (when (some? data-file)
+    #::data-file{:id      id
+                 :name    filename
+                 :content content}))
 
 (defn transact-item [conn key data-item]
-  @(d/transact conn [#::data-file{:id key
-                                  :name (:filename data-item)
-                                  :timestamp (inst-ms (java.time.Instant/now))
-                                  :content  (:content data-item)}]))
+  @(d/transact conn [#:data-file{:id       key
+                                 :filename (:filename data-item)
+                                 :content  (:content data-item)}]))
 
 (defn pull-entity [conn key]
-  (d/pull (d/db conn) pattern [::data-file/id key]))
+  (translate (d/pull (d/db conn) pattern [:data-file/id key])))
 
 (defn pull-n [conn limit]
-  (map first (take limit (d/q '[:find (pull ?e pattern)
-                                :in $ pattern
-                                :where
-                                [?e ::data-file/id]]
-                              (d/db conn) pattern))))
+  (map (comp translate first)
+       (take limit (d/q '[:find (pull ?e pattern)
+                          :in $ pattern
+                          :where
+                          [?e :data-file/id]]
+                        (d/db conn) pattern))))
 
 (defn delete [conn key]
-  @(d/transact conn [[:db.fn/retractEntity [::data-file/id key]]]))
+  @(d/transact conn [[:db.fn/retractEntity [:data-file/id key]]]))
