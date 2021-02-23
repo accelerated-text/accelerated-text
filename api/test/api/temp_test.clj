fd226d2e1f57b0afcbd22210292385068d3d4a51
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2021-02-23 15:19:14 +0200

    chore: remove temp tests

--- api/test/api/temp_test.clj
+++
@@ -1,8 +0,0 @@
-(ns api.temp-test
-  (:require [clojure.test :refer :all]))
-
-(deftest failing-unit
-  (is (= true false)))
-
-(deftest ^:integration failing-integr
-  (is (= true false)))
--- core/test/acc_text/nlg/temp_test.clj
+++
@@ -1,8 +0,0 @@
-(ns acc-text.nlg.temp-test
-  (:require [clojure.test :refer :all]))
-
-(deftest failing-unit
-  (is (= true false)))
-
-(deftest ^:integration failing-integr
-  (is (= true false)))
