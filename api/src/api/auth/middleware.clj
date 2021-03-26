d858ffff3a3ff441f370abe671133b80dab76952
Šarūnas Navickas <zaibacu@gmail.com>
2021-03-26 14:04:51 +0200

    Dummy wrapper

--- api/src/api/auth/middleware.clj
+++ api/src/api/auth/middleware.clj
@@ -1,4 +1,13 @@
-(ns api.auth.middleware)
+(ns api.auth.middleware
+    (:require [clojure.tools.logging :as log]))
+
+(defn wrapper
+  [handler]
+  (fn [{:keys [headers] :as req}]
+    (log/infof "Look, I'm doing something!. This is headers: %s" headers)
+    (handler req)))
 
 
-(defn auth-middleware [& args])
+(def auth-middleware
+  {:name ::auth
+   :wrap wrapper})
