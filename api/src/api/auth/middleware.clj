018b822a8ee82d2ffb1f5c34fb1fabf29bde1558
Šarūnas Navickas <zaibacu@gmail.com>
2021-03-26 17:39:07 +0200

    Parse auth token

--- api/src/api/auth/middleware.clj
+++ api/src/api/auth/middleware.clj
@@ -1,9 +1,17 @@
 (ns api.auth.middleware
     (:require [clojure.tools.logging :as log]
-              [api.auth.service :as service]))
+              [api.auth.service :as service]
+              [clojure.walk :as walk]
+              [clojure.string :as str]))
+
+(defn normalize-headers [headers]
+  (->> (map (fn [[k v]] [(str/lower-case k) v]) headers)
+       (into {})
+       (walk/keywordize-keys)))
 
 (defn append-auth-info [{:keys [headers] :as req}]
-  (log/infof "Look, I'm doing something!. This is headers: %s" headers)
+  (let [token (:auth-token (normalize-headers headers))]
+    (log/infof "Look, I'm doing something!. Token: %s" token))
   req)
 
 (defn wrapper
