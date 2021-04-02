29f537e0b9c0f43d2110e94225a0e7951b072b9e
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2021-04-02 15:43:39 +0300

    chore: fix lint warnings in core

--- core/src/acc_text/nlg/graph/dictionary.clj
+++ core/src/acc_text/nlg/graph/dictionary.clj
@@ -1,9 +1,8 @@
 (ns acc-text.nlg.graph.dictionary
-  (:require [acc-text.nlg.graph.utils :refer [find-nodes]]
-            [clojure.tools.logging :as log]))
+  (:require [acc-text.nlg.graph.utils :refer [find-nodes]]))
 
 (defn ensure-dictionary-items [g {dictionary :dictionary}]
-  (doseq [[_ {key :label category :category :as attrs}] (find-nodes g {:type :dictionary-item})]
+  (doseq [[_ {key :label category :category}] (find-nodes g {:type :dictionary-item})]
     (when-not (contains? dictionary [key category])
       (throw (Exception. (format "Dictionary item `%s` (%s) not found." key category)))))
   g)
