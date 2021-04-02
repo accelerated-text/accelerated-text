9298501ab7dc1d238275c169d286391f8ebce3c4
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2021-04-02 19:47:01 +0300

    fix: dp translate

--- api/src/api/graphql/translate/document_plan.clj
+++ api/src/api/graphql/translate/document_plan.clj
@@ -7,8 +7,17 @@
             [clojure.tools.logging :as log]
             [clojure.zip :as zip]
             [data.entities.dictionary :refer [get-dictionary-item-category]]
-            [data.entities.document-plan.zip :as dp-zip]
             [jsonista.core :as json]))
+
+(defn dp-zipper [m]
+  (zip/zipper
+    (fn [x] (or (map? x) (sequential? x)))
+    seq
+    (fn [p xs]
+      (if (isa? (type p) clojure.lang.MapEntry)
+        (into [] xs)
+        (into (empty p) xs)))
+    m))
 
 (defn parse-blockly-xml [is]
   (try
@@ -17,35 +26,41 @@
       (log/error "Failed to parse blockly xml."))))
 
 (defn ensure-blockly-categories [blockly-xml]
-  (when (some? blockly-xml)
-    (with-open [is (io/input-stream (.getBytes blockly-xml))]
-      (loop [loc (zip/xml-zip (parse-blockly-xml is))]
-        (if (zip/end? loc)
-          (xml/emit-str (zip/root loc))
-          (-> loc
-              (zip/edit (fn [{{:keys [id concept_id pos concept_kind]} :attrs :as node}]
-                          (cond-> node
-                                  (= "null" pos) (assoc-in [:attrs :pos] (get-dictionary-item-category id))
-                                  (= "null" concept_kind) (assoc-in [:attrs :concept_kind] (:kind (get-amr-concept concept_id))))))
-              (zip/next)
-              (recur)))))))
+  (letfn [(edit [{{:keys [id concept_id pos concept_kind]} :attrs :as node}]
+            (cond-> node
+                    (= "null" pos) (assoc-in [:attrs :pos] (get-dictionary-item-category id))
+                    (= "null" concept_kind) (assoc-in [:attrs :concept_kind] (:kind (get-amr-concept concept_id)))))]
+    (when (some? blockly-xml)
+      (with-open [is (io/input-stream (.getBytes blockly-xml))]
+        (loop [loc (zip/xml-zip (parse-blockly-xml is))]
+          (if (zip/end? loc)
+            (xml/emit-str (zip/root loc))
+            (-> loc
+                (zip/edit edit)
+                (zip/next)
+                (recur))))))))
 
 (defn ensure-categories [document-plan]
-  (loop [loc (dp-zip/make-zipper document-plan)]
-    (if (zip/end? loc)
-      (zip/root loc)
-      (-> loc
-          (zip/edit #(cond
-                       (and
-                         (or (= "Dictionary-item" (:type %))
-                             (= "Dictionary-item-modifier" (:type %)))
-                         (or (nil? (:kind %)) (= "null" (:kind %)))) (assoc % :kind (get-dictionary-item-category (:itemId %)))
-                       (and
-                         (= "AMR" (:type %))
-                         (or (nil? (:kind %)) (= "null" (:kind %)))) (assoc % :kind (:kind (get-amr-concept (:conceptId %))))
-                       :else %))
-          (zip/next)
-          (recur)))))
+  (letfn [(edit [node]
+            (if (map? node)
+              (let [{:keys [type kind itemId conceptId]} node]
+                (cond
+                  (and
+                    (or (= "Dictionary-item" type)
+                        (= "Dictionary-item-modifier" type))
+                    (or (nil? kind) (= "null" kind))) (assoc node :kind (get-dictionary-item-category itemId))
+                  (and
+                    (= "AMR" type)
+                    (or (nil? kind) (= "null" kind))) (assoc node :kind (:kind (get-amr-concept conceptId)))
+                  :else node))
+              node))]
+    (loop [loc (dp-zipper document-plan)]
+      (if (zip/end? loc)
+        (zip/root loc)
+        (-> loc
+            (zip/edit edit)
+            (zip/next)
+            (recur))))))
 
 (defn schema->dp [{:keys [id uid name kind examples blocklyXml documentPlan dataSampleId dataSampleRow dataSampleMethod]}]
   {:id               id
