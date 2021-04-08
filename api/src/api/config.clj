d23806f7407557c9bfc7146c66dcb44a79289c34
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2021-04-08 17:25:16 +0300

    fix: duplicate key

--- api/src/api/config.clj
+++ api/src/api/config.clj
@@ -19,7 +19,6 @@
    :config-path          (or (System/getenv "CONFIG_PATH") (io/resource "config"))
    :dictionary-path      (or (System/getenv "DICT_PATH") (io/resource "dictionary"))
    :document-plan-path   (or (System/getenv "DOCUMENT_PLANS") (io/resource "document-plans"))
-   :relevant-items-limit (or (System/getenv "RELEVANT_ITEMS_MATRIX_LIMIT") 100)
    :auth-url             (System/getenv "AUTH_URL")
    :remove-duplicates    (Boolean/valueOf ^String (or (System/getenv "REMOVE_DUPLICATES") "TRUE"))
    :enable-cache         (Boolean/valueOf ^String (or (System/getenv "ENABLE_CACHE") "FALSE"))
